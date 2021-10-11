d64622a6e79f9f10e719192cc9a48ca6abe249d6
Babken <babgev@gmail.com>
2021-10-11 19:38:10 +0400

    Clean next confis js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const createNextPluginPreval = require('next-plugin-preval/config');
-const withNextPluginPreval = createNextPluginPreval();
+module.exports = {
 
-module.exports = withNextPluginPreval();
+  
+  };
