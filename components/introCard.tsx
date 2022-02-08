5be8db9ba7b3c9490a440f225feafc34e0b5b33d
Babken <babgev@gmail.com>
2022-02-08 17:50:14 +0400

    Add news about usdc

--- components/introCard.tsx
+++ components/introCard.tsx
@@ -15,10 +15,10 @@ function IntroCard(props) {
                     <p className="py-4">
                         <span className="bg-indigo-600 text-sm mr-2 rounded-md py-1 px-2">New</span>
                         <span className="md:hidden">
-                            1 ETH transfers in zkSync!
+                            1 ETH and 1500 USDC in zkSync!
                         </span>
                         <span className="hidden md:inline">
-                            1 ETH transfers in zkSync!
+                            1 ETH and 1500 USDC transfers in zkSync!
                             {/* <a className='underline text-indigo-300' target="_blank" href="https://twitter.com/layerswap/status/1480921959252774918">Join the discussion</a> */}
                         </span>
                     </p>
