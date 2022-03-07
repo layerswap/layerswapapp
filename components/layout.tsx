1c874c59d99f3e6410a7c69d6d58f50de9d08fa3
babgev <babgev@gmail.com>
2022-03-07 16:22:54 +0400

    Remove safaris mask-icon meta

--- components/layout.tsx
+++ components/layout.tsx
@@ -16,7 +16,6 @@ export default function Layout({ hasSideShapes, children }: Props) {
       <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
       <link rel="manifest" href="favicon/site.webmanifest" />
-      <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content="#111827" />
  
