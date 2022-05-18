6a6d19efaa6c4e00bddb402643045b334571cb1c
babken <mesropyan.b@gmail.com>
2022-05-18 12:42:35 +0400

    Added api key wizard

--- context/wizard.tsx
+++ context/wizard.tsx
@@ -32,16 +32,29 @@ const defaultSteps: Step[] = [
     { name: "Swap confirmation", status: "upcoming", content: SwapConfirmationStep },
     { name: "Email confirmation", status: "upcoming", content: EmailStep },
     { name: "Code", status: "upcoming", content: CodeInputStep },
-    { name: "Authorize", status: "upcoming", content: AccountConnectStep },
+    { name: "Please connect your account to Layerswap", status: "upcoming", content: AccountConnectStep },
+    { name: "", status: "upcoming", content: ProccessingStep, navigationDisabled: true },
+    { name: "", status: "upcoming", content: SuccessfulStep, navigationDisabled: true },
+]
+
+const apiKeyFlowSteps: Step[] = [
+    { name: "Swap", status: "current", content: MainStep, navigationDisabled: true },
+    { name: "Swap confirmation", status: "upcoming", content: SwapConfirmationStep },
+    { name: "Email confirmation", status: "upcoming", content: EmailStep },
+    { name: "Code", status: "upcoming", content: CodeInputStep },
+    { name: "Please provide Read-only API keys", status: "upcoming", content: APIKeyStep },
+    { name: "Please connect your account to Layerswap", status: "upcoming", content: AccountConnectStep },
+    { name: "Withdrawal", status: "upcoming", content: WithdrawExchangeStep },
     { name: "", status: "upcoming", content: ProccessingStep, navigationDisabled: true },
     { name: "", status: "upcoming", content: SuccessfulStep, navigationDisabled: true },
 ]
 
 const wizards = {
-    'coinbase': [{ name: "Step 2", status: "upcoming", content: ConfirmationStep },],
-    'bitfinex': [
-        { name: "Step 2", status: "upcoming", content: SomeTestStep },
-        { name: "Step 3", status: "upcoming", content: ConfirmationStep },],
+    // 'coinbase': [{ name: "Step 2", status: "upcoming", content: ConfirmationStep },],
+    'binance': apiKeyFlowSteps,
+    // 'bitfinex': [
+    //     { name: "Step 2", status: "upcoming", content: SomeTestStep },
+    //     { name: "Step 3", status: "upcoming", content: ConfirmationStep },],
 }
 
 export function WizardProvider({ children }) {
@@ -68,11 +81,9 @@ export function WizardProvider({ children }) {
     }, []);
 
     useEffect(() => {
-        // if (swapData?.exchange) {
-        //     setSteps([
-        //         ...defaultSteps,
-        //         ...(wizards[swapData.exchange.id] || [])])
-        // }
+        if (swapData?.exchange && wizards[swapData.exchange.id]) {
+            setSteps(wizards[swapData.exchange.id])
+        }
     }, [swapData])
 
     const wrapper = useRef(null);
@@ -111,28 +122,23 @@ export function WizardProvider({ children }) {
                     }
                     return v;
                 })
-
             );
-
     }, [steps])
 
     return (
         <WizardStateContext.Provider value={{ nextStep, prevStep, }}>
             <div className="bg-darkBlue shadow-card rounded-lg w-full overflow-hidden relative  border-t-4 border-ouline-blue">
-                <div className='grid grid-cols-2 gap-4 place-content-end p-2'>
-                    {
-                        !steps.find(s => s.status === 'current')?.navigationDisabled &&
-                        <>
-                            <button onClick={prevStep} className="justify-self-start">
-                                <ArrowLeftIcon className='h-5 w-5 text-darkblue-200 hover:text-ouline-blue cursor-pointer' />
-                            </button>
-                            <button onClick={nextStep} className="justify-self-end">
-                                <ArrowRightIcon className='h-5 w-5 text-darkblue-200 hover:text-ouline-blue cursor-pointer' />
-                            </button>
-                        </>
-                    }
+                <div className="grid grid-cols-2 gap-4 place-content-end p-2" style={{ visibility: steps.find(s => s.status === 'current')?.navigationDisabled ? 'hidden' : 'visible' }}>
+                    <>
+                        <button onClick={prevStep} className="justify-self-start">
+                            <ArrowLeftIcon className='h-5 w-5 text-darkblue-200 hover:text-ouline-blue cursor-pointer' />
+                        </button>
+                        <button onClick={nextStep} className="justify-self-end">
+                            <ArrowRightIcon className='h-5 w-5 text-darkblue-200 hover:text-ouline-blue cursor-pointer' />
+                        </button>
+                    </>
                 </div>
-                <div className='text-center text-lg text-darkblue-200'>
+                <div className='text-center text-xl text-darkblue-200'>
                     {steps.find(s => s.status === 'current').name}
                 </div>
                 <div className="p-2">
@@ -151,7 +157,7 @@ export function WizardProvider({ children }) {
                                             ? `translate-x-96 opacity-0`
                                             : `-translate-x-96 opacity-0`
                                     }
-                                    enterTo={`translate-x-0 opacity-100`}
+                                    enterTo={`opacity-100`}
                                     leave="transform transition ease-in-out duration-500"
                                     leaveFrom={`translate-x-0 opacity-100`}
                                     leaveTo={
@@ -165,7 +171,7 @@ export function WizardProvider({ children }) {
                                     <div
                                         style={{ width: `${wrapperWidth}px`, minHeight: '440px' }}
                                     >
-                                        <step.content current={step.status === 'current'}/>
+                                        <step.content current={step.status === 'current'} />
                                     </div>
                                 </Transition>)
                             }
