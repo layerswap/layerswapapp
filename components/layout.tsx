e8aa1b102a1b5f9724c1a522d1eca9ece46e2a2c
Aren <aren@bransfer.io>
2022-05-27 17:02:47 +0400

    change favicons, updated title and fixed apiKey step(bransfer to layerswap)

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,7 +11,7 @@ type Props = {
 export default function Layout({ hasSideShapes, children }: Props) {
   return (<>
     <Head>
-      <title>LayerSwap</title>
+      <title>Layerswap</title>
       <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
