b6c91a3d30667c5b05337c625ae4c7eab4e67790
Aren <aren@bransfer.io>
2022-05-31 18:10:57 +0400

    added Intercom widget

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,21 @@
 import '../styles/globals.css'
 import { Web3ReactProvider } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
+import { IntercomProvider } from 'react-use-intercom';
 
 const getLibrary = () => {
   const provider = window.web3.currentProvider
   return new Web3Provider(provider)
 }
 
+const INTERCOM_APP_ID = 'h5zisg78';
+
 function App({ Component, pageProps }) {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
+      <IntercomProvider autoBoot={true} appId={INTERCOM_APP_ID}>
+        <Component {...pageProps} />
+      </IntercomProvider>
     </Web3ReactProvider>)
 }
 
