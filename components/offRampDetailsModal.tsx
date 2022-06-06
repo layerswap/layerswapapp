46af24743f9fe6f0c6d74f64e994d2b13632f4de
Aren <aren@bransfer.io>
2022-06-06 16:33:49 +0400

    off-ramp details modal update

--- components/offRampDetailsModal.tsx
+++ components/offRampDetailsModal.tsx
@@ -16,9 +16,26 @@ interface OffRampDetailsModalParams {
 const OffRampDetailsModal: FC<OffRampDetailsModalParams> = ({ onConfirm, address, amount, memo, ...modalParams }) => {
     const modalDescription = () => {
         return (
-            <p className="text-sm text-gray-300 text-left">
-                Please do a transaction with the provided details
-            </p>)
+            <div className='flex-col w-full rounded-lg bg-pink-600 shadow-lg p-2'>
+                <div className='flex items-center'>
+                    <div className='mr-2 p-2 rounded-lg bg-pink-800'>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                        </svg>
+                    </div>
+                    <p className='font-medium'>
+                        Please do a transaction with the provided details
+                    </p>
+                </div>
+                <div className='mt-2 pl-5'>
+                    <ul className='list-disc'>
+                        <li>Go to Loopring Wallet and click “Send”</li>
+                        <li>Then select the <strong>“To Another Loopring L2 Account”</strong> option</li>
+                        <li>Fill in the information provided below</li>
+                    </ul>
+                </div>
+            </div>
+            )
     }
     
     const checkButtonIcon = <CheckIcon className='h-5 w-5'></CheckIcon>
@@ -41,7 +58,7 @@ const OffRampDetailsModal: FC<OffRampDetailsModalParams> = ({ onConfirm, address
                 </div>
                 <div>
                     <label htmlFor="address" className="block text-sm font-medium">
-                        Address
+                        Recipient
                     </label>
                     <input
                         id="address"
@@ -59,6 +76,18 @@ const OffRampDetailsModal: FC<OffRampDetailsModalParams> = ({ onConfirm, address
                         id="memo"
                         className="block bg-gray-800 border-2 text-sm border-gray-600 w-full font-semibold rounded-md py-3 px-4 ntdi"
                         value={memo}
+                        disabled={true}
+                    >
+                    </input>
+                </div>
+                <div>
+                    <label htmlFor="AddressType" className="block text-sm font-medium">
+                        Address Type
+                    </label>
+                    <input
+                        id="addressType"
+                        className="block bg-gray-800 border-2 text-sm border-gray-600 w-full font-semibold rounded-md py-3 px-4 ntdi"
+                        value="EOA Wallet"
                         disabled={true}
                     >
                     </input>
