db5ef7bc9562a8994c7fc47a1fa68923e4258dd1
babken <mesropyan.b@gmail.com>
2022-06-15 22:21:50 +0400

    safari lag

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,38 +29,7 @@ export default function Home({ data, query, isOfframpEnabled }: InferGetServerSi
 
   const [addressSource, setAddressSource] = useState(query.addressSource);
 
-  useEffect(() => {
-    let isImtoken = (window as any)?.ethereum?.isImToken !== undefined;
-    let isTokenPocket = (window as any)?.ethereum?.isTokenPocket !== undefined;
-
-    if (isImtoken || isTokenPocket) {
-      if (isImtoken) {
-        setAddressSource("imtoken");
-      }
-      else if (isTokenPocket) {
-        setAddressSource("tokenpocket");
-      }
-      let supportedNetworks = data.networks.filter(x => x.chain_id != -1 && x.is_enabled);
-      const injected = new InjectedConnector({
-        // Commented to allow visitors from other networks to use this page
-        // supportedChainIds: supportedNetworks.map(x => x.chain_id)
-      });
-
-      if (!active) {
-        activate(injected, onerror => {
-          if (onerror.message.includes('user_canceled')) {
-            return alert('You canceled the operation, please refresh and try to reauthorize.')
-          }
-          else if (onerror.message.includes('Unsupported chain')) {
-            // Do nothing
-          }
-          else {
-            alert(`Failed to connect: ${onerror.message}`)
-          }
-        });
-      }
-    }
-  },[])
+  
 
   if (chainId) {
     let network = data.networks.find(x => x.chain_id == chainId);
