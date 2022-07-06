8cf5d240965c81acf28338085ed84f4aaffa7dc2
babken <mesropyan.b@gmail.com>
2022-07-06 19:31:56 +0400

    Adding slidover

---
+++ components/SlideOver.tsx
@@ -0,0 +1,75 @@
+import { Transition } from "@headlessui/react";
+import { XIcon } from "@heroicons/react/outline";
+import { FC, Fragment, useState } from "react"
+
+const SlideOver: FC = () => {
+    const [open, setOpen] = useState(false)
+    const handleClose = () => setOpen(false)
+
+    
+    return (
+        <>
+            <Transition
+                appear
+                show={open}
+                as={Fragment}
+                enter="ease-in-out duration-300"
+                enterFrom="translate-y-full"
+                enterTo="translate-y-0"
+                leave="ease-in duration-200"
+                leaveFrom="translate-y-0"
+                leaveTo="translate-y-full">
+                <div className='absolute inset-0 z-40 -inset-y-11 flex flex-col w-full bg-darkBlue'>
+                    <span className='relative z-40 overflow-hidden bg-darkBlue p-10 pt-0'>
+                        <div className='relative grid grid-cols-1 gap-4 place-content-end z-40 mb-2 mt-1'>
+                            <span className="justify-self-end text-light-blue cursor-pointer">
+                                <div className="">
+                                    <button
+                                        type="button"
+                                        className="rounded-md text-darkblue-200 focus:ring-2 hover:text-light-blue"
+                                        onClick={handleClose}
+                                    >
+                                        <span className="sr-only">Close</span>
+                                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                                    </button>
+                                </div>
+                            </span>
+                        </div>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <div className="relative inset-0" ></div>
+                        </Transition.Child>
+
+                        <div className="relative inset-0 text-pink-primary-300 flex flex-col overflow-y-auto scrollbar:!w-1.5 scrollbar:!h-1.5 scrollbar:bg-darkblue-500 scrollbar-track:!bg-slate-100 scrollbar-thumb:!rounded scrollbar-thumb:!bg-slate-300 scrollbar-track:!rounded scrollbar-track:!bg-slate-500/[0.16] scrollbar-thumb:!bg-slate-500/50">
+                            <div className="relative min-h-full items-center justify-center p-4 pt-0 text-center">
+                                <Transition.Child
+                                    as={Fragment}
+                                    enter="ease-out duration-300"
+                                    enterFrom="opacity-0 scale-95"
+                                    enterTo="opacity-100 scale-100"
+                                    leave="ease-in duration-200"
+                                    leaveFrom="opacity-100 scale-100"
+                                    leaveTo="opacity-0 scale-95"
+                                >
+
+                                    <div className='pb-12 grid grid-flow-row min-h-[480px] text-pink-primary-300'>
+
+                                    </div>
+                                </Transition.Child>
+                            </div>
+                        </div>
+                    </span>
+                </div>
+            </Transition>
+        </>
+    )
+}
+
+export default SlideOver;
