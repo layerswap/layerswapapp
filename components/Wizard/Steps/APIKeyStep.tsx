b397e03cf6898aedd638a3ed86f39405a2b5c8dd
babken <mesropyan.b@gmail.com>
2022-07-14 23:25:52 +0400

    Removed unnecessary imports

--- components/Wizard/Steps/APIKeyStep.tsx
+++ components/Wizard/Steps/APIKeyStep.tsx
@@ -1,17 +1,9 @@
-import { InformationCircleIcon } from '@heroicons/react/outline';
-import { FC, useCallback, useRef, useState } from 'react'
-import toast from 'react-hot-toast';
-import { useAuthDataUpdate } from '../../../context/auth';
+import { FC} from 'react'
 import { useFormWizardaUpdate } from '../../../context/formWizardProvider';
-import { useSettingsState } from '../../../context/settings';
 import { useSwapDataState } from '../../../context/swap';
-import { BransferApiClient } from '../../../lib/bransferApiClients';
-import ExchangeSettings from '../../../lib/ExchangeSettings';
 import { FormWizardSteps } from '../../../Models/Wizard';
-import SubmitButton from '../../buttons/submitButton';
 import ConnectApiKeyExchange from '../../connectApiKeyExchange';
-import { DocIframe } from '../../docInIframe';
-import SlideOver, { SildeOverRef } from '../../SlideOver';
+
 
 const APIKeyStep: FC = () => {
     const { swapFormData } = useSwapDataState()
