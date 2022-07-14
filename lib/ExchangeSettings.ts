012860cffa31b172b59c8bdf302c86f29d09a371
babken <mesropyan.b@gmail.com>
2022-07-14 22:08:33 +0400

    Added apikey userguides

--- lib/ExchangeSettings.ts
+++ lib/ExchangeSettings.ts
@@ -20,6 +20,7 @@ export default class ExchangeSettings {
         ExchangeSettings._isInitialized = true;
 
         ExchangeSettings.KnownSettings[KnownIds.Exchanges.BinanceId] = {
+            UserApiKeyGuideUrl: "https://app.tango.us/app/embed/279b954e2d3343038aef5e6a5ad21734?iframe",
             ExchangeApiKeyPageUrl: "https://www.binance.com/en/my/settings/api-management",
             ExchangeWithdrawalPageUrl: "https://www.binance.com/en/my/wallet/account/main/withdrawal/crypto",
         };
@@ -27,6 +28,7 @@ export default class ExchangeSettings {
             UserApiKeyGuideUrl: "https://app.tango.us/app/embed/c8cf508a2c5a4780a9225ac87668d349?iframe",
         };
         ExchangeSettings.KnownSettings[KnownIds.Exchanges.BittrexId] = {
+            UserApiKeyGuideUrl: "https://app.tango.us/app/embed/5564cb40d40446aba1702c3f7b2f5424?iframe",
             ExchangeApiKeyPageUrl: "https://global.bittrex.com/Manage?view=api",
         };
         ExchangeSettings.KnownSettings[KnownIds.Exchanges.BlocktaneId] = {
@@ -35,10 +37,12 @@ export default class ExchangeSettings {
         ExchangeSettings.KnownSettings[KnownIds.Exchanges.CoinbaseId] = {
         };
         ExchangeSettings.KnownSettings[KnownIds.Exchanges.CryptoComId] = {
+            UserApiKeyGuideUrl: "https://app.tango.us/app/embed/d791596ecafb41ed8c0bf98dcaa44f7b?iframe",
             ExchangeApiKeyPageUrl: "https://crypto.com/exchange/user/settings/api-management",
             WithdrawalWarningMessage: "Only use Crypto.com 'Exchange' to do the transfer. Transfers done from the Crypto.com *App* will not be credited."
         };
         ExchangeSettings.KnownSettings[KnownIds.Exchanges.FtxComId] = {
+            UserApiKeyGuideUrl: "https://app.tango.us/app/embed/7221bd286bf5445fa124994ec02f4d85?iframe",
         };
         ExchangeSettings.KnownSettings[KnownIds.Exchanges.FtxUsId] = {
             UserApiKeyGuideUrl: "https://app.tango.us/app/embed/7221bd286bf5445fa124994ec02f4d85?iframe",
