c011b0f9a7df93529a8429a3e3d2bc8a09aa413f
babgev <babgev@gmail.com>
2022-07-19 16:47:50 +0400

    Fix env variables not being accessible from the browser

--- lib/AppSettings.ts
+++ lib/AppSettings.ts
@@ -1,8 +1,6 @@
-enum configConstans { BransferApi = "BRANSFER_API", LayerswapApi =  "LAYERSWAP_API", IdentityApi =  "IDENTITY_API" }
-
 export default class AppSettings {
-    static LayerswapApiUri: string = process.env[configConstans.LayerswapApi];
-    static BransferApiUri: string = process.env[configConstans.BransferApi];
-    static IdentityApiUri: string = process.env[configConstans.IdentityApi];
+    static LayerswapApiUri: string = process.env.NEXT_PUBLIC_LAYERSWAP_API;
+    static BransferApiUri: string = process.env.NEXT_PUBLIC_BRANSFER_API;
+    static IdentityApiUri: string = process.env.NEXT_PUBLIC_IDENTITY_API;
 }
 
