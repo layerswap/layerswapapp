725a8fc041444b69e283629137bbbab246db300e
Aren <aren@bransfer.io>
2022-08-30 16:29:37 +0400

    second try to fix issue

--- components/utils/GoHome.tsx
+++ components/utils/GoHome.tsx
@@ -1,5 +1,5 @@
-import router from "next/router";
-import { FC, useCallback } from "react";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
 import LayerSwapLogo from "../icons/layerSwapLogo";
 
 interface Props {
@@ -7,16 +7,16 @@ interface Props {
 }
 
 const GoHomeButton = (({ className }: Props) => {
-
+    const router = useRouter()
     const handleGoHome = useCallback(() => {
-        router.push({
+        typeof window !== 'undefined' && router.push({
             pathname: "/",
             query: router.query
         })
     }, [router.query])
 
     return (
-        <a onClick={handleGoHome}>
+        <a onClick={() => handleGoHome()}>
             <LayerSwapLogo className={className ?? "h-8 w-auto text-white"} />
         </a>
     )
