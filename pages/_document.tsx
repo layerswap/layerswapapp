3d09b1ed41151cfa08cacbce03abe4d61f4c6c74
babgev <babgev@gmail.com>
2022-09-22 22:49:14 +0400

    Enable immutablex color scheme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html>
             <Head />
-            <body>
+            <body className='immutablex'>
                 <Main />
                 <NextScript />
             </body>
