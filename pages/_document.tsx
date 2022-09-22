965a12f90ffa8d9a250347695976b60626c92569
babgev <babgev@gmail.com>
2022-09-22 23:33:30 +0400

    Remove immutable color scheme selected

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html>
             <Head />
-            <body className='immutablex'>
+            <body>
                 <Main />
                 <NextScript />
             </body>
