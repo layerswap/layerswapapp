9bb4ba0c5fc8cad0a8465eda919be6161d0799ff
Aren <aren@bransfer.io>
2022-10-25 16:07:32 +0400

    add min max buttons

--- components/Input/Amount.tsx
+++ components/Input/Amount.tsx
@@ -1,5 +1,5 @@
 import { useFormikContext } from "formik";
-import { forwardRef, useRef } from "react";
+import { forwardRef, useRef, useState } from "react";
 import { getCurrencyDetails } from "../../helpers/currencyHelper";
 import { CalculateMaxAllowedAmount, CalculateMinAllowedAmount } from "../../lib/fees";
 import { SwapFormValues } from "../DTOs/SwapFormValues";
@@ -10,6 +10,7 @@ const AmountField = forwardRef((props: any, ref: any) => {
 
     const { values: { currency, swapType, exchange, network } } = useFormikContext<SwapFormValues>();
     const name = "amount"
+    const [amount, setAmount] = useState(undefined)
 
     const minAllowedAmount = CalculateMinAllowedAmount(currency?.baseObject, exchange?.baseObject, network?.baseObject, swapType);
     const maxAllowedAmount = CalculateMaxAllowedAmount(currency?.baseObject, exchange?.baseObject, network?.baseObject, swapType);
@@ -20,9 +21,25 @@ const AmountField = forwardRef((props: any, ref: any) => {
     const step = 1 / Math.pow(10, currencyDetails?.precision)
     const amountRef = useRef(ref)
 
+    const amountLabel = (
+        <div className="flex items-center space-x-2">
+            <p>Amount</p>
+            {document.activeElement == amountRef?.current &&
+                <div className="text-xs text-primary-text flex items-center space-x-1">
+                    <button onClick={() => setAmount(minAllowedAmount)} className="py-0.5 px-1 bg-darkblue-600 hover:bg-darkblue-400 rounded-md">
+                        Min {minAllowedAmount}
+                    </button>
+                    <span>-</span>
+                    <button onClick={() => setAmount(maxAllowedAmount)} className="py-0.5 px-1 bg-darkblue-600 hover:bg-darkblue-400 rounded-md">
+                        Max {maxAllowedAmount}
+                    </button>
+                </div>}
+        </div>
+    )
+
     return (<>
         <NumericInput
-            label={<p>Amount {document.activeElement == amountRef?.current && <span className="text-xs text-primary-text">({minAllowedAmount} - {maxAllowedAmount})</span>}</p>}
+            label={amountLabel}
             disabled={!currency}
             placeholder={placeholder}
             min={minAllowedAmount}
