cb9c3efd16c054ca1cddac520ac6667375167705
Aren <aren@bransfer.io>
2022-10-31 20:14:43 +0400

    update logo url storage (again)

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ module.exports = (phase, { defaultConfig }) => {
       defaultLocale: "en",
     },
     images: {
-      domains: ["bransferstorage.blob.core.windows.net", "devlslayerswapbridgesa.blob.core.windows.net"],
+      domains: ["bransferstorage.blob.core.windows.net", "devlslayerswapbridgesa.blob.core.windows.net", "prodlslayerswapbridgesa.blob.core.windows.net"],
     },
     compiler: {
       removeConsole: false,
