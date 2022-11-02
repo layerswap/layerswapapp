2f00180a30109f8076ce07743cc987f7e2483bbd
Aren <aren@bransfer.io>
2022-11-02 18:20:56 +0400

    update ExchangeDelayStep

--- components/Wizard/Steps/ExchangeDelayStep.tsx
+++ components/Wizard/Steps/ExchangeDelayStep.tsx
@@ -1,3 +1,4 @@
+import { BookOpenIcon, HomeIcon, InformationCircleIcon } from '@heroicons/react/outline';
 import { FC } from 'react'
 import SubmitButton from '../../buttons/submitButton';
 import MessageComponent from '../../MessageComponent';
@@ -13,23 +14,26 @@ const ExchangeDelay: FC = () => {
                 </MessageComponent.Header>
                 <MessageComponent.Description>
                     <p>This swap is being delayed by Coinbase. This usually means that the exchange needs additional verification.</p>
-                    <p className='font-bold'>
-                        What to do
-                        <ul>
+                    <div className='p-4 bg-darkblue-700 text-white rounded-lg border border-darkblue-500'>
+                        <div className="flex items-center">
+                            <InformationCircleIcon className='h-5 w-5 text-primary-600 mr-3' />
+                            <label className="block text-sm md:text-base font-medium leading-6">What to do?</label>
+                        </div>
+                        <ul className="list-disc font-light space-y-1 text-xs md:text-sm mt-2 ml-8 text-left">
                             <li>Check your email for details from Coinbase</li>
                             <li>Check your Coinbase account's transfer history</li>
                         </ul>
-                    </p>
+                    </div>
                 </MessageComponent.Description>
             </MessageComponent.Content>
             <MessageComponent.Buttons>
-                <SubmitButton buttonStyle='outline' isDisabled={false} isSubmitting={false} onClick={() => {
+                <SubmitButton buttonStyle='outline' icon={<BookOpenIcon className="h-5 w-5 ml-2" />} isDisabled={false} isSubmitting={false} onClick={() => {
                     window.open('https://docs.layerswap.io/user-docs/why-is-coinbase-transfer-taking-so-long', '_blank')
                 }}>
                     Learn More
                 </SubmitButton>
                 <GoHomeButton>
-                    <SubmitButton buttonStyle='outline' isDisabled={false} isSubmitting={false}>
+                    <SubmitButton buttonStyle='outline' isDisabled={false} isSubmitting={false} icon={<HomeIcon className="h-5 w-5 ml-2" aria-hidden="true" />}>
                         Go Home
                     </SubmitButton>
                 </GoHomeButton>
