6530c7ea412b917dab38551e3ffbbe6c17121907
Aren <aren@bransfer.io>
2022-11-09 17:47:22 +0400

    refactor modal component, add slideover for mobile

--- components/modalComponent.tsx
+++ components/modalComponent.tsx
@@ -1,8 +1,9 @@
-import { FC } from 'react'
+import { FC, useEffect, useRef } from 'react'
 import { Dialog } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline';
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, useAnimation } from "framer-motion";
 import { useQueryState } from '../context/query';
+import { FocusTrap } from '@headlessui/react';
 
 type modalSize = 'small' | 'medium' | 'large';
 
@@ -34,6 +35,29 @@ function constructModalSize(size: modalSize) {
 
 const Modal: FC<ModalParams> = ({ onDismiss, isOpen, children, title, className, modalSize = 'large' }) => {
     const query = useQueryState()
+    const mobileModalRef = useRef(null);
+    const desktopModalRef = useRef(null);
+    const controls = useAnimation();
+    const transitionProps = { type: "spring", stiffness: 500, damping: 30 };
+    useEffect(() => {
+        controls.start({
+            y: 0,
+            transition: transitionProps,
+        });
+        console.log('plor')
+    }, []);
+
+    async function handleDragEnd(_, info) {
+        const offset = info.offset.y;
+        const velocity = info.velocity.y;
+        const height = mobileModalRef.current.getBoundingClientRect().height;
+        if (offset > height / 2 || velocity > 800) {
+            await controls.start({ y: "100%", transition: transitionProps });
+            onDismiss(true);
+        } else {
+            controls.start({ y: 0, transition: transitionProps });
+        }
+    }
 
     return (
         <AnimatePresence>
@@ -41,6 +65,7 @@ const Modal: FC<ModalParams> = ({ onDismiss, isOpen, children, title, className,
                 className={`${query?.addressSource} relative z-40`}
                 onClose={() => onDismiss(false)}
                 open={isOpen}>
+
                 <motion.div
                     initial={{ opacity: 0 }}
                     animate={{
@@ -51,7 +76,7 @@ const Modal: FC<ModalParams> = ({ onDismiss, isOpen, children, title, className,
                         opacity: 0,
                         transition: { duration: 0.3, ease: [0.36, 0.66, 0.04, 1] },
                     }}
-                    className="fixed inset-0 flex min-h-full items-center justify-center bg-black/40">
+                    className="fixed inset-0 min-h-full items-center justify-center bg-black/40 hidden md:flex">
                     <Dialog.Panel className={constructModalSize(modalSize)}>
                         <div className={`${className} space-y-4 bg-darkblue py-6 md:py-8 px-6 md:px-8 transform overflow-hidden rounded-md align-middle shadow-xl`}>
                             <Dialog.Title as="div" >
@@ -75,6 +100,26 @@ const Modal: FC<ModalParams> = ({ onDismiss, isOpen, children, title, className,
                             </div>
                         </div>
                     </Dialog.Panel>
+                </motion.div>
+                <motion.div
+                    key="mobile-modal"
+                    ref={mobileModalRef}
+                    className="group fixed inset-x-0 bottom-0 z-40 w-screen cursor-grab active:cursor-grabbing bg-darkblue-800 px-6 rounded-t-2xl shadow-lg border-t border-darkblue-100 pb-6 sm:hidden"
+                    initial={{ y: "100%" }}
+                    animate={controls}
+                    exit={{ y: "100%" }}
+                    transition={transitionProps}
+                    drag="y"
+                    dragDirectionLock
+                    onDragEnd={handleDragEnd}
+                    dragElastic={{ top: 0, bottom: 1 }}
+                    dragConstraints={{ top: 0, bottom: 0 }}
+                >
+                    <div className="h-7  rounded-t-4xl -mb-1 flex w-full items-center justify-center">
+                        <div className="-mr-1 h-1 w-6 rounded-full bg-darkblue-100 transition-all group-active:rotate-12" />
+                        <div className="h-1 w-6 rounded-full bg-darkblue-100 transition-all group-active:-rotate-12" />
+                    </div>
+                    {children}
                 </motion.div>
             </Dialog >}
         </AnimatePresence>
