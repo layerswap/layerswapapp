ee1536313eb0d5ab75f20557222438428a633640
Aren <aren@bransfer.io>
2022-11-10 15:01:55 +0400

    footer changes

--- components/introCard.tsx
+++ components/introCard.tsx
@@ -7,7 +7,7 @@ function IntroCard(props) {
                 <div>
                     <h1 className="text-xl font-light text-white">About Layerswap</h1>
                     <p className="text-base mt-4">
-                        Save up to 10x on fees when moving crypto from Coinbase, Binance or FTX to Arbitrum, zkSync, Loopring and other L2s.
+                        Save up to 10x on fees when moving crypto from Coinbase, Binance or FTX to Arbitrum, zkSync, Loopring and other L2s. <a target="_blank" className='underline decoration-text-primary-text hover:no-underline' href="https://docs.layerswap.io/user-docs/">Read more</a>
                     </p>
                     <p className="py-4">
                         <span className="text-sm font-semibold mr-2 rounded-md py-1 px-2 text-pink-primary border border-pink-primary uppercase">New</span>
