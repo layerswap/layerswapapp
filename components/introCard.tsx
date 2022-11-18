918e7095cc7ae4c06ea217fd0af336bbb5524f76
babgev <babgev@gmail.com>
2022-11-18 15:52:39 +0400

    Update intro card

--- components/introCard.tsx
+++ components/introCard.tsx
@@ -7,13 +7,15 @@ function IntroCard(props) {
                 <div>
                     <h1 className="text-xl font-light text-white">About Layerswap</h1>
                     <p className="text-base mt-4">
-                        Save up to 10x on fees when moving crypto from Coinbase, Binance or FTX to Arbitrum, zkSync, Loopring and other L2s.
+                        Move crypto from Coinbase, Binance or Kraken to zkSync, Loopring, StarkNet and other L2s.
                     </p>
                     <p className="py-4">
                         <span className="text-sm font-semibold mr-2 rounded-md py-1 px-2 text-pink-primary border border-pink-primary uppercase">New</span>
-
-                        <span className="inline">
-                            SNX (Synthetix) in Optimism L2 
+                        <span className="hidden md:inline">
+                            BNB Chain (BSC) 🤯, and a new feature<a target="_blank" className='underline text-slate-300 hover:no-underline px-1' href="https://twitter.com/layerswap/status/1593259469722423299?s=20&t=QSWDB8kg8z2-wywDemHk-w">Check the announcement</a>
+                        </span>
+                        <span className="inline md:hidden">
+                            <a target="_blank" className='underline text-slate-300 hover:no-underline px-1' href="https://twitter.com/layerswap/status/1593259469722423299?s=20&t=QSWDB8kg8z2-wywDemHk-w">BNB Chain (BSC)</a> 🤯
                         </span>
                     </p>
                 </div>
