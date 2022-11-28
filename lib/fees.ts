742f68e0d56708015e61dff2c0cae24356a8aaf4
babken <mesropyan.b@gmail.com>
2022-11-28 21:27:02 +0400

    Fixed MinimalAuthorizeAmount to round up

--- lib/fees.ts
+++ lib/fees.ts
@@ -9,7 +9,7 @@ export function GetExchangeFee(currency?: Currency, exchange?: Exchange): number
     return exchange?.currencies.find(ec => ec.asset === currency?.asset)?.current_withdrawal_fee || 0;
 }
 export function CalculateMinimalAuthorizeAmount(usd_price: number, amount: number) {
-    return Math.round((usd_price * amount) + (usd_price * amount * 0.02))
+    return Math.ceil((usd_price * amount) + (usd_price * amount * 0.02))
 }
 export function CalculateFee(amount?: number, currency?: Currency, exchange?: Exchange, network?: CryptoNetwork, swapType?: SwapType): number {
     if (!currency || !exchange || !network)
