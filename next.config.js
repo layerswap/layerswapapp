8c933963ac351a7c1895e1d8499fe725bd7d525a
babken <mesropyan.b@gmail.com>
2022-12-05 18:53:19 +0400

    Added redirects for new docs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const { PHASE_PRODUCTION_SERVER } = require('next/constants')
+const { PHASE_PRODUCTION_SERVER } = require('next/constants');
 
 const securityHeaders = [
   {
@@ -10,12 +10,71 @@ const securityHeaders = [
     value: 'frame-ancestors *.immutable.com'
   },
 ]
+const DOCS_URL = 'https://docs.layerswap.io/user-docs'
+const redirects = async () => [
+  {
+    source: '/blog/guide/Privacy_Policy',
+    destination: `${DOCS_URL}/information/privacy-policy`,
+    permanent: true,
+  },
+  {
+    source: '/blog/guide/Terms_of_Service',
+    destination: `${DOCS_URL}/information/terms-of-services`,
+    permanent: true,
+  },
+  {
+    source: '/blog/guide/How_to_transfer_crypto_from_Binance_to_L2',
+    destination: `${DOCS_URL}/your-first-swap/exchange-greater-than-network/transfer-from-binance`,
+    permanent: true,
+  },
+  {
+    source: '/blog/guide/How_to_transfer_crypto_from_Bitfinex_to_L2',
+    destination: `${DOCS_URL}/your-first-swap/exchange-greater-than-network/transfer-from-bitfinex`,
+    permanent: true,
+  },
+  {
+    source: '/blog/guide/How_to_transfer_crypto_from_BITTREX_GLOBAL_to_L2',
+    destination: `${DOCS_URL}/your-first-swap/exchange-greater-than-network/transfer-from-bittrex-global`,
+    permanent: true,
+  },
+  {
+    source: '/blog/guide/How_to_transfer_crypto_from_Coinbase_to_L2',
+    destination: `${DOCS_URL}/your-first-swap/exchange-greater-than-network/transfer-from-coinbase`,
+    permanent: true,
+  },
+  {
+    source: '/blog/guide/How_to_transfer_crypto_from_cryptocom_to_L2',
+    destination: `${DOCS_URL}/your-first-swap/exchange-greater-than-network/transfer-from-crypto.com`,
+    permanent: true,
+  },
+  {
+    source: '/blog/guide/How_to_transfer_crypto_from_Huobi_to_L2',
+    destination: `${DOCS_URL}/your-first-swap/exchange-greater-than-network/transfer-from-huobi-global`,
+    permanent: true,
+  },
+  {
+    source: '/blog/guide/How_to_transfer_crypto_from_Kraken_to_L2',
+    destination: `${DOCS_URL}/your-first-swap/exchange-greater-than-network/transfer-from-kraken`,
+    permanent: true,
+  },
+  {
+    source: '/blog/guide/How_to_transfer_crypto_from_KuCoin_to_L2',
+    destination: `${DOCS_URL}/your-first-swap/exchange-greater-than-network/transfer-from-kucoin`,
+    permanent: true,
+  },
+  {
+    source: '/blog/guide/How_to_transfer_crypto_from_Okex_to_L2',
+    destination: `${DOCS_URL}/your-first-swap/exchange-greater-than-network/transfer-from-okx`,
+    permanent: true,
+  },
+]
 
 module.exports = (phase, { defaultConfig }) => {
   /**
    * @type {import('next').NextConfig}
    */
   const nextConfig = {
+    redirects,
     i18n: {
       locales: ["en"],
       defaultLocale: "en",
