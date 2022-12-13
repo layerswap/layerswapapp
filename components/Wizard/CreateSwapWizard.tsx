0592f50da9d43b3d7ac3cbfe2749c0e52da4135d
Aren <aren@bransfer.io>
2022-12-13 15:48:08 +0400

    2nd try

--- components/Wizard/CreateSwapWizard.tsx
+++ components/Wizard/CreateSwapWizard.tsx
@@ -25,7 +25,7 @@ const CreateSwap: FC = () => {
     const GoBackToMainStep = useCallback(() => goToStep(SwapCreateStep.MainForm, "back"), [])
     const GoBackToConfirmStep = useCallback(() => goToStep(SwapCreateStep.Confirm, "back"), [])
     const GoBackToEmailStep = useCallback(() => goToStep(SwapCreateStep.Email, "back"), [])
-    const GoBackFromError = useCallback(() => goToStep(error.Step, "back"), [])
+    const GoBackFromError = useCallback(() => goToStep(error?.Step, "back"), [])
 
     return (
         <TimerProvider>
