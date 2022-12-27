bff3549de1a6e4436b9676df01cd786d727c0df6
Aren <aren@bransfer.io>
2022-12-27 19:11:10 +0400

    move Get Help button to wizard header

--- components/LayerswapMenu/index.tsx
+++ components/LayerswapMenu/index.tsx
@@ -1,14 +1,11 @@
 import { Menu } from "@headlessui/react";
 import { BookOpenIcon, ExternalLinkIcon, MenuIcon } from "@heroicons/react/outline";
-import { HomeIcon, LightBulbIcon, LinkIcon, LoginIcon, LogoutIcon, PaperAirplaneIcon, SupportIcon, TableIcon, UserIcon } from "@heroicons/react/solid";
+import { HomeIcon, LoginIcon, LogoutIcon, TableIcon, UserIcon } from "@heroicons/react/solid";
 import { useRouter } from "next/router";
 import { useCallback, useState } from "react";
-import { useIntercom } from "react-use-intercom";
 import { useAuthState } from "../../context/authContext";
 import { useMenuState } from "../../context/menu";
 import TokenService from "../../lib/TokenService";
-import SendFeedback from '../sendFeedback'
-import SlideOver from "../SlideOver";
 import { AnimatePresence, motion } from "framer-motion";
 import Item, { ItemType } from "./MenuItem";
 import { shortenEmail } from "../utils/ShortenAddress";
@@ -16,11 +13,9 @@ import Modal from "../modalComponent";
 import { DocIframe } from "../docInIframe";
 
 export default function () {
-    const { email, authData, userId } = useAuthState()
+    const { email, authData } = useAuthState()
     const router = useRouter();
     const { menuVisible } = useMenuState()
-    const { boot, show, update } = useIntercom()
-    const updateWithProps = () => update({ email: email, userId: userId })
 
     const [modalUrl, setModalUrl] = useState<string>(null);
     const handleSetUrl = (url: string) => setModalUrl(url)
@@ -47,7 +42,7 @@ export default function () {
                 <Menu as="div" className={`relative inline-block text-left ${menuVisible ? 'visible' : 'invisible'}`}>
                     {({ open }) => (
                         <>
-                            <div className="relative top-1">
+                            <div className="relative top-[3px]">
                                 <Menu.Button className="w-full rounded-md shadow-sm text-sm font-medium">
                                     <MenuIcon className='h-7 w-7 cursor-pointer' />
                                 </Menu.Button>
@@ -91,16 +86,6 @@ export default function () {
                                                                 Login
                                                             </Item>
                                                         </Menu.Item>
-                                                        <Menu.Item>
-                                                            <Item type={ItemType.button} icon={<LightBulbIcon className='h-4 w-4' />}
-                                                                onClick={() => {
-                                                                    boot();
-                                                                    show();
-                                                                    updateWithProps()
-                                                                }} >
-                                                                Get Help
-                                                            </Item>
-                                                        </Menu.Item>
                                                     </>
                                             }
                                             {
@@ -125,16 +110,6 @@ export default function () {
                                                         </Item>
                                                     </Menu.Item> */}
                                                     <hr className="horizontal-gradient" />
-                                                    <Menu.Item>
-                                                        <Item type={ItemType.button} icon={<LightBulbIcon className='h-4 w-4' />}
-                                                            onClick={() => {
-                                                                boot();
-                                                                show();
-                                                                updateWithProps()
-                                                            }} >
-                                                            Get Help
-                                                        </Item>
-                                                    </Menu.Item>
                                                     <Menu.Item>
                                                         <Item type={ItemType.button} onClick={() => handleSetUrl("https://docs.layerswap.io/")} icon={<BookOpenIcon className='h-4 w-4' />}>
                                                             User Docs
