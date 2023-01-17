33a50fee4ae38d1031509ce004b06b6dceb6eed5
babken <mesropyan.b@gmail.com>
2023-01-17 16:06:35 +0400

    Fixed optional network

--- helpers/settingsHelper.ts
+++ helpers/settingsHelper.ts
@@ -5,7 +5,7 @@ export function mapNetworkCurrencies(exchanges: Exchange[], networks: CryptoNetw
 
     return exchanges.map(e => {
         const currencies = e.currencies.map(ec => {
-            const network = networks.find(n => n.internal_name.toLowerCase() === ec.network.toLowerCase())
+            const network = networks.find(n => n.internal_name.toLowerCase() === ec.network?.toLowerCase())
             const networkCurrency = network?.currencies?.find(nc => nc.asset.toLowerCase() === ec.asset.toLowerCase())
             return { ...ec, ...networkCurrency }
         })
