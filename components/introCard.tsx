8b9844a08ae5dc58e65442b49cb4bdea340df605
Aren Tantushyan <101871962+arentant@users.noreply.github.com>
2023-02-03 14:34:01 +0400

    fix rhinofi api url and update information card (#380)

--- components/introCard.tsx
+++ components/introCard.tsx
@@ -14,9 +14,9 @@ function IntroCard(props) {
                     <p className="text-base mt-2">
                         Move crypto from Coinbase, Binance or Kraken to zkSync, Loopring, StarkNet and other L2s. <a className='underline hover:no-underline cursor-pointer plausible-event-name=Read+more' href='https://docs.layerswap.io/user-docs/' target='_blank'>Read more</a>
                     </p>
-                    <a target="_blank" href="https://twitter.com/layerswap/status/1593259469722423299?s=20&t=QSWDB8kg8z2-wywDemHk-w" className="mt-3 inline-flex group items-center rounded-lg border border-darkblue-400  bg-darkblue-600 p-1 pr-2 text-white sm:text-base lg:text-sm xl:text-base">
+                    <a target="_blank" href="https://twitter.com/layerswap/status/1621194164586123269" className="mt-3 inline-flex group items-center rounded-lg border border-darkblue-400  bg-darkblue-600 p-1 pr-2 text-white sm:text-base lg:text-sm xl:text-base">
                         <span className="rounded-md bg-primary px-3 py-0.5 text-sm font-semibold leading-5 text-white"> New </span>
-                        <span className="ml-4 text-sm font-medium">BNB Chain (BSC) 🤯 <span className='hidden sm:inline'>and a new feature.</span></span>
+                        <span className="ml-4 md:text-sm text-xs font-medium">Cross-Chain transfers are live! 🔥</span>
                         <ChevronRightIcon className="ml-2 h-5 w-5 text-primary-text group-hover:text-primary-500" aria-hidden="true" />
                     </a>
                 </div>
