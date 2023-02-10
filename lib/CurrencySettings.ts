5698485920668a405951d7617459ce237a77cbef
Aren <aren@bransfer.io>
2023-02-10 17:35:50 +0400

    add sorting for currencies

---
+++ lib/CurrencySettings.ts
@@ -0,0 +1,35 @@
+import KnownInternalNames from "./knownIds";
+
+export default class CurrencySettings {
+    Order?: number;
+
+    public static ForceDisable?: { [network: string]: { offramp: boolean, onramp: boolean, crossChain: boolean } }
+    public static KnownSettings: { [network: string]: CurrencySettings } = {};
+
+    private static _isInitialized = false;
+    public static Initialize() {
+        if (CurrencySettings._isInitialized) {
+            return;
+        }
+
+        CurrencySettings._isInitialized = true;
+        CurrencySettings.ForceDisable = JSON.parse(process.env.NEXT_PUBLIC_NETWORK_FORCE_SETTINGS || "{}")
+
+
+        CurrencySettings.KnownSettings[KnownInternalNames.Currencies.LRC] = {
+            Order: 0,
+        };
+        CurrencySettings.KnownSettings[KnownInternalNames.Currencies.ETH] = {
+            Order: 1,
+        };
+        CurrencySettings.KnownSettings[KnownInternalNames.Currencies.USDC] = {
+            Order: 2,
+        };
+        CurrencySettings.KnownSettings[KnownInternalNames.Currencies.USDT] = {
+            Order: 3,
+        };
+        
+    }
+}
+
+CurrencySettings.Initialize();
