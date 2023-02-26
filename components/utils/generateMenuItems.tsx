9bf875d7b12da0c9429c84e54d4b4b9025cad76d
Aren <aren@bransfer.io>
2023-02-26 16:28:11 +0400

    add lockExchange query parameter

--- components/utils/generateMenuItems.tsx
+++ components/utils/generateMenuItems.tsx
@@ -97,7 +97,8 @@ type ExchangeMenuItemsProps = {
     resource_storage_url: string
     values: SwapFormValues,
     networks: CryptoNetwork[],
-    sourceExchangeName: string
+    sourceExchangeName: string,
+    lockExchange: boolean
 }
 
 const exchangeCurrencyIsAvailableForNetwork = ((ec: ExchangeCurrency & NetworkCurrency, network: CryptoNetwork, exchange: Exchange, swapType: SwapType) => {
@@ -107,7 +108,7 @@ const exchangeCurrencyIsAvailableForNetwork = ((ec: ExchangeCurrency & NetworkCu
         && !exchange.currencies.filter(c => c.asset === ec.asset && c.is_default).some(c => c.network === network.internal_name))
 })
 
-export const generateExchangeMenuItems = ({ exchanges, networks, values, resource_storage_url, sourceExchangeName }: ExchangeMenuItemsProps): SelectMenuItem<Exchange>[] => {
+export const generateExchangeMenuItems = ({ exchanges, networks, values, resource_storage_url, sourceExchangeName, lockExchange }: ExchangeMenuItemsProps): SelectMenuItem<Exchange>[] => {
     const { swapType, from, to } = values
     const isSourceExchangeAvailable = exchanges.some(e => e?.internal_name?.toLowerCase() === sourceExchangeName?.toLowerCase() && e.status === "active" && (swapType === SwapType.OffRamp ?
         e.currencies.some(ec => (from ? exchangeCurrencyIsAvailableForNetwork(ec, from.baseObject, e, swapType) : networks.some(n => exchangeCurrencyIsAvailableForNetwork(ec, n, e, swapType))))
@@ -126,7 +127,7 @@ export const generateExchangeMenuItems = ({ exchanges, networks, values, resourc
             name: e.display_name,
             order: ExchangeSettings.KnownSettings[e.internal_name]?.Order,
             imgSrc: `${resource_storage_url}/layerswap/networks/${e.internal_name.toLowerCase()}.png`,
-            isAvailable: !isSourceExchangeAvailable,
+            isAvailable: e.status === "active" && (swapType === SwapType.OnRamp ? !lockExchange : !isSourceExchangeAvailable),
             isDefault: false
         })).sort(SortingByOrder);
 
