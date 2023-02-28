5c7fab45cb2528746d8a1bba9ed8a59250937e60
babken <mesropyan.b@gmail.com>
2023-02-28 16:50:55 +0400

    Moved query data to session storage

--- hooks/usePersistedState.ts
+++ hooks/usePersistedState.ts
@@ -5,13 +5,13 @@ type PersistedState<T> = [T, Dispatch<SetStateAction<T>>];
 
 function usePersistedState<T>(defaultValue: T, key: string, type: storageType = 'localStorage'): PersistedState<T> {
   const [value, setValue] = useState<T>(() => {
-    const value = checkStorageIsAvailable(type) && window.localStorage.getItem(key);
+    const value = checkStorageIsAvailable(type) && window[type]?.getItem(key);
 
     return value ? (JSON.parse(value || "null") as T) : defaultValue;
   });
 
   useEffect(() => {
-    checkStorageIsAvailable(type) && window.localStorage.setItem(key, JSON.stringify(value));
+    checkStorageIsAvailable(type) && window[type]?.setItem(key, JSON.stringify(value));
   }, [key, value]);
 
   return [value, setValue];
