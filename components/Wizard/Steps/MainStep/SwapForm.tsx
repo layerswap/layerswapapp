35c61434ff83d5d5140544c7a2a668df9481f907
babken <mesropyan.b@gmail.com>
2023-03-01 15:40:28 +0400

    Fixed offramp destination address truncate

--- components/Wizard/Steps/MainStep/SwapForm.tsx
+++ components/Wizard/Steps/MainStep/SwapForm.tsx
@@ -177,41 +177,27 @@ const SwapForm: FC<Props> = ({ partner, isPartnerWallet, resource_storage_url, l
                         <div className="mb-6 leading-4">
                             <AmountField />
                         </div>
-                        {
-                            values.swapType === SwapType.OffRamp ?
-                                <div className="w-full mb-3.5 leading-4">
-                                    <label htmlFor="destination_address" className="block font-normal text-primary-text text-sm">
-                                        {`To ${values?.to?.name || ''} address`}
-                                    </label>
-                                    <div className="relative rounded-md shadow-sm mt-1.5">
-                                        <div onClick={handleOpenAddressModal} className="flex rounded-lg items-center cursor-pointer shadow-sm mt-1.5 bg-darkblue-700 border-darkblue-500 border disabled:cursor-not-allowed h-12 leading-4 focus:ring-primary focus:border-primary font-semibold w-full placeholder-gray-400 truncate px-3.5 py-3">
-                                            {values.destination_address || (NetworkSettings.KnownSettings[values?.to?.baseObject?.internal_name]?.AddressPlaceholder ?? "0x123...ab56c")}
-                                        </div>
-                                    </div>
-                                </div>
-                                :
-                                <div className="w-full mb-3.5 leading-4">
-                                    <label htmlFor="destination_address" className="block font-normal text-primary-text text-sm">
-                                        {`To ${values?.to?.name || ''} address`}
-                                    </label>
-                                    <div onClick={handleOpenAddressModal} className="flex rounded-lg space-x-3 items-center cursor-pointer shadow-sm mt-1.5 bg-darkblue-700 border-darkblue-500 border disabled:cursor-not-allowed h-12 leading-4 focus:ring-primary focus:border-primary font-semibold w-full placeholder-gray-400 px-3.5 py-3">
-                                        {isPartnerWallet &&
-                                            <div className="shrink-0 flex items-center pointer-events-none">
-                                                {
-                                                    partnerImage &&
-                                                    <Image alt="Partner logo" className='rounded-md object-contain' src={partnerImage} width="24" height="24"></Image>
-                                                }
-                                            </div>
+                        <div className="w-full mb-3.5 leading-4">
+                            <label htmlFor="destination_address" className="block font-normal text-primary-text text-sm">
+                                {`To ${values?.to?.name || ''} address`}
+                            </label>
+                            <div onClick={handleOpenAddressModal} className="flex rounded-lg space-x-3 items-center cursor-pointer shadow-sm mt-1.5 bg-darkblue-700 border-darkblue-500 border disabled:cursor-not-allowed h-12 leading-4 focus:ring-primary focus:border-primary font-semibold w-full placeholder-gray-400 px-3.5 py-3">
+                                {isPartnerWallet && values.swapType !== SwapType.OffRamp &&
+                                    <div className="shrink-0 flex items-center pointer-events-none">
+                                        {
+                                            partnerImage &&
+                                            <Image alt="Partner logo" className='rounded-md object-contain' src={partnerImage} width="24" height="24"></Image>
                                         }
-                                        <div className="truncate">
-                                            {values.destination_address ?
-                                                <TruncatedAdrress address={values.destination_address} />
-                                                :
-                                                (NetworkSettings.KnownSettings[values?.to?.baseObject?.internal_name]?.AddressPlaceholder ?? "0x123...ab56c")}
-                                        </div>
                                     </div>
+                                }
+                                <div className="truncate">
+                                    {values.destination_address ?
+                                        <TruncatedAdrress address={values.destination_address} />
+                                        :
+                                        (NetworkSettings.KnownSettings[values?.to?.baseObject?.internal_name]?.AddressPlaceholder ?? "0x123...ab56c")}
                                 </div>
-                        }
+                            </div>
+                        </div>
                         <div className="w-full">
                             <AmountAndFeeDetails values={values} />
                         </div>
