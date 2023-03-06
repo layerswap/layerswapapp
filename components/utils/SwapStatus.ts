a1f9ee2cbaae37676b910e58e03fbcfcc16a90c9
babken <mesropyan.b@gmail.com>
2023-03-06 21:39:40 +0400

    Refactored GetSwapStatusStep

--- components/utils/SwapStatus.ts
+++ components/utils/SwapStatus.ts
@@ -4,11 +4,8 @@ import { SwapWithdrawalStep } from "../../Models/Wizard";
 
 export const GetSwapStatusStep = (swap: SwapItem): SwapWithdrawalStep => {
     const swapStatus = swap?.status;
-    if (swapStatus == SwapStatus.UserTransferPending && swap.has_pending_deposit && !swap.input_transaction)
-        return SwapWithdrawalStep.SwapProcessing
-    else if (swapStatus == SwapStatus.UserTransferPending && swap.input_transaction)
-        return SwapWithdrawalStep.SwapProcessing
-    else if (swapStatus == SwapStatus.LsTransferPending)
+    if ((swapStatus == SwapStatus.UserTransferPending && (swap.has_pending_deposit || swap.input_transaction))
+        || (swapStatus == SwapStatus.LsTransferPending))
         return SwapWithdrawalStep.SwapProcessing
     else if (swapStatus == SwapStatus.UserTransferPending)
         return swap.source_exchange ? SwapWithdrawalStep.Withdrawal : SwapWithdrawalStep.OffRampWithdrawal
