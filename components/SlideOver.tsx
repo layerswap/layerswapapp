92a6888f13e98981c750612df11ad6f7ebfbcd24
Aren Tantushyan <101871962+arentant@users.noreply.github.com>
2023-03-23 20:41:48 +0400

    fix modal (#447)
    * fix modal and slideover

--- components/SlideOver.tsx
+++ components/SlideOver.tsx
@@ -5,7 +5,7 @@ import { FC, useState } from "react"
 import { MobileModalContent, modalHeight } from "./modalComponent";
 import useWindowDimensions from "../hooks/useWindowDimensions";
 import IconButton from "./buttons/iconButton";
-import { Root, Portal, Overlay, Content, } from '@radix-ui/react-dialog';
+import { ReactPortal } from "./Wizard/Widget";
 
 export type slideOverPlace = 'inStep' | 'inModal' | 'inMenu'
 
@@ -27,7 +27,7 @@ const SlideOver: FC<Props> = (({ header, opener, modalHeight, imperativeOpener, 
     const { width } = useWindowDimensions()
     const isMobile = width < 640
 
-    if (open && isMobile) {
+    if (open) {
         document.body.style.overflow = 'hidden'
     }
     else {
@@ -103,23 +103,17 @@ const SlideOver: FC<Props> = (({ header, opener, modalHeight, imperativeOpener, 
                             <div className='text-primary-text relative items-center justify-center text-center h-full overflow-y-auto styled-scroll'>
                                 {children && children(handleClose, openAnimaionCompleted)}
                             </div>
-                            <div id="test" />
                         </div>
                     </motion.div>
                 }
             </AnimatePresence>
             <AnimatePresence>
                 {open && isMobile &&
-                    <Root open={open} onOpenChange={() => { }} >
-                        <Portal>
-                            <Overlay />
-                            <Content>
-                                <MobileModalContent modalHeight={modalHeight} ref={mobileModalRef} showModal={open} setShowModal={setOpen} title={header} className={moreClassNames}>
-                                    {children && children(handleClose)}
-                                </MobileModalContent>
-                            </Content>
-                        </Portal>
-                    </Root>
+                    <ReactPortal>
+                        <MobileModalContent modalHeight={modalHeight} ref={mobileModalRef} showModal={open} setShowModal={setOpen} title={header} className={moreClassNames}>
+                            {children && children(handleClose)}
+                        </MobileModalContent>
+                    </ReactPortal>
                 }
             </AnimatePresence>
         </>
