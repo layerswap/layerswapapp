f6c94515f3e6e4dd4e51c8db3d373e6ffc337f76
babgev <babgev@gmail.com>
2023-04-12 22:00:05 +0400

    Add arb nova

--- lib/chainConfigs.ts
+++ lib/chainConfigs.ts
@@ -25,5 +25,38 @@ export const kcc = {
     },
 }
 
+export const arbitrumNova = {
+    id: 42_170,
+    name: 'Arbitrum Nova',
+    network: 'arbitrum-nova',
+    nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+    rpcUrls: {
+      blast: {
+        http: ['https://arbitrum-nova.public.blastapi.io'],
+        webSocket: ['wss://arbitrum-nova.public.blastapi.io'],
+      },
+      default: {
+        http: ['https://nova.arbitrum.io/rpc'],
+      },
+      public: {
+        http: ['https://nova.arbitrum.io/rpc'],
+      },
+    },
+    blockExplorers: {
+      etherscan: { name: 'Arbiscan', url: 'https://nova.arbiscan.io' },
+      blockScout: {
+        name: 'BlockScout',
+        url: 'https://nova-explorer.arbitrum.io/',
+      },
+      default: { name: 'Arbiscan', url: 'https://nova.arbiscan.io' },
+    },
+    contracts: {
+      multicall3: {
+        address: '0xca11bde05977b3631167028862be2a173976ca11',
+        blockCreated: 1746963,
+      },
+    },
+}
+
 export var supportedChains =
-    [arbitrum, arbitrumGoerli, bsc, bscTestnet, goerli, mainnet, okc, kcc, optimism, optimismGoerli, polygon, polygonMumbai, polygonZkEvmTestnet, polygonZkEvm, sepolia, zkSync, zkSyncTestnet];
+    [arbitrum, arbitrumGoerli, arbitrumNova, bsc, bscTestnet, goerli, mainnet, okc, kcc, optimism, optimismGoerli, polygon, polygonMumbai, polygonZkEvmTestnet, polygonZkEvm, sepolia, zkSync, zkSyncTestnet];
