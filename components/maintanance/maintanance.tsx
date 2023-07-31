98513ae982951bbc0421a74f00030f59adfdbfe7
Babken Mesropyan <babken@topsoft.am>
2023-07-31 14:02:21 +0400

    Maintanance

--- components/maintanance/maintanance.tsx
+++ components/maintanance/maintanance.tsx
@@ -28,7 +28,7 @@ function MaintananceContent(props) {
                             We're upgrading our systems and infrastructure to give you the best experience yet.
                         </p>
                         <span className="block xl:inline">We'll be back</span>{' '}
-                        <span className="block text-primary xl:inline">in 1 hour <span className="block xl:inline text-xl">(19:30 UTC)</span></span>
+                        <span className="block text-primary xl:inline">in 1.5 hour <span className="block xl:inline text-xl">(11:30 UTC)</span></span>
                         <p className="mt-4 text-primary-text">
                             Any pending swap will be completed after maintanance.
                         </p>
