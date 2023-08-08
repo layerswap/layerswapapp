f7295b998b9e4bd6935a7c071d21a24b4c900585
Aren <aren@bransfer.io>
2023-08-08 16:31:52 +0400

    add null checks

--- components/Input/NetworkFormField.tsx
+++ components/Input/NetworkFormField.tsx
@@ -47,18 +47,18 @@ const NetworkFormField = forwardRef(({ direction, label }: Props, ref: any) => {
         menuItems = GenerateMenuItems(filteredLayers, resolveImgSrc, direction, from && lockFrom);
         valueGrouper = (values: ISelectMenuItem[]) => {
             let groups: SelectMenuItemGroup[] = groupByType(values);
-            const indexOfLinea = groups?.find(g => g?.name === 'Networks')?.items.indexOf(values.find(i => i.id === KnownInternalNames.Networks.LineaMainnet))
+            const indexOfLinea = groups?.find(g => g?.name === 'Networks')?.items?.indexOf(values?.find(i => i?.id === KnownInternalNames.Networks.LineaMainnet))
             let newGroup = new SelectMenuItemGroup({
                 name: "New",
                 items: [
-                    ...groups?.find(g => g?.name === 'Networks')?.items.splice(indexOfLinea, 1),
+                    ...groups?.find(g => g?.name === 'Networks')?.items?.splice(indexOfLinea, 1),
                 ]
             })
             let popularsGroup = new SelectMenuItemGroup({
                 name: "Popular",
                 items: [
-                    ...groups?.find(g => g?.name === 'Networks')?.items.splice(0, 2),
-                    ...(groups?.find(g => g?.name === 'Exchanges')?.items.splice(0, 2) || [])
+                    ...groups?.find(g => g?.name === 'Networks')?.items?.splice(0, 2),
+                    ...(groups?.find(g => g?.name === 'Exchanges')?.items?.splice(0, 2) || [])
                 ]
             })
             groups.unshift(popularsGroup, newGroup);
@@ -72,15 +72,15 @@ const NetworkFormField = forwardRef(({ direction, label }: Props, ref: any) => {
         menuItems = GenerateMenuItems(filteredLayers, resolveImgSrc, direction, to && lockTo);
         valueGrouper = (values: ISelectMenuItem[]) => {
             let groups: SelectMenuItemGroup[] = groupByType(values);
-            const indexOfBase = groups?.find(g => g?.name === 'Networks')?.items.indexOf(values.find(i => i.id === KnownInternalNames.Networks.BaseMainnet))
-            const indexOfMantle = groups?.find(g => g?.name === 'Networks')?.items.indexOf(values.find(i => i.id === KnownInternalNames.Networks.MantleMainnet))
-            const indexOfPGN = groups?.find(g => g?.name === 'Networks')?.items.indexOf(values.find(i => i.id === KnownInternalNames.Networks.PGNMainnet))
+            const indexOfBase = groups?.find(g => g?.name === 'Networks')?.items?.indexOf(values?.find(i => i?.id === KnownInternalNames.Networks.BaseMainnet))
+            const indexOfMantle = groups?.find(g => g?.name === 'Networks')?.items?.indexOf(values?.find(i => i?.id === KnownInternalNames.Networks.MantleMainnet))
+            const indexOfPGN = groups?.find(g => g?.name === 'Networks')?.items?.indexOf(values?.find(i => i?.id === KnownInternalNames.Networks.PGNMainnet))
             let newGroup = new SelectMenuItemGroup({
                 name: "New",
                 items: [
-                    ...groups?.find(g => g?.name === 'Networks')?.items.splice(indexOfBase, 1),
-                    ...groups?.find(g => g?.name === 'Networks')?.items.splice(indexOfPGN, 1),
-                    ...groups?.find(g => g?.name === 'Networks')?.items.splice(indexOfMantle, 1),
+                    ...groups?.find(g => g?.name === 'Networks')?.items?.splice(indexOfBase, 1),
+                    ...groups?.find(g => g?.name === 'Networks')?.items?.splice(indexOfPGN, 1),
+                    ...groups?.find(g => g?.name === 'Networks')?.items?.splice(indexOfMantle, 1),
                 ]
             })
             let popularsGroup = new SelectMenuItemGroup({
