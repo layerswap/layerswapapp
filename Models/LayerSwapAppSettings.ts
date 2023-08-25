f79a6333554121e523c363e8160ed2c8c1e08150
proy13 <paruyr@bransfer.io>
2023-08-25 20:20:11 +0400

    fixed images

--- Models/LayerSwapAppSettings.ts
+++ Models/LayerSwapAppSettings.ts
@@ -22,13 +22,13 @@ export class LayerSwapAppSettings extends LayerSwapSettings {
         }
         // Shitty way to check for partner
         else if ((item as Partner).is_wallet != undefined) {
-            return `${this.discovery.resource_storage_url}/layerswap/partners/${(item as Partner)?.organization_name?.toLowerCase()}.png`
+            return `${this.discovery.resource_storage_url}layerswap/partners/${(item as Partner)?.organization_name?.toLowerCase()}.png`
         }
         else if ((item as any)?.internal_name != undefined) {
-            return `${this.discovery.resource_storage_url}/layerswap/networks/${(item as any)?.internal_name?.toLowerCase()}.png`;
+            return `${this.discovery.resource_storage_url}layerswap/networks/${(item as any)?.internal_name?.toLowerCase()}.png`;
         }
         else if ((item as any)?.asset != undefined) {
-            return `${this.discovery.resource_storage_url}/layerswap/currencies/${(item as any)?.asset?.toLowerCase()}.png`;
+            return `${this.discovery.resource_storage_url}layerswap/currencies/${(item as any)?.asset?.toLowerCase()}.png`;
         }
     }
 
