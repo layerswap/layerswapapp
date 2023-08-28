99530ce937fa563ee487afa372f7aca5ee365fe3
babkengevorgyan <babken@bransfer.io>
2023-08-28 17:16:58 +0400

    Add children to props

--- components/ContactSupport.tsx
+++ components/ContactSupport.tsx
@@ -2,7 +2,7 @@ import { FC } from "react"
 import { useIntercom } from "react-use-intercom"
 import { useAuthState } from "../context/authContext"
 
-const ContactSupport: FC<{}> = ({ children }) => {
+const ContactSupport: FC<{ children?: React.ReactNode }> = ({ children }) => {
     const { email, userId } = useAuthState()
     const { boot, show, update } = useIntercom()
     const updateWithProps = () => update({ email: email, userId: userId })
