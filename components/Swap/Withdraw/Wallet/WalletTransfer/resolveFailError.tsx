893d62f519183fdbdcf1e8deb86d12535145efc5
proy13 <paruyr@bransfer.io>
2023-09-14 12:02:41 +0400

    changed inconsistent enum name

--- components/Swap/Withdraw/Wallet/WalletTransfer/resolveFailError.tsx
+++ components/Swap/Withdraw/Wallet/WalletTransfer/resolveFailError.tsx
@@ -4,7 +4,7 @@ const resolveFailError = (error: SwapFailReasons): string => {
     switch (error) {
       case SwapFailReasons.RECEIVED_LESS_THAN_VALID_RANGE:
         return "Received less than the valid range.";
-      case SwapFailReasons.RECEIVED_MORE_THAN_Valid_RANGE:
+      case SwapFailReasons.RECEIVED_MORE_THAN_VALID_RANGE:
         return "Received more than the valid range.";
       default:
         return "Unknown error";
