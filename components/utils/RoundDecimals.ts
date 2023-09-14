292e3202cd8f8541a862a781ff8a161f090d9f9b
babken <mesropyan.b@gmail.com>
2023-09-14 21:40:45 +0400

    Fixed truncate

--- components/utils/RoundDecimals.ts
+++ components/utils/RoundDecimals.ts
@@ -5,12 +5,6 @@ export function roundDecimals(value: number, decimals: number) {
     return Number(Math.ceil(Number(value + 'e' + decimals)) + 'e-' + decimals);
 }
 
-export function truncateDecimals(value: number, decimals: number): string {
-    if (!decimals)
-        return '0'
-    if (Math.pow(10, decimals) * value >= 1) {
-        return Number(value).toFixed(decimals)
-    }
-    else if (decimals >= 18) return '0'
-    else return truncateDecimals(value, ++decimals);
+export function truncateDecimals(value: number, decimals: number) {
+    return Number(value?.toFixed(decimals));
 }
