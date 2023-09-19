c1a49693912b16f4266066788dd809c1a43329ab
Aren <aren@bransfer.io>
2023-09-19 17:31:01 +0400

    add BaseFeeMultiplier for some networks in NetworkSettings

--- lib/NetworkSettings.ts
+++ lib/NetworkSettings.ts
@@ -115,6 +115,7 @@ export default class NetworkSettings {
         };
         NetworkSettings.KnownSettings[KnownInternalNames.Networks.PolygonMainnet] = {
             ChainId: 137,
+            BaseFeeMultiplier: 1.01
         };
         NetworkSettings.KnownSettings[KnownInternalNames.Networks.ArbitrumMainnet] = {
             ChainId: 42161,
@@ -125,7 +126,8 @@ export default class NetworkSettings {
         NetworkSettings.KnownSettings[KnownInternalNames.Networks.ArbitrumNova] = {
             ChainId: 42170,
             AccountExplorerTemplate: 'https://nova.arbiscan.io/address/{0}',
-            DefaultPriorityFee: 0
+            DefaultPriorityFee: 0,
+            BaseFeeMultiplier: 1.35
         };
         NetworkSettings.KnownSettings[KnownInternalNames.Networks.ArbitrumGoerli] = {
             ChainId: 421613,
@@ -229,7 +231,8 @@ export default class NetworkSettings {
         };
         NetworkSettings.KnownSettings[KnownInternalNames.Networks.AvalancheMainnet] = {
             ChainId: 43114,
-            DefaultPriorityFee: 1.5
+            DefaultPriorityFee: 1.5,
+            BaseFeeMultiplier: 1.35
         };
         NetworkSettings.KnownSettings[KnownInternalNames.Networks.PolygonZkMainnet] = {
             ChainId: 1101,
