3f5add13d52cdf7bdb17bf2f5bf4742a766c906f
proy13 <paruyr@bransfer.io>
2023-09-26 16:13:52 +0400

    updated path-scurry

--- components/Swap/Withdraw/Wallet/StarknetWalletWithdraw.tsx
+++ components/Swap/Withdraw/Wallet/StarknetWalletWithdraw.tsx
@@ -24,10 +24,8 @@ type Props = {
 function getUint256CalldataFromBN(bn: number.BigNumberish) {
     return { type: "struct" as const, ...uint256.bnToUint256(bn) }
 }
-export function parseInputAmountToUint256(
-    input: string,
-    decimals: number = 18,
-) {
+
+export function parseInputAmountToUint256(input: string,decimals: number = 18) {
     return getUint256CalldataFromBN(parseUnits(input, decimals).toString())
 }
 
