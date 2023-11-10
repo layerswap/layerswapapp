ebca1369ff2c9f661f34f10173d89cc08a693ffe
Aren <aren@bransfer.io>
2023-11-10 09:08:44 +0400

    provide chain id to starknet connect in withdrawal

--- lib/wallets/starknet/useStarknet.ts
+++ lib/wallets/starknet/useStarknet.ts
@@ -21,7 +21,7 @@ export default function useStarknet(): WalletProvider {
 
     const connectWallet = useCallback(async (chain: string) => {
         const constants = (await import('starknet')).constants
-        const chainId = (chain && fromHex(chain as `0x${string}`, 'string')) ?? constants.NetworkName.SN_MAIN
+        const chainId = (chain && fromHex(chain as `0x${string}`, 'string')) || constants.NetworkName.SN_MAIN
         const connect = (await import('starknetkit')).connect
         try {
             const res = await connect({
