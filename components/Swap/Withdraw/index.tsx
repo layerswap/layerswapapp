b7d70ec0357540d8ef8b3566db7b6d39d050b4ec
Aren <aren@bransfer.io>
2023-12-08 13:51:00 +0400

    fix withdraw footer in modal

--- components/Swap/Withdraw/index.tsx
+++ components/Swap/Withdraw/index.tsx
@@ -158,7 +158,7 @@ const Withdraw: FC = () => {
             </Widget.Content>
             {
                 activeTab?.footer &&
-                <Widget.Footer>
+                <Widget.Footer sticky={false}>
                     {activeTab?.footer}
                 </Widget.Footer>
             }
