8590121de2c28b894f6ffabdfb073f7f4ecba966
Paruyr <paruyryeprikyan9@gmail.com>
2023-12-13 17:12:39 +0400

    fixed can be swapped

--- components/Swap/Form/Form.tsx
+++ components/Swap/Form/Form.tsx
@@ -20,7 +20,7 @@ import ToggleButton from "../../buttons/toggleButton";
 import { ArrowUpDown, Fuel } from 'lucide-react'
 import { useAuthState } from "../../../context/authContext";
 import WarningMessage from "../../WarningMessage";
-import { FilterDestinationLayers, FilterSourceLayers, GetDefaultAsset } from "../../../helpers/settingsHelper";
+import { GetDefaultAsset } from "../../../helpers/settingsHelper";
 import KnownInternalNames from "../../../lib/knownIds";
 import { Widget } from "../../Widget/Index";
 import { classNames } from "../../utils/classNames";
@@ -57,10 +57,8 @@ const SwapForm: FC<Props> = ({ partner, isPartnerWallet }) => {
         errors, isValid, isSubmitting, setFieldValue
     } = useFormikContext<SwapFormValues>();
 
-    const { to: destination } = values
+    const { to: destination, fromCurrency, toCurrency, from: source } = values
     const { minAllowedAmount, valuesChanger, fee } = useFee()
-    const settings = useSettingsState();
-    const source = values.from
     const toAsset = values.toCurrency?.asset
     const { authData } = useAuthState()
     const { getBalance, getGas } = useBalancesUpdate()
@@ -74,7 +72,7 @@ const SwapForm: FC<Props> = ({ partner, isPartnerWallet }) => {
     const { setDepositeAddressIsfromAccount, setAddressConfirmed } = useSwapDataUpdate()
     const { depositeAddressIsfromAccount } = useSwapDataState()
     const query = useQueryState();
-    const [valuesSwapperDisabled, setValuesSwapperDisabled] = useState(false)
+    let valuesSwapperDisabled = false;
     const [showAddressModal, setShowAddressModal] = useState(false);
     const lockAddress =
         (values.destination_address && values.to)
@@ -136,27 +134,32 @@ const SwapForm: FC<Props> = ({ partner, isPartnerWallet }) => {
         { rotate: 180 }
     );
     //TODO always map to toAsset from query
-    const lockedCurrency = query?.lockAsset ? values.to?.assets?.find(c => c?.asset?.toUpperCase() === toAsset?.toUpperCase()) : null
+    const lockedCurrency = query?.lockAsset ? values.to?.assets?.find(c => c?.asset?.toUpperCase() === toAsset?.toUpperCase()) : null;
+    const apiVersion = LayerSwapApiClient.apiVersion
 
-    useEffect(() => {
+    const sourceRoutesEndpoint = `/routes/sources?destination_network=${source?.internal_name}&destination_asset=${fromCurrency?.asset}${apiVersion ? '&version=' : ''}${apiVersion}`
+    const destinationRoutesEndpoint = `/routes/destinations?source_network=${destination?.internal_name}&source_asset=${toCurrency?.asset}${apiVersion ? '&version=' : ''}${apiVersion}`
+    const { data: sourceRoutes, isLoading: sourceLoading } = useSWR<ApiResponse<{
+        network: string,
+        asset: string
+    }[]>>((source && fromCurrency) ?
+        sourceRoutesEndpoint : `/routes/sources?${apiVersion ? '&version=' : ''}${apiVersion}`, layerswapApiClient.fetcher)
 
-        const filteredSourceLayers = FilterSourceLayers(settings.layers, source, lockedCurrency);
-        const filteredDestinationLayers = FilterDestinationLayers(settings.layers, destination, lockedCurrency);
+    const { data: destinationRoutes, isLoading: destinationLoading } = useSWR<ApiResponse<{
+        network: string,
+        asset: string
+    }[]>>((destination && toCurrency) ?
+        destinationRoutesEndpoint : `/routes/destinations?${apiVersion ? '&version=' : ''}${apiVersion}`, layerswapApiClient.fetcher)
 
-        const sourceCanBeSwapped = filteredDestinationLayers.some(l => l.internal_name === source?.internal_name)
-        const destinationCanBeSwapped = filteredSourceLayers.some(l => l.internal_name === destination?.internal_name)
+    const sourceCanBeSwapped = destinationRoutes?.data?.some(l => l.network === source?.internal_name)
+    const destinationCanBeSwapped = sourceRoutes?.data?.some(l => l.network === destination?.internal_name)
 
-        if (query.lockTo || query.lockFrom || query.hideTo || query.hideFrom) {
-            setValuesSwapperDisabled(true)
-            return;
-        }
-        if (!(sourceCanBeSwapped || destinationCanBeSwapped)) {
-            setValuesSwapperDisabled(true)
-            return;
-        }
-        setValuesSwapperDisabled(false)
-
-    }, [source, destination, query, settings, lockedCurrency])
+    if (query.lockTo || query.lockFrom || query.hideTo || query.hideFrom) {
+        valuesSwapperDisabled = true;
+    }
+    if (!(sourceCanBeSwapped || destinationCanBeSwapped)) {
+        valuesSwapperDisabled = true;
+    }
 
     useEffect(() => {
         values.from && getBalance(values.from)
@@ -202,14 +205,24 @@ const SwapForm: FC<Props> = ({ partner, isPartnerWallet }) => {
                         {!(query?.hideFrom && values?.from) && <div className="flex flex-col w-full">
                             <NetworkFormField direction="from" label="From" />
                         </div>}
-                        {!query?.hideFrom && !query?.hideTo && <button type="button" disabled={valuesSwapperDisabled} onClick={valuesSwapper} className='absolute right-[calc(50%-16px)] top-[74px] z-10 border-4 border-secondary-900 bg-secondary-900 rounded-full disabled:cursor-not-allowed hover:text-primary disabled:text-secondary-text duration-200 transition'>
-                            <motion.div
-                                animate={animate}
-                                transition={{ duration: 0.3 }}
-                                onTap={() => !valuesSwapperDisabled && cycle()}
-                            >
-                                <ArrowUpDown className={classNames(valuesSwapperDisabled && 'opacity-50', "w-8 h-auto p-1 bg-secondary-900 border-2 border-secondary-500 rounded-full disabled:opacity-30")} />
-                            </motion.div>
+                        {!query?.hideFrom && !query?.hideTo && <button type="button" disabled={valuesSwapperDisabled} onClick={valuesSwapper} className={`${sourceLoading || destinationLoading ? "" : "border-4"} absolute right-[calc(50%-16px)] top-[74px] z-10 border-secondary-900 bg-secondary-900 rounded-full disabled:cursor-not-allowed hover:text-primary disabled:text-secondary-text duration-200 transition`}>
+                            {sourceLoading || destinationLoading ?
+                                <div role="status">
+                                    <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                    </svg>
+                                    <span className="sr-only">Loading...</span>
+                                </div>
+                                :
+                                <motion.div
+                                    animate={animate}
+                                    transition={{ duration: 0.3 }}
+                                    onTap={() => !valuesSwapperDisabled && cycle()}
+                                >
+                                    <ArrowUpDown className={classNames(valuesSwapperDisabled && 'opacity-50', "w-8 h-auto p-1 bg-secondary-900 border-2 border-secondary-500 rounded-full disabled:opacity-30")} />
+                                </motion.div>
+                            }
                         </button>}
                         {!(query?.hideTo && values?.to) && <div className="flex flex-col w-full">
                             <NetworkFormField direction="to" label="To" />
