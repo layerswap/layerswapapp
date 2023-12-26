363bffe51ce2c0c0aec4d818cfa93d471171249b
Aren <aren@bransfer.io>
2023-12-26 14:10:23 +0400

    fix little issue

--- components/Select/Popover/PopoverSelect.tsx
+++ components/Select/Popover/PopoverSelect.tsx
@@ -8,12 +8,6 @@ export default function PopoverSelect({ values, value, setValue }: SelectProps) 
         <CommandWrapper>
             <CommandList>
                 {values.map(item => {
-                    const filteredItems = values.filter(item => item.isAvailable.value && item.isAvailable.disabledReason);
-
-                    const upperItem = filteredItems.reduce((maxItem, currentItem) => {
-                        return Number(currentItem.order) > Number(maxItem.order) ? currentItem : maxItem;
-                    }, filteredItems[0]).id === item.id;
-                    
                     return (
                         <CommandItem disabled={!item.isAvailable.value} value={item.id} key={item.id} onSelect={() => {
                             setValue(item)
