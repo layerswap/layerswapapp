2927e2f90c9711bad81ee3a1f195830cfbffd291
babkengevorgyan <babgev@gmail.com>
2024-01-12 19:59:46 +0400

    Fix contract wallet storage

--- hooks/useWalletTransferOptions.ts
+++ hooks/useWalletTransferOptions.ts
@@ -2,47 +2,14 @@ import { useSettingsState } from "../context/settings"
 import { useSwapDataState } from "../context/swap"
 import { NetworkType } from "../Models/CryptoNetwork"
 import useWallet from "./useWallet"
-import { useEffect, useMemo, useState } from "react"
-import { ContractWalletStorage, useContractWalletsStore } from "../stores/contractWalletsStore"
+import { useEffect, useMemo } from "react"
+import { useContractWalletsStore } from "../stores/contractWalletsStore"
 import resolveChain from "../lib/resolveChain"
 import { createPublicClient, http } from "viem"
 
-type ContractWallet = ContractWalletStorage & { ready: boolean };
-
-let checkContractWallet = async (address, network) => {
-    if (!network || !address) throw new Error('Arguments are required')
-    let isContractWallet: boolean = false
-    let isReady: boolean = false
-
-    if (network.type != NetworkType.EVM) {
-        isReady = true;
-        isContractWallet = false;
-    }
-    else {
-        const chain = resolveChain(network)
-        const publicClient = createPublicClient({
-            chain,
-            transport: http()
-        })
-        try {
-            const bytecode = await publicClient.getBytecode({
-                address: address as `0x${string}`
-            });
-            isContractWallet = !!bytecode
-            isReady = true;
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    return { isReady, isContractWallet }
-
-}
-
 export default function useWalletTransferOptions() {
-
     const { swap } = useSwapDataState()
-    const { addContractWallet, contractWallets } = useContractWalletsStore()
+    const { addContractWallet, getContractWallet, updateContractWallet } = useContractWalletsStore()
     const { getWithdrawalProvider: getProvider } = useWallet()
     const { layers, networks } = useSettingsState()
     const source_layer = layers.find(n => n.internal_name === swap?.source_network)
@@ -54,27 +21,50 @@ export default function useWalletTransferOptions() {
     const wallet = provider?.getConnectedWallet()
     useEffect(() => {
         if (wallet?.address == undefined || source_layer == undefined) return;
-        let contractWallet = contractWallets.find(w => w.address === wallet?.address && w.network === source_layer?.internal_name)
+        let contractWallet = getContractWallet(wallet.address, source_layer.internal_name);
+
         if (!contractWallet) {
-            //now this does get code 3 times 
+            // add before checking to check only once
+            addContractWallet(wallet.address, source_layer.internal_name);
             checkContractWallet(wallet.address, source_network).then(
-                r => {
-                    if (r.isReady) {
-                        addContractWallet(wallet.address, source_layer.internal_name, r.isContractWallet)
-                    }
+                result => {
+                    updateContractWallet(wallet.address, source_layer.internal_name, result)
                 }
             )
         }
 
     }, [wallet?.address])
 
-    let contractWalletResolved = contractWallets.find(w => w.address === wallet?.address && w.network === source_layer?.internal_name)
+    let walletTypeResolved = getContractWallet(wallet?.address, source_layer?.internal_name);
     const canDoSweepless = source_layer?.isExchange == false
-        && ((source_layer.type == NetworkType.EVM && !(contractWalletResolved?.network === source_layer.internal_name && contractWalletResolved?.isContract)) || source_layer.type == NetworkType.Starknet)
+        && ((source_layer.type == NetworkType.EVM
+            && (walletTypeResolved?.ready && !walletTypeResolved?.isContract))
+             || source_layer.type == NetworkType.Starknet)
         || wallet?.address?.toLowerCase() === swap?.destination_address.toLowerCase()
 
-    var result: ContractWallet =
-        { ready: !!contractWalletResolved, ...contractWalletResolved }
+    return { canDoSweepless, isContractWallet: walletTypeResolved }
+}
 
-    return { canDoSweepless, isContractWallet: result }
+let checkContractWallet = async (address, network) => {
+    if (!network || !address) throw new Error('Arguments are required')
+
+    if (network.type != NetworkType.EVM) {
+        return false;
+    }
+    else {
+        const chain = resolveChain(network)
+        const publicClient = createPublicClient({
+            chain,
+            transport: http()
+        })
+        try {
+            const bytecode = await publicClient.getBytecode({
+                address: address as `0x${string}`
+            });
+
+            return !!bytecode;
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
--- stores/contractWalletsStore.ts
+++ stores/contractWalletsStore.ts
@@ -1,77 +1,46 @@
 import { create } from 'zustand'
-import resolveChain from '../lib/resolveChain';
-import { createPublicClient, http } from 'viem';
-import { CryptoNetwork, NetworkType } from '../Models/CryptoNetwork';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface WalletState {
-    contractWallets: ContractWalletStorage[];
-    addContractWallet: (address: string, network_internal_name: string, isContractWallet: boolean) => void;
+    contractWallets: ContractWalletInfo[];
+    addContractWallet: (address: string, network_internal_name: string) => void;
+    getContractWallet:  (address, network_internal_name) => ContractWalletInfo | undefined;
+    updateContractWallet: (address, network_internal_name, isContractWallet) => void;
 }
 
-export type ContractWalletStorage = {
-    address?: string,
-    isContract?: boolean
-    network?: string
+export class ContractWalletInfo {
+    key: string; // address+network_internal_name
+    isContract: boolean;
+    ready: boolean;
+
+    public static keyDeriver(address: string, network_internal_name: string) {
+        return address + network_internal_name;
+    }
 }
 
-export const useContractWalletsStore = create<WalletState>()(persist((set) => ({
+
+export const useContractWalletsStore = create<WalletState>()(persist((set, get) => ({
     contractWallets: [],
-    addContractWallet: (address, network_internal_name, isContractWallet) =>
+    getContractWallet:  (address, network_internal_name) => {
+        return get().contractWallets.find(x=> x.key == ContractWalletInfo.keyDeriver(address, network_internal_name))
+    },
+    addContractWallet: (address, network_internal_name) =>
         set((state) => {
             return ({
                 contractWallets: [
-                    ...state.contractWallets.filter(w => w.address !== address),
-                    { address: address, isContract: isContractWallet, network: network_internal_name }
+                    ...state.contractWallets.filter(w => w.key != ContractWalletInfo.keyDeriver(address, network_internal_name)),
+                    { key: ContractWalletInfo.keyDeriver(address, network_internal_name), ready: false, isContract: false }
                 ]
             })
         }),
-    checkContractWallet: (address, network) => {
-        if (!network) { throw new Error('Network is not provided') }
-        (async () => {
-            if (address && network.type == NetworkType.EVM) {
-                let isContractWallet: boolean = false
-                let isReady: boolean = false
-                set((state) => {
-                    if (state.contractWallets.some(w => w.address === address)) {
-                        return ({
-                            contractWallets: [...state.contractWallets]
-                        })
-                    }
-                    else {
-                        (async () => {
-                            const chain = resolveChain(network)
-                            const publicClient = createPublicClient({
-                                chain,
-                                transport: http()
-                            })
-                            try {
-                                const bytecode = await publicClient.getBytecode({
-                                    address: address as `0x${string}`
-                                });
-                                isContractWallet = !!bytecode
-                                isReady = true;
-                            } catch (error) {
-                                console.log(error)
-                            }
-                        })()
+    updateContractWallet: (address, network_internal_name, isContractWallet) =>
+        set((state) => ({
+            contractWallets: [
+                ...state.contractWallets.filter(w => w.key !== ContractWalletInfo.keyDeriver(address, network_internal_name)),
+                { key: ContractWalletInfo.keyDeriver(address, network_internal_name), isContract: isContractWallet, ready: true }
+            ]
 
-                        return ({
-                            contractWallets: [
-                                ...state.contractWallets.filter(w => w.address !== address),
-                                { address: address, ready: isReady, isContract: isContractWallet, network: network.internal_name }
-                            ]
-                        })
-                    }
-                })
-
-                return { address: address, ready: isReady, value: isContractWallet, network: network.internal_name }
-            }
-        })()
-
-        return { ready: true, isContract: false, network: network.internal_name }
-
-    },
+        })),
 }),
     {
         name: 'contractWallets',
