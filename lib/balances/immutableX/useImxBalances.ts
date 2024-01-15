e23260b69c2666188c0f46f2f92ce08b815793df
Aren <aren@bransfer.io>
2024-01-15 18:31:24 +0400

    add imx balance and gas provider

---
+++ lib/balances/immutableX/useImxBalances.ts
@@ -0,0 +1,67 @@
+import KnownInternalNames from "../../knownIds"
+import Erc20Abi from '../../abis/ERC20.json'
+import formatAmount from "../../formatAmount";
+import { Balance, BalanceProps, BalanceProvider, GasProps } from "../../../Models/Balance";
+import { formatEther } from "viem";
+
+export default function useImxBalance(): BalanceProvider {
+    const name = 'starknet'
+
+    const supportedNetworks = [
+        KnownInternalNames.Networks.ImmutableXMainnet,
+        KnownInternalNames.Networks.ImmutableXGoerli
+    ]
+
+    const getBalance = async ({ layer, address }: BalanceProps) => {
+
+        const axios = (await import("axios")).default
+
+        if (layer.isExchange === true || !layer.assets) return
+
+        const res: BalancesResponse = await axios.get(`${layer?.assets?.[0].network?.nodes[0].url}/v2/balances/${address}`).then(r => r.data)
+
+        const balances = layer?.assets?.map(asset => {
+            const balance = res.result.find(r => r.symbol === asset.asset)
+
+            return {
+                network: layer.internal_name,
+                amount: formatAmount(balance?.balance, asset.decimals),
+                decimals: asset.decimals,
+                isNativeCurrency: false,
+                token: asset.asset,
+                request_time: new Date().toJSON(),
+            }
+        })
+
+        return balances
+
+    }
+
+    // Transfers in imx are free
+    const getGas = async ({ layer, currency }: GasProps) => {
+
+        if (layer.isExchange === true || !layer.assets) return
+
+        return [{
+            token: currency.asset,
+            gas: 0,
+            request_time: new Date().toJSON()
+        }]
+
+    }
+
+    return {
+        getBalance,
+        getGas,
+        name,
+        supportedNetworks
+    }
+}
+
+type BalancesResponse = {
+    result: {
+        balance: string,
+        symbol: string,
+        token_address: string
+    }[]
+}
