bf272f16323cbf2633de973d47ae81867af3e942
babken <mesropyan.b@gmail.com>
2024-02-13 18:16:23 +0400

    Brought back sentry

--- components/layout.tsx
+++ components/layout.tsx
@@ -81,20 +81,13 @@ export default function Layout({ children, settings, themeData }: Props) {
     ...(router.query.hideLogo === 'true' ? { hideLogo: true } : {}),
 
   };
-  
+
   function logErrorToService(error, info) {
-    const transaction = Sentry.startTransaction({
-      name: "error_boundary_handler",
-    });
-    Sentry.configureScope((scope) => {
-      scope.setSpan(transaction);
-    });
     const extension_error = IsExtensionError(error)
     if (process.env.NEXT_PUBLIC_VERCEL_ENV && !extension_error) {
       SendErrorMessage("UI error", `env: ${process.env.NEXT_PUBLIC_VERCEL_ENV} %0A url: ${process.env.NEXT_PUBLIC_VERCEL_URL} %0A message: ${error?.message} %0A errorInfo: ${info?.componentStack} %0A stack: ${error?.stack ?? error.stack} %0A`)
     }
     Sentry.captureException(error, info);
-    transaction?.finish();
   }
 
   themeData = themeData || THEME_COLORS.default
