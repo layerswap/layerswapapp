fe3dd3e570ce8407783b6f87f77c00b7c69c45ee
Aren <aren@bransfer.io>
2024-03-13 18:18:59 +0400

    delete imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import Layout from '../components/layout'
 import { InferGetServerSidePropsType } from 'next'
 import Swap from '../components/swapComponent'
 import { getServerSideProps } from '../helpers/getSettings'
-import CardContainer from '../components/cardContainer'
-import { Info } from 'lucide-react'
 
 export default function Home({ settings, themeData }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
