16ddf4062b17924440c2893d21d7170130f6f182
Aren <aren@bransfer.io>
2024-04-03 18:07:07 +0400

    jamanakavor fix for loopring api

--- lib/AppSettings.ts
+++ lib/AppSettings.ts
@@ -2,4 +2,5 @@ export default class AppSettings {
     static LayerswapBridgeApiUri?: string = process.env.NEXT_PUBLIC_LS_BRIDGE_API;
     static LayerswapApiUri?: string = process.env.NEXT_PUBLIC_LS_API
     static ExplorerURl: string = `https://www.layerswap.io/explorer`
+    static ApiVersion?: string = process.env.NEXT_PUBLIC_API_VERSION
 }
--- lib/loopring/LoopringAPI.ts
+++ lib/loopring/LoopringAPI.ts
@@ -3,10 +3,8 @@ import { ChainId } from "./defs";
 import { activateAccount, getOffchainFeeAmt, transfer, unlockAccount } from "./helpers";
 
 export const LoopringAPI = {
-    // CHAIN: AppSettings.ApiVersion === "sandbox" ? ChainId.GOERLI : ChainId.MAINNET,
-    // BaseApi: AppSettings.ApiVersion === "sandbox" ? "https://uat2.loopring.io" : "https://api3.loopring.io",
-    CHAIN: ChainId.GOERLI,
-    BaseApi: "https://uat2.loopring.io",
+    CHAIN: AppSettings.ApiVersion === "sandbox" ? ChainId.GOERLI : ChainId.MAINNET,
+    BaseApi: AppSettings.ApiVersion === "sandbox" ? "https://uat2.loopring.io" : "https://api3.loopring.io",
     userAPI: {
         getOffchainFeeAmt,
         unlockAccount,
