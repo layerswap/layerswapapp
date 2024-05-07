c98d187bed5069aecd48bac8aaeea36f489ff24e
Aren <aren@bransfer.io>
2024-05-07 17:41:56 +0400

    little fixes

--- components/Swap/Withdraw/Wallet/index.tsx
+++ components/Swap/Withdraw/Wallet/index.tsx
@@ -1,8 +1,6 @@
 import { useSwapDataState } from "../../../../context/swap"
 import NetworkGas from "./WalletTransfer/networkGas"
 import { WalletTransferContent } from "./WalletTransferContent"
-import ManualTransferNote from "./WalletTransfer/manualTransferNote"
-import { TransactionType } from "../../../../lib/layerSwapApiClient"
 
 const WalletTransferWrapper = () => {
     const { swapResponse } = useSwapDataState()
@@ -18,10 +16,6 @@ const WalletTransferWrapper = () => {
             <NetworkGas network={source_network} token={fee_token} />
         }
         <WalletTransferContent />
-        {
-            source_network?.deposit_methods.some(m => m === 'deposit_address') && !swap?.transactions?.find(t => t.type === TransactionType.Input) &&
-            <ManualTransferNote />
-        }
     </div>
 }
 
