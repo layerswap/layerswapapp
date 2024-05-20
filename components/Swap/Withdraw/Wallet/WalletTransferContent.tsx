3729214d1b231fa96992467c028b1adb2f56b4ac
Aren <aren@bransfer.io>
2024-05-20 21:00:29 +0400

    chore: Refactor SolanaWalletWithdrawStep component for improved transaction handling

--- components/Swap/Withdraw/Wallet/WalletTransferContent.tsx
+++ components/Swap/Withdraw/Wallet/WalletTransferContent.tsx
@@ -77,6 +77,7 @@ export const WalletTransferContent: FC = () => {
             network={swap?.source_network}
             token={swap?.source_token}
             swapId={swap?.id}
+            callData={callData}
         />;
     else
         return <>
