4b96fe3e5d70e95cacb8ee7e8edc6dab5d63015d
paur94 <paruyryeprikyan9@gmail.com>
2024-05-21 15:31:29 +0400

    removed console

--- lib/sorting.ts
+++ lib/sorting.ts
@@ -10,7 +10,6 @@ export function SortingByOrder<T>(x: SelectMenuItem<T>, y: SelectMenuItem<T>) {
 }
 
 export function SortingByAvailability<T>(x: SelectMenuItem<T>, y: SelectMenuItem<T>) {
-    if (x.id === "XLAYER_MAINNET") console.log(x)
     const reasonA = x.isAvailable && x.isAvailable.disabledReason;
     const reasonB = y.isAvailable && y.isAvailable.disabledReason;
     if (reasonA && !reasonB) {
