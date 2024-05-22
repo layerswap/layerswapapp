62327a43ae3ddfbbfef5ce48e362b42b9d3d677c
paur94 <paruyryeprikyan9@gmail.com>
2024-05-22 13:47:18 +0400

    test-3

--- components/modal/leaflet.tsx
+++ components/modal/leaflet.tsx
@@ -78,18 +78,17 @@ export const Leaflet = forwardRef<HTMLDivElement, PropsWithChildren<LeafletProps
             <motion.div
                 key="mobile-modal"
                 ref={mobileModalRef}
-                className={`${wrapperHeightClass} max-h-full overflow-y-hidden group ${position} inset-x-0 bottom-0 z-50 w-full ${height != 'full' ? 'rounded-t-2xl border-t border-secondary-500' : ''}  bg-secondary-900 ${className} shadow-lg`}
+                className={`${wrapperHeightClass} max-h-full overflow-y-hidden group ${position} inset-x-0 bottom-0 z-40 w-full ${height != 'full' ? 'rounded-t-2xl border-t border-secondary-500' : ''}  bg-secondary-900 ${className} shadow-lg`}
                 initial={{ y: "20%" }}
                 animate={controls}
                 exit={{ y: "100%" }}
                 transition={transitionProps}
                 drag={height != 'full' ? "y" : false}
-                dragDirectionLock
                 onDragEnd={handleDragEnd}
                 dragElastic={{ top: 0, bottom: 1 }}
                 dragConstraints={{ top: 0, bottom: 0 }}
             >
-                <div className={`py-3 overflow-y-auto flex flex-col h-full z-50 ${height != 'full' ? 'bg-secondary-900 border-t border-secondary-500 rounded-t-2xl ' : ''} pb-6`}>
+                <div className={`py-3 overflow-y-auto flex flex-col h-full z-40 ${height != 'full' ? 'bg-secondary-900 border-t border-secondary-500 rounded-t-2xl ' : ''} pb-6`}>
                     <div className='px-6 flex justify-between items-center'>
                         <div className="text-lg text-primary-text font-normal">
                             <div>{title}</div>
