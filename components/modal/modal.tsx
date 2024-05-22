e7893377c0c6c149a696a632c06389a9a91da933
paur94 <paruyryeprikyan9@gmail.com>
2024-05-22 18:01:20 +0400

    test-4

--- components/modal/modal.tsx
+++ components/modal/modal.tsx
@@ -60,7 +60,7 @@ const Modal: FC<ModalProps> = (({ header, height, className, children, subHeader
                     {delayedShow &&
                         <Leaflet
                             position="fixed"
-                            height={height == 'full' ? '80%' : height == 'fit' ? 'fit' : 'full'}
+                            height={height == 'full' ? '90%' : height == 'fit' ? 'fit' : 'full'}
                             ref={mobileModalRef}
                             show={delayedShow}
                             setShow={setShow}
