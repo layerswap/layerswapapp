dec053b1cc70e76812b82b82d74f9dae2faff9b6
babkenmes <mesropyan.b@gmail.com>
2024-07-02 20:27:40 +0400

    added base path

--- pages/imtblRedirect.tsx
+++ pages/imtblRedirect.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { passportInstance, initilizePassport } from "../components/ImtblPassportProvider";
+import { useRouter } from "next/router";
 
 const ImtblRedirect = () => {
+    const router = useRouter();
 
     useEffect(() => {
         (async () => {
-            if (!passportInstance) await initilizePassport()
+            if (!passportInstance) await initilizePassport(router.basePath)
             passportInstance.loginCallback();
         })()
     }, [passportInstance])
