af9487b5a5b9554103a5904fca8cc19557dc4f7c
erik <erik@bransfer.io>
2024-08-26 22:52:10 +0400

    FIxed issue

--- components/buttons/copyButton.tsx
+++ components/buttons/copyButton.tsx
@@ -1,7 +1,7 @@
 import { Check, Copy } from 'lucide-react'
 import { classNames } from '../utils/classNames'
 import useCopyClipboard from '../../hooks/useCopyClipboard'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { Tooltip, TooltipContent, TooltipTrigger } from '../shadcn/tooltip'
 
 interface CopyButtonProps {
@@ -14,31 +14,34 @@ interface CopyButtonProps {
 
 const CopyButton: FC<CopyButtonProps> = ({ className, toCopy, children, iconSize, iconClassName }) => {
   const [isCopied, setCopied] = useCopyClipboard()
+  const [isTooltipOpen, setTooltipOpen] = useState(false);
+  const handleCopyClick = () => {
+    setCopied(toCopy);
+    setTooltipOpen(true);
+  };
 
   return (
-      <Tooltip>
-        <TooltipTrigger>
-          <div className={classNames(className)} onClick={() => setCopied(toCopy)}>
-            {isCopied && (
-              <div className="flex items-center gap-1 cursor-pointer">
-                <Check className={iconClassName} width={iconSize ? iconSize : 16} height={iconSize ? iconSize : 16} />
-                <p>Copied</p>
-              </div>
-            )}
-
-            {!isCopied && (
-              <div className="flex items-center gap-1 cursor-pointer">
-                <Copy className={iconClassName} width={iconSize ? iconSize : 16} height={iconSize ? iconSize : 16} />
-                {children}
-              </div>
-            )}
-          </div>
-        </TooltipTrigger>
-          <TooltipContent>
-            <p>Copy</p>
-          </TooltipContent>
-      </Tooltip>
-  )
+    <Tooltip open={isTooltipOpen} onOpenChange={setTooltipOpen}>
+      <TooltipTrigger>
+        <div className={classNames(className)} onClick={handleCopyClick}>
+          {isCopied ? (
+            <div className="flex items-center gap-1 cursor-pointer">
+              <Check className={iconClassName} width={iconSize ? iconSize : 16} height={iconSize ? iconSize : 16} />
+              {children}
+            </div>
+          ) : (
+            <div className="flex items-center gap-1 cursor-pointer">
+              <Copy className={iconClassName} width={iconSize ? iconSize : 16} height={iconSize ? iconSize : 16} />
+              {children}
+            </div>
+          )}
+        </div>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>{isCopied ? "Copied" : "Copy"}</p>
+      </TooltipContent>
+    </Tooltip>
+  );
 }
 
 export default CopyButton
