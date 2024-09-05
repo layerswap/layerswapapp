fd174648252f8c54aea453e88de00d5be262762a
babkenmes <mesropyan.b@gmail.com>
2024-09-05 13:47:25 +0400

    Fixed  validation texts

--- context/validationErrorContext.tsx
+++ context/validationErrorContext.tsx
@@ -6,6 +6,11 @@ import { QueryParams } from '../Models/QueryParams';
 import { ApiError, LSAPIKnownErrorCode } from '../Models/ApiError';
 import { useFormikContext } from 'formik';
 import { useQueryState } from './query';
+import { resolveRoutesURLForSelectedToken } from '../helpers/routes';
+import useSWR from 'swr';
+import { ApiResponse } from '../Models/ApiResponse';
+import { RouteNetwork } from '../Models/Network';
+import LayerSwapApiClient from '../lib/layerSwapApiClient';
 
 interface ValidationDetails {
     title?: string;
@@ -39,9 +44,16 @@ export const ValidationProvider: React.FC<{ children: ReactNode }> = ({ children
     const exchangeUnavailable = currencyGroup?.status === 'inactive';
 
     let validationMessage = '';
-    let validationDetails: ValidationDetails = {};                                                                             
+    let validationDetails: ValidationDetails = {};
 
-    if (query?.lockFromAsset) {
+    const layerswapApiClient = new LayerSwapApiClient()
+    const sourceRoutesEndpoint = (from || to) ? resolveRoutesURLForSelectedToken({ direction: 'from', network: from?.name, token: fromCurrency?.symbol, includes: { unavailable: true, unmatched: true } }) : null
+    const destinationRoutesEndpoint = (from || to) ? resolveRoutesURLForSelectedToken({ direction: 'to', network: from?.name, token: toCurrency?.symbol, includes: { unavailable: true, unmatched: true } }) : null
+
+    const { data: sourceRoutes } = useSWR<ApiResponse<RouteNetwork[]>>(sourceRoutesEndpoint, layerswapApiClient.fetcher, { keepPreviousData: true })
+    const { data: destinationRoutes } = useSWR<ApiResponse<RouteNetwork[]>>(destinationRoutesEndpoint, layerswapApiClient.fetcher, { keepPreviousData: true })
+
+    if (query?.lockToAsset) {
         if (fromCurrency?.status === 'not_found') {
             validationMessage = `Transfers from ${fromDisplayName} ${fromCurrency.symbol} to this token are not supported`;
             validationDetails = { title: 'Route Unavailable', type: 'warning', icon: <RouteOff stroke='#f8974b' className='w-4 h-4 ' /> };
@@ -51,11 +63,11 @@ export const ValidationProvider: React.FC<{ children: ReactNode }> = ({ children
             validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <CircleAlert stroke='#f8974b' className='w-4 h-4 ' /> };
         }
         else if (!fromCurrency) {
-            validationMessage = `Sorry, transfers from ${fromDisplayName || query.from} ${query?.fromAsset} are not available at the moment. Please try later.`;
+            validationMessage = `Sorry, transfers to ${toDisplayName || query.to} ${query?.toAsset} are not available at the moment. Please try later.`;
             validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <CircleAlert stroke='#f8974b' className='w-4 h-4 ' /> };
         }
     }
-    else if (query?.lockToAsset) {
+    else if (query?.lockFromAsset) {
         if (toCurrency?.status === 'not_found') {
             validationMessage = `Transfers to ${toDisplayName} ${toCurrency?.symbol} from this token are not supported`;
             validationDetails = { title: 'Route Unavailable', type: 'warning', icon: <RouteOff stroke='#f8974b' className='w-4 h-4 ' /> };
@@ -64,37 +76,35 @@ export const ValidationProvider: React.FC<{ children: ReactNode }> = ({ children
             validationMessage = `Sorry, transfers to ${toDisplayName} ${toCurrency?.symbol} are not available at the moment. Please try later.`;
             validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <CircleAlert stroke='#f8974b' className='w-4 h-4 ' /> };
         }
-        else if (!toCurrency) {
-            validationMessage = `Sorry, transfers to ${toDisplayName || query.to} ${query?.toAsset} are not available at the moment. Please try later.`;
+        else if (!fromCurrency) {
+            validationMessage = `Sorry, transfers from ${fromDisplayName || query.from} ${query?.fromAsset} are not available at the moment. Please try later.`;
             validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <CircleAlert stroke='#f8974b' className='w-4 h-4 ' /> };
         }
     }
-    else if (currencyGroup?.status === 'not_found') {
+    else if (currencyGroup?.status === 'not_found' || toCurrency?.status === 'not_found' || fromCurrency?.status === 'not_found') {
         validationMessage = 'Please change one of the selected tokens';
-        validationDetails = { title: 'Warning', type: 'warning', icon: <RouteOff stroke='#f8974b' className='w-4 h-4 ' /> };
+        validationDetails = { title: 'Route Unavailable', type: 'warning', icon: <RouteOff stroke='#f8974b' className='w-4 h-4 ' /> };
     }
     else if (currencyGroup?.status === 'inactive') {
         const unavailableDirection = fromUnavailable ? `${fromDisplayName} ${fromCurrency.symbol}` : exchangeUnavailable ? `${fromExchange ? fromDisplayName : toDisplayName} ${currencyGroup?.symbol}` : `${toDisplayName} ${toCurrency?.symbol}`;
         validationMessage = `Sorry, transfers ${fromUnavailable || (exchangeUnavailable && fromExchange) ? 'from' : 'to'} ${unavailableDirection} are not available at the moment. Please try later.`;
         validationDetails = { title: 'Temporarily unavailable', type: 'warning', icon: <CircleAlert stroke='#f8974b' className='w-4 h-4 ' /> };
     }
-    else if (fromCurrency?.status === 'not_found') {
-        validationMessage = 'Please change one of the selected tokens';
-        validationDetails = { title: 'Route Unavailable', type: 'warning', icon: <RouteOff stroke='#f8974b' className='w-4 h-4 ' /> };
-    }
-    else if (fromCurrency?.status === 'inactive') {
-        const unavailableDirection = fromUnavailable ? `${fromDisplayName} ${fromCurrency.symbol}` : '';
-        validationMessage = `Sorry, transfers from ${unavailableDirection} are not available at the moment. Please try later.`;
-        validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <CircleAlert stroke='#f8974b' className='w-4 h-4 ' /> };
-    }
-    else if (toCurrency?.status === 'not_found') {
-        validationMessage = 'Please change one of the selected tokens';
-        validationDetails = { title: 'Route Unavailable', type: 'warning', icon: <RouteOff stroke='#f8974b' className='w-4 h-4 ' /> };
-    }
-    else if (toCurrency?.status === 'inactive') {
-        const unavailableDirection = toUnavailable ? `${toDisplayName} ${toCurrency.symbol}` : '';
-        validationMessage = `Sorry, transfers to ${unavailableDirection} are not available at the moment. Please try later.`;
-        validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <CircleAlert stroke='#f8974b' className='w-4 h-4 ' /> };
+    else if (toCurrency?.status === 'inactive' || fromCurrency?.status === 'inactive') {
+        if (sourceRoutes?.data?.some(r => r.tokens.some(t => t.status !== 'inactive'))) {
+            const unavailableDirection = `${toDisplayName} ${toCurrency?.symbol}`;
+            validationMessage = `Sorry, transfers to ${unavailableDirection} are not available at the moment. Please try later.`;
+            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <CircleAlert stroke='#f8974b' className='w-4 h-4 ' /> };
+        }
+        else if (destinationRoutes?.data?.some(r => r.tokens.some(t => t.status !== 'inactive'))) {
+            const unavailableDirection = `${fromDisplayName} ${fromCurrency?.symbol}`;
+            validationMessage = `Sorry, transfers from ${unavailableDirection} are not available at the moment. Please try later.`;
+            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <CircleAlert stroke='#f8974b' className='w-4 h-4 ' /> };
+        }
+        else {
+            validationMessage = `Please change one of the selected tokens or try later.`;
+            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <CircleAlert stroke='#f8974b' className='w-4 h-4 ' /> };
+        }
     }
     return (
         <ValidationContext.Provider
