61306eb866bb412bbac303c9aa1c50ca371c09ae
Aren <aren@bransfer.io>
2024-10-01 21:05:02 +0400

    Some fixes in fetching balances and gases

--- hooks/useBalance.ts
+++ hooks/useBalance.ts
@@ -10,6 +10,7 @@ import { useBalancesState, useBalancesUpdate } from "../context/balances"
 import { Network, NetworkWithTokens, Token } from "../Models/Network"
 import { useQueryState } from "../context/query"
 import useTonBalance from "../lib/balances/ton/useTonBalance"
+import useIsWindowVisible from "./useIsWindowVisible"
 
 export default function useBalanceProvider() {
 
@@ -23,7 +24,7 @@ export default function useBalanceProvider() {
         useTonBalance()
     ]
 
-    const { balances, gases } = useBalancesState()
+    const { balances, gases, isBalanceLoading } = useBalancesState()
     const query = useQueryState()
 
     const {
@@ -34,87 +35,106 @@ export default function useBalanceProvider() {
     } = useBalancesUpdate()
 
     const { getAutofillProvider } = useWallet()
+    const isWindowVisible = useIsWindowVisible()
 
     const fetchNetworkBalances = async (network: NetworkWithTokens, address?: string) => {
+        if (!isWindowVisible) return
+
         const provider = getAutofillProvider(network)
         const wallet = provider?.getConnectedWallet(network)
         address = address || query.account || wallet?.address
 
-        const balance = balances[address || '']?.find(b => b?.network === network?.name)
-        const isBalanceOutDated = !balance || new Date().getTime() - (new Date(balance.request_time).getTime() || 0) > 10000
+        const balance = address ? balances[address]?.find(b => b?.network === network?.name) : undefined
+        const isBalanceOutDated = !balance || (balance.request_time ? (new Date().getTime() - new Date(balance.request_time).getTime() > 15000) : true)
 
         if (network
             && isBalanceOutDated
             && address) {
-            setIsBalanceLoading(true)
+            try {
+                setIsBalanceLoading(true)
 
-            const provider = getBalanceProvider(network)
-            const networkBalances = await provider?.getNetworkBalances({
-                networkName: network.name,
-                address: address,
-            }) || []
+                const provider = getBalanceProvider(network)
+                const networkBalances = await provider?.getNetworkBalances({
+                    networkName: network.name,
+                    address: address,
+                })
 
-            setAllBalances((data) => {
-                const walletBalances = data[address]
-                const otherNetworkBalances = walletBalances?.filter(b => b?.network !== network.name) || []
-                return { ...data, [address]: [...otherNetworkBalances, ...networkBalances] }
-            })
-            setIsBalanceLoading(false)
+                if (networkBalances) {
+                    setAllBalances((data) => {
+                        const walletBalances = data[address]
+                        const otherNetworkBalances = walletBalances?.filter(b => b?.network !== network.name) || []
+                        return { ...data, [address]: [...otherNetworkBalances, ...networkBalances] }
+                    })
+                }
+
+            }
+            catch (e) { console.log(e) }
+            finally { setIsBalanceLoading(false) }
         }
     }
 
     const fetchBalance = async (network: Network, token: Token, address?: string) => {
+        if (!isWindowVisible) return
+
         const provider = getAutofillProvider(network)
         const wallet = provider?.getConnectedWallet(network)
         address = address || query.account || wallet?.address
 
-        const balance = balances[address || '']?.find(b => b?.network === network?.name)
-        const isBalanceOutDated = !balance || new Date().getTime() - (new Date(balance.request_time).getTime() || 0) > 10000
+        const balance = balances[address || '']?.find(b => b?.network === network?.name && b?.token === token?.symbol)
+        const isBalanceOutDated = !balance || (balance.request_time ? (new Date().getTime() - new Date(balance.request_time).getTime() > 15000) : true)
 
         if (network
             && isBalanceOutDated
             && address) {
-            setIsBalanceLoading(true)
+            try {
+                setIsBalanceLoading(true)
+                debugger
 
-            const provider = getBalanceProvider(network)
-            const balance = await provider?.getBalance({
-                networkName: network.name,
-                address: address,
-                token
-            })
+                const provider = getBalanceProvider(network)
+                const newBalance = await provider?.getBalance({
+                    networkName: network.name,
+                    address: address,
+                    token
+                })
 
-            setAllBalances((data) => {
-                const walletBalances = data[address]
-                const filteredBalances = walletBalances?.filter(b => !(b?.network === network?.name && b?.token === token?.symbol)) || []
-                return { ...data, [address]: filteredBalances?.concat(balance || []) }
-            })
-            setIsBalanceLoading(false)
+                setAllBalances((data) => {
+                    const walletBalances = data[address]
+                    const filteredBalances = walletBalances?.filter(b => !(b?.network === network?.name && b?.token === token?.symbol)) || []
+                    return { ...data, [address]: filteredBalances?.concat(newBalance || []) }
+                })
 
-            return balance
+                return newBalance
+
+
+            }
+            catch (e) { console.log(e) }
+            finally { setIsBalanceLoading(false) }
         }
 
         return balance
     }
 
     const fetchGas = async (network: Network, token: Token, userDestinationAddress: string, recipientAddress?: string) => {
-
-        if (!network) {
+        if (!network || !isWindowVisible) {
             return
         }
 
         const destination_address = userDestinationAddress as `0x${string}`
         const gas = gases[network.name]?.find(g => g?.token === token?.symbol)
-        const isGasOutDated = !gas || new Date().getTime() - (new Date(gas.request_time).getTime() || 0) > 10000
+        const isGasOutDated = !gas || (gas.request_time ? (new Date().getTime() - new Date(gas.request_time).getTime() > 15000) : true)
 
         const provider = getAutofillProvider(network)
         const wallet = provider?.getConnectedWallet(network)
 
-        if (isGasOutDated
+        if (
+            isGasOutDated
             && token
             && wallet?.address
-            && destination_address) {
-            setIsGasLoading(true)
+            && destination_address
+        ) {
             try {
+                setIsGasLoading(true)
+
                 const gasProvider = getBalanceProvider(network)
                 const gas = gasProvider?.getGas && await gasProvider?.getGas({
                     address: wallet?.address as `0x${string}`,
@@ -123,7 +143,7 @@ export default function useBalanceProvider() {
                     recipientAddress,
                     isSweeplessTx: wallet.address !== userDestinationAddress,
                     wallet
-                }) || []
+                })
 
                 if (gas) {
                     setAllGases((data) => {
