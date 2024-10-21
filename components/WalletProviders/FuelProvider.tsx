9775a30a51eadfe5840a46e0fc76d6e8a2907388
Aren <aren@bransfer.io>
2024-10-21 18:58:12 +0400

    Fix fuel config

--- components/WalletProviders/FuelProvider.tsx
+++ components/WalletProviders/FuelProvider.tsx
@@ -2,6 +2,7 @@
 import { defaultConnectors } from '@fuels/connectors';
 import { FuelProvider } from '@fuels/react';
 import { CHAIN_IDS, Provider } from 'fuels';
+import { useConfig } from 'wagmi';
 
 const WALLETCONNECT_PROJECT_ID = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || '28168903b2d30c75e5f7f2d71902581b';
 
@@ -12,17 +13,20 @@ const NETWORKS = [
     },
 ];
 
-const fuelConfig = {
-    connectors: defaultConnectors({
-        devMode: true,
-        wcProjectId: WALLETCONNECT_PROJECT_ID,
-        chainId: NETWORKS[0].chainId,
-        fuelProvider: Provider.create(NETWORKS[0].url),
-    }),
-};
 const FuelProviderWrapper = ({
     children,
 }: { children: React.ReactNode }) => {
+
+    const config = useConfig()
+    const fuelConfig = {
+        connectors: defaultConnectors({
+            devMode: false,
+            wcProjectId: WALLETCONNECT_PROJECT_ID,
+            chainId: NETWORKS[0].chainId,
+            fuelProvider: Provider.create(NETWORKS[0].url),
+            ethWagmiConfig: config
+        }),
+    };
 
     return (
         <FuelProvider uiConfig={{ suggestBridge: false }} theme={'dark'} fuelConfig={fuelConfig} networks={NETWORKS}>
