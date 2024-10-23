9d8e3ab8207a8c2583464a601f18b063ac7220fe
Aren <aren@bransfer.io>
2024-10-23 13:20:41 +0400

    Add fuel wallet icon resolver

--- lib/wallets/fuel/useFuel.ts
+++ lib/wallets/fuel/useFuel.ts
@@ -1,48 +1,42 @@
 import KnownInternalNames from "../../knownIds";
 import { Wallet } from "../../../stores/walletStore";
 import { WalletProvider } from "../../../hooks/useWallet";
-import { useEffect, useState } from "react";
 import {
     useConnectUI,
     useDisconnect,
     useWallet,
 } from '@fuels/react';
-import Fuel from "../../../components/icons/Wallets/Fuel";
 import useStorage from "../../../hooks/useStorage";
+import resolveWalletConnectorIcon from "../utils/resolveWalletIcon";
 
 export default function useFuel(): WalletProvider {
     const autofillSupportedNetworks = [KnownInternalNames.Networks.FuelTestnet]
     const name = 'fuel'
 
     const { wallet, isRefetching, isLoading, isFetching } = useWallet()
-    const { connect,isConnecting } = useConnectUI()
+    const { connect, isConnecting } = useConnectUI()
     const { disconnectAsync } = useDisconnect()
     const { storageAvailable, setItem, getItem } = useStorage()
 
-    const [shouldConnect, setShouldConnect] = useState(false)
-
-    useEffect(() => {
-        if (shouldConnect) {
-            connectWallet()
-            setShouldConnect(false)
-        }
-    }, [shouldConnect])
-
     const getWallet = () => {
+
         if (!isConnecting && !isFetching && !isRefetching && storageAvailable && !wallet?.address && !isLoading) {
             const fuelCurrentConnector = getItem('fuel-current-connector', 'localStorage')
+
             if (fuelCurrentConnector && fuelCurrentConnector === 'Bako Safe') {
                 setItem('fuel-current-connector', '', 'localStorage')
             }
         }
 
         if (wallet) {
+            const fuelCurrentConnector = getItem('fuel-current-connector', 'localStorage')
+
             const address = wallet.address.toB256()
             const w: Wallet = {
                 address: address,
-                connector: 'Fuel',
+                connector: fuelCurrentConnector,
                 providerName: name,
-                icon: Fuel
+                icon: resolveWalletConnectorIcon({ connector: fuelCurrentConnector, address: address })
             }
             return w
         }
@@ -64,7 +58,7 @@ export default function useFuel(): WalletProvider {
     const reconnectWallet = async () => {
         try {
             await disconnectWallet()
-            setShouldConnect(true)
+            connectWallet()
         }
         catch (e) {
             console.log(e)
