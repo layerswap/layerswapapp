712c163e01c29fa53d982859b360dc9bcbd3cea7
babkenmes <mesropyan.b@gmail.com>
2024-11-04 21:25:21 +0400

    Added custom baki api requester

--- components/WalletProviders/FuelProvider.tsx
+++ components/WalletProviders/FuelProvider.tsx
@@ -1,10 +1,11 @@
 
-import { defaultConnectors } from '@fuels/connectors';
+import { BakoSafeConnector, FuelWalletConnector, FueletWalletConnector, SolanaConnector, WalletConnectConnector, defaultConnectors } from '@fuels/connectors';
 import { FuelProvider } from '@fuels/react';
-import { CHAIN_IDS, Provider } from '@fuel-ts/account';
+import { CHAIN_IDS, Provider, urlJoin } from '@fuel-ts/account';
 import { useConfig } from 'wagmi';
 import KnownInternalNames from '../../lib/knownIds';
 import { useSettingsState } from '../../context/settings';
+export const HOST_URL = 'https://api.bako.global';
 
 const FuelProviderWrapper = ({
     children,
@@ -16,23 +17,27 @@ const FuelProviderWrapper = ({
 
     const network = networks.find(network => network.name === KnownInternalNames.Networks.FuelMainnet || network.name === KnownInternalNames.Networks.FuelTestnet)
     const isMainnet = network?.name === KnownInternalNames.Networks.FuelMainnet
-    
-    if (!network)
-        return <FuelProvider theme='dark' fuelConfig={{
-            connectors: defaultConnectors({ devMode: false, wcProjectId: WALLETCONNECT_PROJECT_ID }),
-        }}>
-            {children}
-        </FuelProvider>
 
     const fuelConfig = {
-        connectors: defaultConnectors({
-            devMode: false,
-            wcProjectId: WALLETCONNECT_PROJECT_ID,
-            chainId: isMainnet ? CHAIN_IDS.fuel.mainnet : CHAIN_IDS.fuel.testnet,
-            fuelProvider: Provider.create(network?.node_url),
-            ethWagmiConfig: config
-        }),
-    };
+        connectors: [
+            new FuelWalletConnector(),
+            new BakoSafeConnector({
+                api: new BaskoRequestAPI(HOST_URL)
+            }),
+            new FueletWalletConnector(),
+            new WalletConnectConnector({
+                projectId: WALLETCONNECT_PROJECT_ID,
+                wagmiConfig: config,
+                chainId: isMainnet ? CHAIN_IDS.fuel.mainnet : CHAIN_IDS.fuel.testnet,
+                ...(network?.node_url ? { fuelProvider: Provider.create(network?.node_url) } : {}),
+            }),
+            new SolanaConnector({
+                projectId: WALLETCONNECT_PROJECT_ID,
+                chainId: isMainnet ? CHAIN_IDS.fuel.mainnet : CHAIN_IDS.fuel.testnet,
+                ...(network?.node_url ? { fuelProvider: Provider.create(network?.node_url) } : {}),
+            })
+        ]
+    }
 
     return (
         <FuelProvider uiConfig={{ suggestBridge: false }} theme={'dark'} fuelConfig={fuelConfig}>
@@ -40,5 +45,47 @@ const FuelProviderWrapper = ({
         </FuelProvider>
     );
 };
+
+export const BAKO_STATE: {
+    state: { last_req?: Date, data: boolean, req_count: number, period_start?: Date },
+    period_durtion: number
+} = { state: { data: false, req_count: 0 }, period_durtion: 10000 };
+
+export class BaskoRequestAPI {
+    baseUrl: string;
+
+    constructor(baseUrl: string) {
+        this.baseUrl = baseUrl;
+    }
+
+    async get(pathname: string) {
+
+        if (!pathname.includes('/state')) {
+            const data = await fetch(urlJoin(this.baseUrl, pathname)).then((res) =>
+                res.json(),
+            );
+            return data;
+        }
+
+        const period_elapsed = BAKO_STATE.state?.period_start && new Date().getTime() - BAKO_STATE.state?.period_start?.getTime() > BAKO_STATE.period_durtion;
+        const skip = BAKO_STATE.state?.last_req && new Date().getTime() - BAKO_STATE.state?.last_req?.getTime() < 1000 * 60 * 2 && period_elapsed;
+
+        if (skip)
+            return BAKO_STATE.state?.data;
+
+        const data = await fetch(urlJoin(this.baseUrl, pathname)).then((res) =>
+            res.json(),
+        );
+        const count = BAKO_STATE.state?.req_count || 0;
+        BAKO_STATE.state = { last_req: new Date(), data, req_count: count + 1, period_start: period_elapsed ? new Date() : BAKO_STATE.state?.period_start || new Date() };
+        return data;
+    }
+
+    async delete(pathname: string) {
+        await fetch(urlJoin(this.baseUrl, pathname), {
+            method: 'DELETE',
+        });
+    }
+}
 
 export default FuelProviderWrapper;
