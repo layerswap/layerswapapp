266630183821c56d3903fd409d49275b33439d9e
paur94 <paruyryeprikyan9@gmail.com>
2024-11-18 18:39:58 +0400

    changed meta title, description

--- components/layout.tsx
+++ components/layout.tsx
@@ -98,20 +98,20 @@ export default function Layout({ children, settings, themeData }: Props) {
 
   return (<>
     <Head>
-      <title>Layerswap App</title>
+      <title>Seamless Cross-Chain Bridging | Layerswap App</title>
       <link rel="apple-touch-icon" sizes="180x180" href={`${basePath}/favicon/apple-touch-icon.png`} />
       <link rel="icon" type="image/png" sizes="32x32" href={`${basePath}/favicon/favicon-32x32.png`} />
       <link rel="icon" type="image/png" sizes="16x16" href={`${basePath}/favicon/favicon-16x16.png`} />
       <link rel="manifest" href={`${basePath}/favicon/site.webmanifest`} />
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content={`rgb(${themeData.secondary?.[900]})`} />
-      <meta name="description" content="Streamline your asset transaction experience with Layerswap across 35+ blockchains" />
+      <meta name="description" content="Streamline your asset transaction experience with Layerswap across 50+ blockchains and 15+ exchanges. Fast, affordable and secure." />
 
       {/* Facebook Meta Tags */}
       <meta property="og:url" content={`https://www.layerswap.io/${basePath}`} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Layerswap App" />
-      <meta property="og:description" content="Streamline your asset transaction experience with Layerswap across 35+ blockchains" />
+      <meta property="og:description" content="Streamline your asset transaction experience with Layerswap across 50+ blockchains and 15+ exchanges. Fast, affordable and secure." />
       <meta property="og:image" content={`https://layerswap.io/${basePath}/opengraph.jpg?v=2`} />
 
       {/* Twitter Meta Tags */}
@@ -119,7 +119,7 @@ export default function Layout({ children, settings, themeData }: Props) {
       <meta property="twitter:domain" content="layerswap.io" />
       <meta property="twitter:url" content={`https://www.layerswap.io/${basePath}`} />
       <meta name="twitter:title" content="Layerswap App" />
-      <meta name="twitter:description" content="Streamline your asset transaction experience with Layerswap across 35+ blockchains" />
+      <meta name="twitter:description" content="Streamline your asset transaction experience with Layerswap across 50+ blockchains and 15+ exchanges. Fast, affordable and secure." />
       <meta name="twitter:image" content={`https://layerswap.io/${basePath}/opengraphtw.jpg`} />
     </Head>
     {
