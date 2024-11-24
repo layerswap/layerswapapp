cde25d946c8ce7879ca9b2c13be451456579dfe9
babkenmes <mesropyan.b@gmail.com>
2024-11-24 19:28:49 +0400

    Changed title

--- components/layout.tsx
+++ components/layout.tsx
@@ -99,7 +99,7 @@ export default function Layout({ children, settings, themeData }: Props) {
 
   return (<>
     <Head>
-      <title>Seamless Cross-Chain Bridging | Layerswap App</title>
+      <title>Layerswap App</title>
       <link rel="apple-touch-icon" sizes="180x180" href={`${basePath}/favicon/apple-touch-icon.png`} />
       <link rel="icon" type="image/png" sizes="32x32" href={`${basePath}/favicon/favicon-32x32.png`} />
       <link rel="icon" type="image/png" sizes="16x16" href={`${basePath}/favicon/favicon-16x16.png`} />
