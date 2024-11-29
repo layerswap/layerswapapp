ddc413627d2abe7dc45c251b0b1b8d850556c72d
paur94 <paruyryeprikyan9@gmail.com>
2024-11-29 16:38:29 +0400

    test

--- components/Input/Address/AddressPicker/ConnectedWallets/index.tsx
+++ components/Input/Address/AddressPicker/ConnectedWallets/index.tsx
@@ -88,48 +88,81 @@ const ConnectedWallets: FC<Props> = ({ provider, wallets, onClick, onConnect, de
 
         {
             notCompatibleWallets.length > 0 &&
-            <ResizablePanel>
-                <div className="flex flex-col gap-2">
-                    <button
-                        onClick={() => { setShowIncompatibleWallets(!showIncompatibleWallets) }}
-                        disabled={isLoading}
-                        type="button" className="flex items-center justify-between w-full">
-                        <p className="text-sm font-medium text-secondary-text"><span>Not compatible with</span> <span>{destination.display_name}</span></p>
-                        <div
-
-                            className="text-secondary-text hover:text-primary-text text-xs rounded-lg flex items-center gap-1.5 transition-colors duration-200"
+            (notCompatibleWallets.length > 1 ? (
+                <ResizablePanel>
+                    <div className="flex flex-col gap-2">
+                        <button
+                            onClick={() => setShowIncompatibleWallets(!showIncompatibleWallets)}
+                            disabled={isLoading}
+                            type="button"
+                            className="flex items-center justify-between w-full"
                         >
-                            {
-                                isLoading ?
+                            <p className="text-sm font-medium text-secondary-text">
+                                <span>Not compatible with</span> <span>{destination.display_name}</span>
+                            </p>
+                            <div
+                                className="text-secondary-text hover:text-primary-text text-xs rounded-lg flex items-center gap-1.5 transition-colors duration-200"
+                            >
+                                {isLoading ? (
                                     <RefreshCw className="h-3 w-auto animate-spin" />
-                                    :
-                                    <ChevronDown className={`h-5 w-auto ${showIncompatibleWallets && 'rotate-180'} transition-all duration-200`} />
-                            }
-                        </div>
-                    </button>
-                    {
-                        showIncompatibleWallets && notCompatibleWallets && notCompatibleWallets.map((wallet, index) => {
-                            return <span key={index}>
-                                {
-                                    wallet.addresses?.map((address) => {
+                                ) : (
+                                    <ChevronDown
+                                        className={`h-5 w-auto ${showIncompatibleWallets ? 'rotate-180' : ''} transition-all duration-200`}
+                                    />
+                                )}
+                            </div>
+                        </button>
+                        {showIncompatibleWallets &&
+                            notCompatibleWallets.map((wallet, index) => (
+                                <span key={index}>
+                                    {wallet.addresses?.map((address) => {
                                         const addressItem = {
                                             address: address,
                                             group: AddressGroup.ConnectedWallet,
-                                        }
+                                        };
 
-                                        return <div key={address} className="flex flex-col gap-2">
-                                            <div className={`group/addressItem w-full px-3 py-3 rounded-md hover:!bg-secondary-700 transition duration-200 opacity-50`}>
-                                                <AddressWithIcon addressItem={addressItem} connectedWallet={wallet} network={destination} />
+                                        return (
+                                            <div key={address} className="flex flex-col gap-2">
+                                                <div className="group/addressItem w-full px-3 py-3 rounded-md hover:!bg-secondary-700 transition duration-200 opacity-50">
+                                                    <AddressWithIcon
+                                                        addressItem={addressItem}
+                                                        connectedWallet={wallet}
+                                                        network={destination}
+                                                    />
+                                                </div>
                                             </div>
-                                        </div>
-                                    })
-                                }
-                            </span>
-                        })
-                    }
+                                        );
+                                    })}
+                                </span>
+                            ))}
+                    </div>
+                </ResizablePanel>
+            ) : (
+                <div
+                    className="relative group/notCompatible w-full px-3 py-3 rounded-md hover:!bg-secondary-700 transition duration-200 opacity-50"
+                >
+                    <div className="absolute right-2 top-1/2 -translate-y-1/2 w-max px-2 py-0.5 text-secondary-text font-medium text-sm rounded-md transition-opacity duration-200 bg-secondary-500 opacity-0 group-hover/notCompatible:opacity-100">
+                        Not compatible with {destination.display_name}
+                    </div>
+                    {notCompatibleWallets[0].addresses?.map((address) => {
+                        const addressItem = {
+                            address: address,
+                            group: AddressGroup.ConnectedWallet,
+                        };
+
+                        return (
+                            <AddressWithIcon
+                                key={address}
+                                addressItem={addressItem}
+                                connectedWallet={notCompatibleWallets[0]}
+                                network={destination}
+                            />
+                        );
+                    })}
                 </div>
-            </ResizablePanel >
+            ))
         }
+
 
     </>
 }
