f659f14c1fa260778031b7b00336f7b192e64f64
babkenmes <mesropyan.b@gmail.com>
2024-12-13 22:16:01 +0400

    Cleared config

--- next.config.js
+++ next.config.js
@@ -46,7 +46,6 @@ module.exports = (phase, { defaultConfig }) => {
     reactStrictMode: true,
     webpack: config => {
       config.resolve.fallback = { fs: false, net: false, tls: false };
-      config.externals.push('pino-pretty', 'lokijs', 'encoding')
       return config;
     },
     productionBrowserSourceMaps: true,
