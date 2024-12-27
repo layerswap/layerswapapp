33f8f41543effe7fc843e2bf0e8bbc0020879c37
paur94 <paruyryeprikyan9@gmail.com>
2024-12-27 16:02:54 +0400

    fixed immutable passport issue, amount placeholder issue

--- lib/gases/useSWRGas.tsx
+++ lib/gases/useSWRGas.tsx
@@ -5,14 +5,14 @@ import { GasResolver } from "./gasResolver"
 //TODO: Add type for address
 const useSWRGas = (address: any, network: Network | undefined, token?: Token) => {
 
-    const { data: gasData, error: gasError } = useSWR((network && address) ? `/gases/${address}/${network.name}/${token?.symbol}` : null, () => {
+    const { data: gasData, error: gasError, isLoading } = useSWR((network && address) ? `/gases/${address}/${network.name}/${token?.symbol}` : null, () => {
         if (!network || !token || !address) return
         return new GasResolver().getGas({ address, network, token })
     }, { refreshInterval: 60000 })
 
     return {
         gas: gasData,
-        isGasLoading: !gasError && !gasData,
+        isGasLoading: !gasError && !gasData && isLoading,
         gasError: gasError
     }
 }
