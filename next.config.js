5455817a87b118b5d791b18051ec9db27b22fa78
Aren <aren@bransfer.io>
2025-01-20 22:20:51 +0400

    fix build

--- next.config.js
+++ next.config.js
@@ -55,9 +55,6 @@ module.exports = (phase, { defaultConfig }) => {
     },
     productionBrowserSourceMaps: true,
     transpilePackages: ['@coral-xyz/anchor', '@solana/web3.js', '@imtbl/sdk', '@fuels/connectors', '@fuels/react', "@radix-ui/react-dismissable-layer"],
-    experimental: {
-      optimizePackageImports: ["viem/chains"]
-    },
   }
   if (process.env.APP_BASE_PATH) {
     nextConfig.basePath = process.env.APP_BASE_PATH
