68aa01ec6087617328274ac5eb8115603b619479
paur94 <paruyryeprikyan9@gmail.com>
2025-01-31 17:58:21 +0400

    added formButton component

--- components/Swap/Form/Form.tsx
+++ components/Swap/Form/Form.tsx
@@ -24,6 +24,7 @@ import { FormSourceWalletButton } from "../../Input/SourceWalletPicker";
 import { useSwapDataState, useSwapDataUpdate } from "../../../context/swap";
 import useWallet from "../../../hooks/useWallet";
 import { useSettingsState } from "../../../context/settings";
+import FormButton from "../FormButton";
 
 type Props = {
     partner?: Partner,
@@ -235,52 +236,18 @@ const SwapForm: FC<Props> = ({ partner }) => {
                 </div>
             </Widget.Content>
             <Widget.Footer>
-                {resolveButton(shouldConnectWallet, values, isValid, errors, isSubmitting, actionDisplayName, partner)}
+                <FormButton
+                    shouldConnectWallet={shouldConnectWallet}
+                    values={values}
+                    isValid={isValid}
+                    errors={errors}
+                    isSubmitting={isSubmitting}
+                    actionDisplayName={actionDisplayName}
+                    partner={partner}
+                />
             </Widget.Footer>
         </Form>
     </Widget>
-}
-
-const resolveButton = (shouldConnectWallet: boolean, values: SwapFormValues, isValid: boolean, errors: FormikErrors<SwapFormValues>, isSubmitting: boolean, actionDisplayName: string, partner: Partner | undefined) => {
-    if (shouldConnectWallet) {
-        return <FormSourceWalletButton />;
-    }
-
-    if (values?.to && !values?.destination_address) {
-        return (
-            <button className="border border-primary disabled:border-primary-900 items-center space-x-1 disabled:text-opacity-40 disabled:bg-primary-900 disabled:cursor-not-allowed relative w-full flex justify-center font-semibold rounded-md transform hover:brightness-125 transition duration-200 ease-in-out bg-primary text-primary-actionButtonText py-3 px-2 md:px-3 plausible-event-name=Swap+initiated">
-                <Address partner={partner}>
-                    {
-                        () => <div className="flex justify-center space-x-2">
-                            <span className="order-first absolute left-0 inset-y-0 flex items-center pl-3"><PlusIcon className="stroke-1" /></span>
-                            <span className="grow text-center">Enter destination address</span>
-                        </div>
-                    }
-                </Address>
-            </button>
-        );
-    }
-
-    return (
-        <SwapButton
-            className="plausible-event-name=Swap+initiated"
-            type='submit'
-            isDisabled={!isValid}
-            isSubmitting={isSubmitting}
-        >
-            {ActionText(errors, actionDisplayName)}
-        </SwapButton>
-    );
-};
-
-function ActionText(errors: FormikErrors<SwapFormValues>, actionDisplayName: string): string {
-    return errors.from?.toString()
-        || errors.to?.toString()
-        || errors.fromCurrency
-        || errors.toCurrency
-        || errors.currencyGroup
-        || errors.amount
-        || (actionDisplayName)
 }
 
 export default SwapForm
