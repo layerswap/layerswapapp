03e4bd0ebaf0d3f4b13155fcf9574dcaa10e0d16
babkenmes <mesropyan.b@gmail.com>
2025-03-03 20:35:40 +0400

    Fixed max amount on updated balance resolves FROONT-318

---
+++ components/Input/Amount/MinMax.tsx
@@ -0,0 +1,63 @@
+import SecondaryButton from "../../buttons/secondaryButton"
+import { useFormikContext } from "formik";
+import { SwapFormValues } from "../../DTOs/SwapFormValues";
+import useSWRBalance from "../../../lib/balances/useSWRBalance";
+import useSWRGas from "../../../lib/gases/useSWRGas";
+import { useSwapDataState } from "../../../context/swap";
+import { RouteNetwork, RouteToken, Token } from "../../../Models/Network";
+import { useMemo } from "react";
+import { resolveMacAllowedAmount } from "./helpers";
+
+type MinMaxProps = {
+    fromCurrency: RouteToken,
+    from: RouteNetwork,
+    limitsMaxAmount: number,
+    limitsMinAmount: number
+}
+
+const MinMax = (props: MinMaxProps) => {
+
+    const { setFieldValue } = useFormikContext<SwapFormValues>();
+    const { fromCurrency, from, limitsMinAmount, limitsMaxAmount } = props;
+
+    const { selectedSourceAccount } = useSwapDataState()
+
+    const { gas } = useSWRGas(selectedSourceAccount?.address, from, fromCurrency)
+    const { balance, mutate } = useSWRBalance(selectedSourceAccount?.address, from)
+
+    const gasAmount = gas || 0;
+
+    const handleSetMinAmount = () => {
+        setFieldValue('amount', limitsMinAmount);
+    }
+    const walletBalance = selectedSourceAccount?.address ? balance?.find(b => b?.network === from?.name && b?.token === fromCurrency?.symbol) : undefined
+    const native_currency = from?.token
+
+    let maxAllowedAmount: number = useMemo(() => {
+        return resolveMacAllowedAmount({ fromCurrency, limitsMinAmount, limitsMaxAmount, walletBalance, gasAmount, native_currency })
+    }, [fromCurrency, limitsMinAmount, limitsMaxAmount, walletBalance, gasAmount, native_currency])
+
+    const handleSetMaxAmount = async () => {
+        const updatedBalance = await mutate()
+        const updatedWalletBalance = updatedBalance?.find(b => b?.network === from?.name && b?.token === fromCurrency?.symbol)
+        const maxAllowedAmount = resolveMacAllowedAmount({ fromCurrency, limitsMinAmount, limitsMaxAmount, walletBalance: updatedWalletBalance, gasAmount, native_currency })
+        setFieldValue('amount', maxAllowedAmount);
+    }
+
+    return (
+        <div className="flex flex-col justify-center">
+            <div className="text-xs flex flex-col items-center space-x-1 md:space-x-2 ml-2 md:ml-5 px-2">
+                <div className="flex">
+                    <SecondaryButton disabled={!limitsMinAmount} onClick={handleSetMinAmount} size="xs">
+                        MIN
+                    </SecondaryButton>
+                    <SecondaryButton disabled={!maxAllowedAmount} onClick={handleSetMaxAmount} size="xs" className="ml-1.5">
+                        MAX
+                    </SecondaryButton>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MinMax
---
+++ components/Input/Amount/helpers.ts
@@ -0,0 +1,33 @@
+import { Balance } from "../../../Models/Balance"
+import { Token } from "../../../Models/Network"
+
+
+type ResoleMaxAllowedAmountProps = {
+    limitsMinAmount: number
+    limitsMaxAmount: number
+    walletBalance: Balance | undefined
+    gasAmount: number
+    fromCurrency: Token
+    native_currency: Token | undefined
+}
+
+export const resolveMacAllowedAmount = (props: ResoleMaxAllowedAmountProps) => {
+    const { limitsMinAmount, limitsMaxAmount, walletBalance, gasAmount, fromCurrency, native_currency } = props
+
+    if (!walletBalance || !isInRange({ value: walletBalance.amount, min: limitsMinAmount, max: limitsMaxAmount }))
+        return limitsMaxAmount
+
+    //calculate balance with reduced gas amount, if it is not in range we do not force the limits api min amount
+    const shouldPayGasWithTheToken = (native_currency?.symbol === fromCurrency?.symbol) || !native_currency
+    const payableAmount = walletBalance.amount - gasAmount
+    const payableIsInRange = isInRange({ value: payableAmount, min: limitsMinAmount, max: limitsMaxAmount })
+
+    if (!shouldPayGasWithTheToken || !payableIsInRange)
+        return walletBalance.amount
+
+    return Number(payableAmount.toFixed(fromCurrency?.decimals))
+}
+
+const isInRange = ({ value, min, max }: { value: Number, min: Number, max: Number }) => {
+    return value >= min && value <= max
+}
