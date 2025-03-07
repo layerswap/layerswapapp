7bf68bee8387b02e32e362ac29ce7086a582a4df
babkengevorgyan <babgev@gmail.com>
2025-03-08 01:00:18 +0400

    Add nft balance checking for nft campaigns

---
+++ lib/nft/types.ts
@@ -0,0 +1,12 @@
+import { Network } from "../../Models/Network";
+
+export interface NftBalanceProps {
+    address: string;
+    network: Network;
+    contractAddress: string;
+}
+
+export interface Provider {
+    supportsNetwork(network: Network): boolean;
+    getBalance(props: NftBalanceProps): Promise<number>;
+} 
