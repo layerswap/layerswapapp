636ca27b6f25217839e678d830a3db3a2533dd62
Aren <aren@bransfer.io>
2025-04-01 19:39:43 +0400

    Remove unnecessary ESLint disable comment in OkxWalletAdapter

--- lib/wallets/tron/connectors/okxWallet.ts
+++ lib/wallets/tron/connectors/okxWallet.ts
@@ -369,7 +369,6 @@ export class OkxWalletAdapter extends Adapter {
         let state = this.state;
         let address = this.address;
         if (supportOkxWallet()) {
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             this._wallet = window.okxwallet!.tronLink;
             this._listenEvent();
             if (state === AdapterState.Connected) {
