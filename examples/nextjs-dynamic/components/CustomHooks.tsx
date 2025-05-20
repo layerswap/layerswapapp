53b57b2d2bdecc8cc658ed9d17ffbebb1f83fa47
Aren <aren@bransfer.io>
2025-05-20 21:16:52 +0400

    fix: add missing function name to CustomHooks component

--- examples/nextjs-dynamic/components/CustomHooks.tsx
+++ examples/nextjs-dynamic/components/CustomHooks.tsx
@@ -1,7 +1,7 @@
 import { WalletHooksProvider } from "@layerswap/widget";
 import useCustomEVM from "../hooks/useCustomEVM";
 
-export default function ({ children }: { children: JSX.Element | JSX.Element[] }) {
+export default function CustomHooks({ children }: { children: JSX.Element | JSX.Element[] }) {
     const customEvm = useCustomEVM()
     return <WalletHooksProvider overides={{ evm: customEvm }}>
         {children}
