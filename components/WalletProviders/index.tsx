6850454271766afea7ec0f3d15c2b80dfb9fc1cc
paur94 <paruyryeprikyan9@gmail.com>
2025-05-21 20:12:44 +0400

    fixed bug with popover close

--- components/WalletProviders/index.tsx
+++ components/WalletProviders/index.tsx
@@ -9,6 +9,7 @@ import TronProvider from "./TronProvider";
 import { WalletProvidersProvider } from "../../context/walletProviders";
 import { WalletModalProvider } from "../WalletModal";
 import FuelProviderWrapper from "./FuelProvider";
+import { StarknetWalletProvider } from "../../context/starknetWalletContext";
 
 const WalletsProviders: FC<{ children: JSX.Element | JSX.Element[], basePath: string, themeData: ThemeData, appName: string | undefined }> = ({ children, basePath, themeData, appName }) => {
     return (
@@ -16,17 +17,19 @@ const WalletsProviders: FC<{ children: JSX.Element | JSX.Element[], basePath: st
             <SolanaProvider>
                 <TronProvider>
                     <StarknetProvider>
-                        <Wagmi>
-                            <FuelProviderWrapper>
-                                <ImtblPassportProvider>
-                                    <WalletModalProvider>
-                                        <WalletProvidersProvider>
-                                            {children}
-                                        </WalletProvidersProvider>
-                                    </WalletModalProvider>
-                                </ImtblPassportProvider>
-                            </FuelProviderWrapper>
-                        </Wagmi>
+                        <StarknetWalletProvider>
+                            <Wagmi>
+                                <FuelProviderWrapper>
+                                    <ImtblPassportProvider>
+                                        <WalletModalProvider>
+                                            <WalletProvidersProvider>
+                                                {children}
+                                            </WalletProvidersProvider>
+                                        </WalletModalProvider>
+                                    </ImtblPassportProvider>
+                                </FuelProviderWrapper>
+                            </Wagmi>
+                        </StarknetWalletProvider>
                     </StarknetProvider>
                 </TronProvider>
             </SolanaProvider>
