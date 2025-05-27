07da9a4e59e3326e971b96f56e41d692d477788c
babkenmes <mesropyan.b@gmail.com>
2025-05-27 16:52:16 +0400

    Added select on token rows

--- components/Input/RoutePicker/Rows/index.tsx
+++ components/Input/RoutePicker/Rows/index.tsx
@@ -32,12 +32,17 @@ export default function Row({ item, direction, selectedRoute, selectedToken, tog
     if (item.type == "network_token" || item.type == "exchange_token") {
         const token = item.route.token
         const route = item.route.route
-        return <CurrencySelectItemDisplay
-            item={token}
-            selected={false}
-            route={route}
-            direction={direction}
-        />
+        const isSelected = selectedRoute === route.name && selectedToken === token.symbol
+        return <div
+            className={`pl-5 cursor-pointer hover:bg-secondary-300 ${isSelected ? "bg-secondary-300" : ""} outline-none disabled:cursor-not-allowed`}
+            onClick={() => onSelect(route, token)}
+        ><CurrencySelectItemDisplay
+                item={token}
+                selected={false}
+                route={route}
+                direction={direction}
+            />
+        </div>
     }
     if (item.type == "group_title")
         return <div className="text-primary-text-placeholder text-base font-medium leading-5 mb-2 px-3 sticky top-0 z-50" style={{ position: 'sticky', top: 0, transform: 'none' }}>{item.text}</div>
