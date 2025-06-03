0b0070df71a52704879252ce8aa09e16ab7bef84
Aren <aren@bransfer.io>
2025-06-03 12:27:33 +0400

    fix: change Drawer.Content positioning from fixed to absolute

--- packages/widget/src/components/Modal/vaulModal.tsx
+++ packages/widget/src/components/Modal/vaulModal.tsx
@@ -112,7 +112,7 @@ const Comp: FC<VaulDrawerProps> = ({ children, show, setShow, header, descriptio
 
                 <Drawer.Content
                     data-testid="content"
-                    className={clsx('fixed flex flex-col bg-secondary-900 rounded-t-xl bottom-0 left-0 right-0 h-full z-50 pb-6 text-primary-text !ring-0 !outline-none ', {
+                    className={clsx('absolute flex flex-col bg-secondary-900 rounded-t-xl bottom-0 left-0 right-0 h-full z-50 pb-6 text-primary-text !ring-0 !outline-none ', {
                         '!border-none !rounded-none': snap === 1,
                         '!fixed sm:!absolute': AppSettings.ThemeData?.enablePortal == true,
                     })}
