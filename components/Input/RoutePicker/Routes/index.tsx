b44ea6a5ebccfb7addef43fb1afaa777a6e55daf
Aren <aren@bransfer.io>
2025-06-06 15:43:18 +0400

    Stick if network has more then one tokeb and improve balance formatting in RoutePicker components

--- components/Input/RoutePicker/Routes/index.tsx
+++ components/Input/RoutePicker/Routes/index.tsx
@@ -1,16 +1,13 @@
 import { NetworkRoute, NetworkRouteToken } from "../../../../Models/Network";
 import useWallet from "../../../../hooks/useWallet";
-import useSWRBalance from "../../../../lib/balances/useSWRBalance";
 import { SwapDirection } from "../../../DTOs/SwapFormValues";
 import { truncateDecimals } from "../../../utils/RoundDecimals";
 import Image from 'next/image'
 import { SelectItem } from "../../../Select/CommandNew/SelectItem/Index";
-import { useMemo } from "react";
 import { Exchange } from "../../../../Models/Exchange";
 import { Route, RouteToken } from "../../../../Models/Route";
 import { ChevronDown } from "lucide-react";
 import RoutePickerIcon from "../../../icons/RoutePickerPlaceholder";
-import { Wallet } from "../../../../Models/WalletProvider";
 import { useBalance } from "../../../../lib/balances/providers/useBalance";
 
 type TokenItemProps = {
@@ -50,14 +47,14 @@ export const NetworkTokenTitle = (props: NetworkTokenItemProps) => {
     const activeAddress = provider?.activeWallet
     const { balances } = useBalance(activeAddress?.address, route)
     const tokenbalance = balances?.find(b => b.token === item.symbol)
-    const formatted_balance_amount = tokenbalance?.amount ? Number(truncateDecimals(tokenbalance?.amount, item.precision)) : 0
+    const formatted_balance_amount = tokenbalance?.amount ? truncateDecimals(tokenbalance?.amount, item.precision) : 0
     const balanceAmountInUsd = (item?.price_in_usd * formatted_balance_amount).toFixed(2)
 
     return <SelectItem.DetailedTitle title={item.symbol} secondary={route.display_name} secondaryLogoSrc={route.logo}>
         {
             (allbalancesLoaded && tokenbalance && Number(formatted_balance_amount) > 0) ? (
                 <span className="text-sm text-secondary-text text-right my-auto leading-4 font-medium">
-                    <div className="text-primary-text"> {formatted_balance_amount}</div>
+                    <div className="text-primary-text"> {formatted_balance_amount.toFixed(item.precision)}</div>
                     {Number(tokenbalance?.amount) > 0 && <div>${balanceAmountInUsd}</div>}
                 </span>
             ) : <></>}
