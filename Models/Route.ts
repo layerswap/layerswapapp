143b5bcdeeaf582d31542a70a84e3e123377965d
paur94 <paruyryeprikyan9@gmail.com>
2025-06-11 20:27:03 +0400

    added types

--- Models/Route.ts
+++ Models/Route.ts
@@ -1,6 +1,8 @@
 import { Exchange, ExchangeToken } from "./Exchange";
 import { NetworkRouteToken, NetworkRoute } from "./Network";
 
+export type RouteToken = NetworkRouteToken | ExchangeToken
+export type Route = ({ cex: true } & Exchange | { cex?: false } & NetworkRoute)
 export type NetworkElement = {
     type: 'network';
     route: NetworkRoute;
