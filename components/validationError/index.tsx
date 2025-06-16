eab6a81be8bd6b77fbda98d7d81628a4f872f06c
Aren <aren@bransfer.io>
2025-06-16 16:50:21 +0400

    Refactor components for improved structure and styling consistency

--- components/validationError/index.tsx
+++ components/validationError/index.tsx
@@ -7,7 +7,7 @@ const ValidationError: React.FC = () => {
     if (!validationMessage) return null;
 
     return (
-        <div className={`p-2.5 my-2.5 relative rounded-md bg-secondary-700 border-l-8 ${validationDetails.type === "warning" ? "border-orange-400" : "border-red-400"}`}>
+        <div className={`p-2.5 my-2.5 relative rounded-xl bg-secondary-400 border-l-8 ${validationDetails.type === "warning" ? "border-orange-400" : "border-red-400"}`}>
             <div className='flex items-center'>
                 {validationDetails.icon}
                 <p className='text-white font-medium ml-1'>{validationDetails.title}</p>
