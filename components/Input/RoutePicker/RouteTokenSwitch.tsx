39c2a5a15b99cb69397b0a9e2d08a6c2576c72c8
paur94 <paruyryeprikyan9@gmail.com>
2025-06-16 17:31:46 +0400

    added tooltips for route token switch

--- components/Input/RoutePicker/RouteTokenSwitch.tsx
+++ components/Input/RoutePicker/RouteTokenSwitch.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { motion } from "framer-motion";
 import TokenIcon from "../../icons/TokenIcon";
 import { Globe } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/shadcn/tooltip";
 
 interface RouteTokenSwitchProps {
     showTokens: boolean;
@@ -18,22 +19,36 @@ const RouteTokenSwitch: FC<RouteTokenSwitchProps> = ({ showTokens, setShowTokens
                     animate={{ x: showTokens ? 53.5 : 1.5 }}
                     transition={{ type: "spring", stiffness: 300, damping: 30 }}
                 />
-                <button
-                    className="z-10 flex items-center justify-center rounded-2xl px-4 py-1"
-                    onClick={() => setShowTokens(false)}
-                >
-                    <Globe
-                        className={`${showTokens ? "text-primary-text-placeholder" : "text-primary-text"} h-5 w-5`}
-                    />
-                </button>
-                <button
-                    className="z-10 flex items-center justify-center rounded-2xl px-4 py-1"
-                    onClick={() => setShowTokens(true)}
-                >
-                    <TokenIcon
-                        className={`${showTokens ? "text-primary-text" : "text-primary-text-placeholder"} h-5 w-5`}
-                    />
-                </button>
+                <Tooltip>
+                    <TooltipTrigger>
+                        <button
+                            className="z-10 flex items-center justify-center rounded-2xl px-4 py-1 relative"
+                            onClick={() => setShowTokens(false)}
+                        >
+                            <Globe
+                                className={`${showTokens ? "text-primary-text-placeholder" : "text-primary-text"} h-5 w-5`}
+                            />
+                        </button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        <p>Group by Network</p>
+                    </TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                    <TooltipTrigger>
+                        <button
+                            className="z-10 flex items-center justify-center rounded-2xl px-4 py-1 relative"
+                            onClick={() => setShowTokens(true)}
+                        >
+                            <TokenIcon
+                                className={`${showTokens ? "text-primary-text" : "text-primary-text-placeholder"} h-5 w-5`}
+                            />
+                        </button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        <p>Group by Token</p>
+                    </TooltipContent>
+                </Tooltip>
             </div>
         </div>
     );
