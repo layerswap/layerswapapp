40205cd60c09d26df22b7c226221f85ea313ae3f
Aren <aren@bransfer.io>
2025-06-26 19:32:54 +0400

    fix form tab position issue

--- components/Swap/Form/NetworkExchangeTabs.tsx
+++ components/Swap/Form/NetworkExchangeTabs.tsx
@@ -14,25 +14,26 @@ export default function NetworkExchangeTabs({ networkForm, exchangeForm }: Props
 
     return (
         <Tabs defaultValue="network" className="relative" >
-            <motion.div
-                onHoverStart={() => setHovered(true)}
-                onHoverEnd={() => setHovered(false)}
-                animate={{ width: hovered ? 180 : 48 }}
-                className="absolute right-[99%] top-24 overflow-hidden rounded-l-lg"
-                layoutId="bubble"
-                transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
-            >
-                <TabsList className="flex flex-col bg-secondary-500 h-full !pl-1.5 !pr-3 py-2 w-full space-y-2">
-                    <TabsTrigger value="network" className="w-full text-white flex items-center justify-start !p-1 data-[state=active]:bg-secondary-300 hover:bg-secondary-100">
-                        <NetworkTabIcon className="h-6 w-6" />
-                        {hovered && <span className="text-sm whitespace-nowrap">Swap</span>}
-                    </TabsTrigger>
-                    <TabsTrigger value="exchange" className="w-full text-white flex items-center justify-start !p-1 data-[state=active]:bg-secondary-300 hover:bg-secondary-100">
-                        <ExchangeTabIcon className="h-6 w-6" />
-                        {hovered && <span className="text-sm whitespace-nowrap">Deposit from CEX</span>}
-                    </TabsTrigger>
-                </TabsList>
-            </motion.div>
+            <div className='relative'>
+                <motion.div
+                    onHoverStart={() => setHovered(true)}
+                    onHoverEnd={() => setHovered(false)}
+                    animate={{ width: hovered ? 180 : 48 }}
+                    className="absolute right-[99%] top-24 overflow-hidden rounded-l-lg"
+                    transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+                >
+                    <TabsList className="flex flex-col bg-secondary-500 h-full !pl-1.5 !pr-3 py-2 w-full space-y-2">
+                        <TabsTrigger value="network" className="w-full text-white grid grid-cols-4 justify-start !p-1 data-[state=active]:bg-secondary-300 hover:bg-secondary-100 overflow-hidden">
+                            <NetworkTabIcon className="h-6 w-6" />
+                            {hovered && <span className="text-sm whitespace-nowrap">Swap</span>}
+                        </TabsTrigger>
+                        <TabsTrigger value="exchange" className="w-full text-white flex items-center justify-start !p-1 data-[state=active]:bg-secondary-300 hover:bg-secondary-100 overflow-hidden">
+                            <ExchangeTabIcon className="h-6 w-6" />
+                            {hovered && <span className="text-sm whitespace-nowrap">Deposit from CEX</span>}
+                        </TabsTrigger>
+                    </TabsList>
+                </motion.div>
+            </div>
 
             <div>
                 <TabsContent value="network" className='!mt-0'>
