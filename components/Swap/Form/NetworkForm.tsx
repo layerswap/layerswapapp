630086779bcf43fe93a9ea7ce2b7c0cc5a9eaf49
paur94 <paruyryeprikyan9@gmail.com>
2025-06-27 14:38:49 +0400

    removed exchange picker from networks tab

--- components/Swap/Form/NetworkForm.tsx
+++ components/Swap/Form/NetworkForm.tsx
@@ -109,14 +109,6 @@ const NetworkForm: FC<Props> = ({ partner, setOpenRefuelModal }) => {
                                 !(query?.hideTo && values?.to) && <DestinationPicker partner={partner} />
                             }
                         </div>
-                        {
-                            (((fromExchange && destination) || (toExchange && source)) && currencyGroup) &&
-                            <div className="mb-6 leading-4">
-                                <ResizablePanel>
-                                    <CexNetworkPicker direction={fromExchange ? 'from' : 'to'} partner={partner} />
-                                </ResizablePanel>
-                            </div>
-                        }
                         <div className="w-full">
                             {values.amount &&
                                 <ReserveGasNote onSubmit={handleReserveGas} />
