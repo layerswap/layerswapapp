49ad486456723368ede382cc888c71f5c8d1fae5
paur94 <paruyryeprikyan9@gmail.com>
2025-07-29 19:53:36 +0400

    test-text

--- components/Swap/Withdraw/QuoteUpdated.tsx
+++ components/Swap/Withdraw/QuoteUpdated.tsx
@@ -23,7 +23,9 @@ const QuoteUpdated: FC<QuoteUpdatedProps> = ({
             <div className="p-3 bg-secondary-500 rounded-lg mb-3 w-fit mx-auto">
                 <AlertIcon className='h-11 w-11' />
             </div>
-            <h2 className="text-primary-text text-xl font-medium text-center mb-3">{isBelowMin ? "Minimum" : "Maximum"} Amount Adjusted</h2>
+            <h2 className="text-primary-text text-xl font-medium text-center mb-3">
+                <span>{isBelowMin ? "Minimum" : "Maximum"}</span> <span>Amount Adjusted</span>
+            </h2>
             <p className="text-center text-secondary-text text-base mb-6">
                 The {isBelowMin ? "minimum" : "maximum"} amount you can send using {network} is {isBelowMin ? minAllowedAmount : maxAllowedAmount} {token}. We’ll adjust your transfer to this limit to proceed.
             </p>
