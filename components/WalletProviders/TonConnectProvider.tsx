70a581ff7efae2ad7a557d598fd313e1f58e1690
paur94 <paruyryeprikyan9@gmail.com>
2025-08-26 14:48:38 +0400

    removed unnecessary slash

--- components/WalletProviders/TonConnectProvider.tsx
+++ components/WalletProviders/TonConnectProvider.tsx
@@ -14,7 +14,7 @@ const TonConnectProvider = ({ children, basePath, themeData, appName }: { childr
 
         return "#" + componentToHex(Number(rgbArray[0])) + componentToHex(Number(rgbArray[1])) + componentToHex(Number(rgbArray[2]));
     }
-    const manifestUrl = `https://${process.env.NEXT_PUBLIC_VERCEL_URL ? `${process.env.NEXT_PUBLIC_VERCEL_URL}${basePath ? `/${basePath}` : ''}` : 'layerswap.io/app'}/tonconnect-manifest.json`
+    const manifestUrl = `https://${process.env.NEXT_PUBLIC_VERCEL_URL ? `${process.env.NEXT_PUBLIC_VERCEL_URL}${basePath ? `${basePath}` : ''}` : 'layerswap.io/app'}/tonconnect-manifest.json`
 
     return (
         <TonConnectUIProvider
