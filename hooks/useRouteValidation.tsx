40f1609a7b2c972f430aef6f0a7ee255405ca095
babkenmes <mesropyan.b@gmail.com>
2025-09-18 13:52:11 +0400

    renamed icon classes variable

--- hooks/useRouteValidation.tsx
+++ hooks/useRouteValidation.tsx
@@ -7,7 +7,7 @@ import { useFormikContext } from 'formik';
 import useWallet from './useWallet';
 import { QuoteError } from './useFee';
 
-const ICON_CLASS_YELLOW = 'w-5 h-5 text-warning-foreground';
+const ICON_CLASSES_WARNING = 'w-5 h-5 text-warning-foreground';
 
 interface ValidationDetails {
     title?: string;
@@ -32,29 +32,29 @@ export function resolveRouteValidation(quoteError?: QuoteError) {
     if (query?.lockToAsset) {
         if (fromCurrency?.status === 'not_found') {
             validationMessage = `Transfers from ${fromDisplayName} ${fromCurrency?.symbol || fromCurrency?.symbol} to this token are not supported`;
-            validationDetails = { title: 'Route Unavailable', type: 'warning', icon: <RouteOff className={ICON_CLASS_YELLOW} /> };
+            validationDetails = { title: 'Route Unavailable', type: 'warning', icon: <RouteOff className={ICON_CLASSES_WARNING} /> };
         }
         else if (fromCurrency?.status === 'inactive') {
             validationMessage = `Sorry, transfers of ${fromCurrency?.symbol} from ${fromDisplayName} are not available at the moment. Please try later.`;
-            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASS_YELLOW} /> };
+            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASSES_WARNING} /> };
         }
         else if (!toCurrency) {
             validationMessage = `Sorry, transfers of ${query?.toAsset} to ${toDisplayName || query.to} are not available at the moment. Please try later.`;
-            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASS_YELLOW} /> };
+            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASSES_WARNING} /> };
         }
     }
     else if (query?.lockFromAsset) {
         if (toCurrency?.status === 'not_found') {
             validationMessage = `Transfers to ${toDisplayName} ${toCurrency?.symbol} from this token are not supported`;
-            validationDetails = { title: 'Route Unavailable', type: 'warning', icon: <RouteOff className={ICON_CLASS_YELLOW} /> };
+            validationDetails = { title: 'Route Unavailable', type: 'warning', icon: <RouteOff className={ICON_CLASSES_WARNING} /> };
         }
         else if (toCurrency?.status === 'inactive') {
             validationMessage = `Sorry, transfers of ${toCurrency?.symbol} to ${toDisplayName} are not available at the moment. Please try later.`;
-            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASS_YELLOW} /> };
+            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASSES_WARNING} /> };
         }
         else if (!fromCurrency) {
             validationMessage = `Sorry, transfers of ${query?.fromAsset} from ${fromDisplayName || query.from} are not available at the moment. Please try later.`;
-            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASS_YELLOW} /> };
+            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASSES_WARNING} /> };
         }
     }
     else if (toCurrency?.status === 'inactive' || fromCurrency?.status === 'inactive') {
@@ -65,39 +65,39 @@ export function resolveRouteValidation(quoteError?: QuoteError) {
 
         if (unfilteredDestinationCurrency?.status === 'inactive') {
             validationMessage = `Sorry, transfers of ${toCurrency?.symbol} to ${toDisplayName} are not available at the moment. Please try later.`;
-            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASS_YELLOW} /> };
+            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASSES_WARNING} /> };
         }
         else if (unfilteredSourceCurrency?.status === 'inactive') {
             validationMessage = `Sorry, transfers of ${fromCurrency?.symbol} from ${fromDisplayName} are not available at the moment. Please try later.`;
-            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASS_YELLOW} /> };
+            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASSES_WARNING} /> };
         }
         else {
             validationMessage = `Please change one of the selected tokens or try later.`;
-            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASS_YELLOW} /> };
+            validationDetails = { title: 'Temporarily unavailable.', type: 'warning', icon: <Info className={ICON_CLASSES_WARNING} /> };
         }
     }
     else if (!validatingSource && !validatingDestination && (toCurrency?.status === 'not_found' || fromCurrency?.status === 'not_found')) {
         validationMessage = 'Please change one of the selected tokens';
-        validationDetails = { title: 'Route Unavailable', type: 'warning', icon: <RouteOff className={ICON_CLASS_YELLOW} /> };
+        validationDetails = { title: 'Route Unavailable', type: 'warning', icon: <RouteOff className={ICON_CLASSES_WARNING} /> };
     }
 
     if (((from?.name && from?.name.toLowerCase() === query.sameAccountNetwork?.toLowerCase()) || (to?.name && to?.name.toLowerCase() === query.sameAccountNetwork?.toLowerCase()))) {
         const network = from?.name.toLowerCase() === query.sameAccountNetwork?.toLowerCase() ? from : to;
         if ((selectedSourceAccount && destination_address && selectedSourceAccount?.address?.toLowerCase() !== destination_address?.toLowerCase())) {
             validationMessage = `Transfers between ${network?.display_name} and other chains are only allowed within the same account. Please make sure you're using the same address on both source and destination.`;
-            validationDetails = { title: 'Action Needed', type: 'warning', icon: <RouteOff className={ICON_CLASS_YELLOW} /> };
+            validationDetails = { title: 'Action Needed', type: 'warning', icon: <RouteOff className={ICON_CLASSES_WARNING} /> };
         }
 
         if (values.depositMethod === "deposit_address") {
             validationMessage = `Manually transferring between ${from?.display_name} and ${to?.display_name} networks is not supported.`;
-            validationDetails = { title: 'Manual Transfer is not supported', type: 'warning', icon: <RouteOff className={ICON_CLASS_YELLOW} /> };
+            validationDetails = { title: 'Manual Transfer is not supported', type: 'warning', icon: <RouteOff className={ICON_CLASSES_WARNING} /> };
         }
 
     }
 
     if (quoteError) {
         validationMessage = '';
-        validationDetails = { title: quoteMessage || 'Unable to retrieve quote', type: 'warning', icon: <RouteOff className={ICON_CLASS_YELLOW} /> };
+        validationDetails = { title: quoteMessage || 'Unable to retrieve quote', type: 'warning', icon: <RouteOff className={ICON_CLASSES_WARNING} /> };
     }
 
     const value = useMemo(() => ({
