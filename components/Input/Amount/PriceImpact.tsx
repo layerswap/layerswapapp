61a23a0069b15bf25a924770108fab916a9033c6
paur94 <paruyryeprikyan9@gmail.com>
2025-09-23 16:18:55 +0400

    price impact calculations

--- components/Input/Amount/PriceImpact.tsx
+++ components/Input/Amount/PriceImpact.tsx
@@ -2,6 +2,7 @@ import { FC, useMemo } from "react";
 import { SwapQuote } from "@/lib/apiClients/layerSwapApiClient";
 import { Triangle } from "lucide-react";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/shadcn/tooltip";
+import { truncateDecimals } from "@/components/utils/RoundDecimals";
 
 type PriceImpactProps = {
     quote: SwapQuote | undefined;
@@ -9,26 +10,43 @@ type PriceImpactProps = {
 export const PriceImpact: FC<PriceImpactProps> = ({ quote }) => {
     const receive_amount = quote?.receive_amount
     const requested_amount = quote?.requested_amount
+    const sourceTokenPriceInUsd = quote?.source_token?.price_in_usd
+    const destinationTokenPriceInUsd = quote?.destination_token?.price_in_usd
 
     const toAmountUSD = useMemo(() => {
-        if (!receive_amount || !quote?.destination_token?.price_in_usd) return undefined;
-        return receive_amount * quote.destination_token.price_in_usd;
-    }, [receive_amount, quote?.destination_token?.price_in_usd]);
+        if (!receive_amount || !destinationTokenPriceInUsd) return undefined;
+        return receive_amount * destinationTokenPriceInUsd;
+    }, [receive_amount, destinationTokenPriceInUsd]);
 
     const fromAmountUSD = useMemo(() => {
-        if (!requested_amount || !quote?.source_token?.price_in_usd) return undefined;
-        return requested_amount * quote.source_token.price_in_usd;
-    }, [requested_amount, quote?.source_token?.price_in_usd]);
+        if (!requested_amount || !sourceTokenPriceInUsd) return undefined;
+        return requested_amount * sourceTokenPriceInUsd;
+    }, [requested_amount, sourceTokenPriceInUsd]);
 
     const priceImpact = useMemo(() => {
         if (fromAmountUSD === undefined || toAmountUSD === undefined) return undefined;
         return Number((toAmountUSD - fromAmountUSD).toFixed(2));
     }, [fromAmountUSD, toAmountUSD]);
 
+    const serviceFee = useMemo(() => {
+        if (quote?.service_fee == null || sourceTokenPriceInUsd == null) return undefined;
+        return truncateDecimals(quote?.service_fee * sourceTokenPriceInUsd, quote?.source_token?.decimals || 6);
+    }, [quote?.service_fee, sourceTokenPriceInUsd]);
+
+    const bridgeExpenses = useMemo(() => {
+        if (quote?.blockchain_fee == null || sourceTokenPriceInUsd == null) return undefined;
+        return truncateDecimals(quote?.blockchain_fee * sourceTokenPriceInUsd, quote?.source_token?.decimals || 6);
+    }, [quote?.blockchain_fee, sourceTokenPriceInUsd]);
+
+    const marketImpact = useMemo(() => {
+        if (priceImpact === undefined || serviceFee === undefined || bridgeExpenses === undefined) return undefined;
+        return truncateDecimals(priceImpact - Number(serviceFee) - Number(bridgeExpenses), quote?.source_token?.decimals || 6);
+    }, [priceImpact, serviceFee, bridgeExpenses, quote?.source_token?.decimals]);
+
     if (priceImpact === undefined) return null;
 
     return (<>
-        <Tooltip>
+        <Tooltip open={true} >
             <TooltipTrigger asChild>
                 <span className="flex items-center text-sm text-secondary-text cursor-default">
                     <span className="flex items-center gap-0.5">
@@ -42,9 +60,40 @@ export const PriceImpact: FC<PriceImpactProps> = ({ quote }) => {
                     </span>
                 </span>
             </TooltipTrigger>
-            <TooltipContent side="top" align="center" className="!bg-secondary-300 !border-secondary-300 !text-secondary-text text-xs font-medium">
+            <TooltipContent side="top" align="center" className="!bg-secondary-300 !border-secondary-300 !text-secondary-text text-xs font-normal">
+                <p className="text-primary-text font-medium text-sm flex items-baseline space-x-0.5 mb-1">Price impact:
+                    <Triangle
+                        aria-label={priceImpact < 0 ? "Negative price impact" : "Positive price impact"}
+                        className={`ml-1 w-3 h-3 stroke-1 fill-current transition-transform ${priceImpact < 0 ? "rotate-180" : ""
+                            }`}
+                    />
+                    <span>$</span>{priceImpact}
+                </p>
                 <p>This is the difference between the USD value of</p>
                 <p>the token you send and the token you receive.</p>
+                <ul className="mt-3 space-y-2 ">
+                    <li className="list-none flex justify-between">
+                        <span>Market impact</span>
+                        <span className="text-primary-text">
+                            <span className="mr-0.5">$</span>
+                            {marketImpact}
+                        </span>
+                    </li>
+                    <li className="list-none flex justify-between">
+                        <span>Bridge expenses</span>
+                        <span className="text-primary-text">
+                            <span className="mr-0.5">$</span>
+                            {bridgeExpenses}
+                        </span>
+                    </li>
+                    <li className="list-none flex justify-between">
+                        <span>Layerswap fees</span>
+                        <span className="text-primary-text">
+                            <span className="mr-0.5">$</span>
+                            {serviceFee}
+                        </span>
+                    </li>
+                </ul>
             </TooltipContent>
         </Tooltip>
     </>)
