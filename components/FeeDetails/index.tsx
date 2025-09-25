8265c8cb59c690b50b8cd18e7ea9a6ac4c6285b0
babkenmes <mesropyan.b@gmail.com>
2025-09-25 20:06:02 +0400

    line break

--- components/FeeDetails/index.tsx
+++ components/FeeDetails/index.tsx
@@ -112,7 +112,8 @@ const DetailsButton: FC<QuoteComponentProps> = ({ quote: quoteData, isQuoteLoadi
                             <GasIcon className='h-4 w-4 text-secondary-text' /> : <ExchangeGasIcon className='h-5 w-5 text-secondary-text' />
                         }
                     </div>
-                    <NumberFlow className="text-primary-text text-sm leading-6" value={gasFeeInUsd < 0.01 ? '0.01' : gasFeeInUsd} format={{ style: 'currency', currency: 'USD' }} prefix={gasFeeInUsd < 0.01 ? '<' : undefined} />                    <div className="ml-3 w-px h-3 bg-primary-text-tertiary rounded-2xl" />
+                    <NumberFlow className="text-primary-text text-sm leading-6" value={gasFeeInUsd < 0.01 ? '0.01' : gasFeeInUsd} format={{ style: 'currency', currency: 'USD' }} prefix={gasFeeInUsd < 0.01 ? '<' : undefined} />
+                    <div className="ml-3 w-px h-3 bg-primary-text-tertiary rounded-2xl" />
                 </div>
             }
             {
