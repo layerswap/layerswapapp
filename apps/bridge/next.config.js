9cd4cf1ea6e8fb74c71f2c8daae3bec7bdd9d83c
Aren <aren@bransfer.io>
2025-09-29 23:41:30 +0400

    still trying

--- apps/bridge/next.config.js
+++ apps/bridge/next.config.js
@@ -49,7 +49,7 @@ module.exports = (phase, { defaultConfig }) => {
       return config;
     },
     productionBrowserSourceMaps: true,
-    transpilePackages: ['@imtbl/sdk', '@fuels/connectors', '@fuels/react', "@radix-ui/react-dismissable-layer"]
+    transpilePackages: ['@imtbl/sdk', '@fuels/connectors', '@fuels/react', "@radix-ui/react-dismissable-layer", '@layerswap/widget']
   }
   if (process.env.APP_BASE_PATH) {
     nextConfig.basePath = process.env.APP_BASE_PATH
