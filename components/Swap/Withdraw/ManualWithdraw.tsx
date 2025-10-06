2a2508fd1060a4e92c8bbb917a6d62f54348ca01
paur94 <paruyryeprikyan9@gmail.com>
2025-10-06 12:00:19 +0400

    reverted check

--- components/Swap/Withdraw/ManualWithdraw.tsx
+++ components/Swap/Withdraw/ManualWithdraw.tsx
@@ -180,7 +180,7 @@ const ManualWithdraw: FC<Props> = ({ swapBasicData, quote, depositActions, refue
     return (
         <div className="rounded-lg space-y-3 text-white w-full">
             <>
-                {(!loading || exchangeSourceNetworksLoading) ? (
+                {(loading || exchangeSourceNetworksLoading) ? (
                     <>
                         <SkeletonStep number={1} />
                         <SkeletonStep number={2} />
