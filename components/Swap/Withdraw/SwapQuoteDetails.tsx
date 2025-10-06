ac54e07d0897a89224d8e6406114d56b57a6ff7d
paur94 <paruyryeprikyan9@gmail.com>
2025-10-06 12:51:19 +0400

    quote loading fix

--- components/Swap/Withdraw/SwapQuoteDetails.tsx
+++ components/Swap/Withdraw/SwapQuoteDetails.tsx
@@ -12,10 +12,10 @@ type Props = {
 export const SwapQuoteDetails: FC<Props> = ({ swapBasicData: swapData, quote, refuel, quoteIsLoading }) => {
     const { source_network, destination_network, use_deposit_address, destination_token, requested_amount, source_token, destination_address } = swapData || {}
 
-    if (!requested_amount || !source_network || !destination_network || !quote) return <div className='h-[56px] w-full rounded-xl bg-secondary-500 animate-pulse'/>
+    if (!quote) return <div className='h-[56px] w-full rounded-xl bg-secondary-500 animate-pulse'/>
 
     const values: SwapValues = {
-        amount: requested_amount.toString(),
+        amount: requested_amount?.toString(),
         from: source_network,
         to: destination_network,
         fromAsset: source_token,
