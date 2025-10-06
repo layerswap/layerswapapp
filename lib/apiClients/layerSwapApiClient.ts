559d2a9a432c36624099446cf442f8013057448c
Aren <aren@bransfer.io>
2025-10-06 16:30:19 +0400

    Provide refund_address when creating swap via wallet, resolves FRONT-665

--- lib/apiClients/layerSwapApiClient.ts
+++ lib/apiClients/layerSwapApiClient.ts
@@ -136,6 +136,7 @@ export type CreateSwapParams = {
     slippage?: number,
     destination_address: string,
     source_address?: string
+    refund_address?: string
     amount: string,
     reference_id?: string,
     source_exchange?: string
