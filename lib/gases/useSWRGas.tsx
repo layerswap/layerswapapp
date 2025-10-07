146c790337912d09c292f1e6cfce782942562c1e
Aren <aren@bransfer.io>
2025-10-07 17:56:48 +0400

    fix build

--- lib/gases/useSWRGas.tsx
+++ lib/gases/useSWRGas.tsx
@@ -2,9 +2,9 @@ import useSWR from "swr"
 import { Network, Token } from "@/Models/Network"
 import { GasResolver } from "./gasResolver"
 
-const useSWRGas = (address: string | undefined, network: Network | undefined, token?: Token, amount?: number | string): { gas: number | undefined, isGasLoading: boolean, gasError: any } => {
+const useSWRGas = (address: string | undefined | null, network: Network | undefined | null, token?: Token | null, amount?: number | string | null ): { gas: number | undefined, isGasLoading: boolean, gasError: any } => {
 
-    const { data: gasData, error: gasError, isLoading } = useSWR((network && address) ? `/gases/${address}/${network.name}/${token?.symbol}` : null, () => {
+    const { data: gasData, error: gasError, isLoading } = useSWR((network && address) ? `/gases/${address}/${network.name}/${token?.symbol}${amount ? `/${amount}` : ''}` : null, () => {
         if (!network || !token || !address) return
         return new GasResolver().getGas({ address, network, token, amount: Number(amount) })
     }, { refreshInterval: 60000 })
