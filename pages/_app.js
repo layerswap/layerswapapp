6aa9ff993ef1be0e269dfab28a77414326f3adf4
paur94 <paruyryeprikyan9@gmail.com>
2025-10-07 17:28:56 +0400

    test posthog

--- pages/_app.js
+++ pages/_app.js
@@ -17,10 +17,12 @@ const progress = new ProgressBar({
 });
 
 if (typeof window !== "undefined" && process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_API_VERSION === 'mainnet') {
-  posthog.init(process.env.POSTHOG_TOKEN, {
+  posthog.init('phc_KyI0bPCry0a8vJjQ7rIQESL8u2EVnyjrX19ZR4uqQgq', {
     capture_pageview: 'history_change',
     capture_pageleave: true,
-  });
+    api_host: 'https://us.i.posthog.com',
+    defaults: '2025-05-24'
+  })
 }
 
 Router.events.on("routeChangeStart", progress.start);
