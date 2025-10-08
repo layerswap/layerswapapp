36b27875964db1b9ef9fe3edbdfa778d466aa358
paur94 <paruyryeprikyan9@gmail.com>
2025-10-08 18:30:37 +0400

    test posthog without env

--- pages/_app.js
+++ pages/_app.js
@@ -18,10 +18,10 @@ const progress = new ProgressBar({
 });
 
 if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  posthog.init('phc_KyI0bPCry0a8vJjQ7rIQESL8u2EVnyjrX19ZR4uqQgq', {
     capture_pageview: 'history_change',
     capture_pageleave: true,
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: 'https://us.i.posthog.com',
     defaults: '2025-05-24'
   })
 }
