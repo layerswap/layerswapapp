54bd7027218a463b331b8a8874bebb4f8406fc85
yasha-meursault <y.hayrumyan.y@gmail.com>
2025-10-14 19:09:53 +0400

    fix incorrect path

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ import plugin from 'tailwindcss/plugin'
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "../../apps/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
     "../../packages/**/*.{js,ts,jsx,tsx}"
