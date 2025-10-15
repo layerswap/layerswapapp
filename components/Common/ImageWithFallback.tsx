5c8d871ef200bf9eef9140258690c972a6713b38
paur94 <paruyryeprikyan9@gmail.com>
2025-10-15 19:05:11 +0400

    test image

--- components/Common/ImageWithFallback.tsx
+++ components/Common/ImageWithFallback.tsx
@@ -1,7 +1,7 @@
 import Image, { ImageProps } from "next/image";
 import React, { forwardRef, useEffect, useState } from "react";
 
-const fallbackImage = '/images/logo_placeholder.png';
+const fallbackImage = 'https://layerswap.io/beta/_next/image?url=%2Fimages%2Flogo_placeholder.png&w=96&q=75&dpl=dpl_F5qCEJtwT2ipBr2zVii46hGUTTff';
 
 export const ImageWithFallback = forwardRef<HTMLImageElement, ImageProps>(({ src, ...props }, ref) => {
     const [imgSrc, setImgSrc] = useState(src);
@@ -14,11 +14,11 @@ export const ImageWithFallback = forwardRef<HTMLImageElement, ImageProps>(({ src
         setImgSrc(fallbackImage)
     }
 
-    return <Image 
+    return <Image
         {...props}
         alt={props.alt || 'ImageWithFallback'}
         ref={ref}
-        src={imgSrc}
+        src={fallbackImage}
         onError={handleErrpr}
         blurDataURL={fallbackImage}
     />;
