f7d008576d91cc4ae5153f3af761b3d009ce3395
paur94 <paruyryeprikyan9@gmail.com>
2025-10-16 15:26:38 +0400

    added svg instead of public image

--- components/Common/ImageWithFallback.tsx
+++ components/Common/ImageWithFallback.tsx
@@ -1,25 +1,29 @@
 import Image, { ImageProps } from "next/image";
 import React, { forwardRef, useEffect, useState } from "react";
-
-const fallbackImage = 'https://layerswap.io/beta/images/logo_placeholder.png';
+import LogoPlaceholder from "../icons/LogoPlaceholder";
 
 export const ImageWithFallback = forwardRef<HTMLImageElement, ImageProps>(({ src, ...props }, ref) => {
     const [imgSrc, setImgSrc] = useState(src);
+    const [hasError, setHasError] = useState(false);
 
     useEffect(() => {
         setImgSrc(src);
+        setHasError(false);
     }, [src])
 
-    const handleErrpr = () => {
-        setImgSrc(fallbackImage)
+    const handleError = () => {
+        setHasError(true);
+    }
+
+    if (hasError) {
+        return <LogoPlaceholder {...props} />;
     }
 
     return <Image
         {...props}
         alt={props.alt || 'ImageWithFallback'}
         ref={ref}
-        src={fallbackImage}
-        onError={handleErrpr}
-        blurDataURL={fallbackImage}
+        src={imgSrc}
+        onError={handleError}
     />;
 });
