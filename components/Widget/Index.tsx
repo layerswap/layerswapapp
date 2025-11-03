59d137736b4d1d4b256857f7c068afc354c30da8
Aren <aren@bransfer.io>
2025-11-03 15:10:32 +0400

    refactor: update ThemeData structure and enhance ColorSchema component
    - Made properties in ThemeData optional for better flexibility.
    - Added new header configuration options (hideMenu, hideTabs, hideWallets) to ThemeData.
    - Updated ColorSchema component to utilize the new optional properties and removed unnecessary state management.
    - Enhanced HeaderWithMenu to conditionally render components based on the new theme settings.
    - Adjusted Widget component to apply card background styles from theme data.

--- components/Widget/Index.tsx
+++ components/Widget/Index.tsx
@@ -2,9 +2,12 @@ import HeaderWithMenu from "../HeaderWithMenu"
 import { useRouter } from "next/router"
 import { default as Content } from './Content';
 import { default as Footer } from './Footer';
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { resolvePersistantQueryParams } from "../../helpers/querryHelper";
 import AppSettings from "../../lib/AppSettings";
+import { THEME_COLORS } from "@/Models/Theme";
+import clsx from "clsx";
+import inIframe from "../utils/inIframe";
 
 type Props = {
    children: JSX.Element | JSX.Element[];
@@ -26,12 +29,25 @@ const Widget = ({ children, hideMenu, contextualMenu }: Props) => {
          })
    }, [])
 
+   const theme = THEME_COLORS[router.query.theme?.toString() || 'default']
+
+   const [embedded, setEmbedded] = useState<boolean>(false)
+
+   useEffect(() => {
+      setEmbedded(inIframe())
+   }, [])
 
    const handleBack = router.pathname === "/" ? null : goBack
 
    return <div className="relative p-px">
       <div className="invisible sm:visible absolute inset-0 rounded-[25px] bg-gradient-to-t from-secondary-800 to-secondary-300 pointer-events-none" />
-      <div id="widget" className='md:shadow-lg sm:pb-4 rounded-3xl w-full sm:overflow-hidden relative bg-gradient-to-b from-secondary-700 to-secondary-700 max-sm:has-openpicker:min-h-svh max-sm:min-h-[99.8svh] sm:has-openpicker:min-h-[79svh] has-openwithdrawalmodal:min-h-[650px] h-full flex flex-col'>
+      <div
+         style={theme?.cardBackgroundStyle}
+         id="widget"
+         className={clsx('md:shadow-lg sm:pb-4 rounded-3xl w-full sm:overflow-hidden relative bg-secondary-700 max-sm:has-openpicker:min-h-svh max-sm:min-h-[99.8svh] sm:has-openpicker:min-h-[79svh] has-openwithdrawalmodal:min-h-[650px] h-full flex flex-col',
+            { "max-sm:min-h-[99svh]!": embedded }
+         )}
+      >
          {
             AppSettings.ApiVersion === 'sandbox' &&
             <div className="relative z-20">
