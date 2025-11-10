8d5cc21ea3efe5dbccd345f38cc0bbaae7cd20fd
Babken Mesropyan <mesropyan.b@gmail.com>
2025-11-10 17:22:41 +0400

    Remove console log from baseFeeMultiplier function

--- lib/resolveChain.ts
+++ lib/resolveChain.ts
@@ -58,7 +58,6 @@ export default function resolveChain(network: Network) {
         res.fees = {
             ...res.fees,
             baseFeeMultiplier: () => {
-                console.log("brrrah")
                 return baseFeeMultiplier
             },
         }
