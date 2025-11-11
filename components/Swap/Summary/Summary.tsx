44a061ddc74d3eea5323470935dbc20fd25f2fad
paur94 <paruyryeprikyan9@gmail.com>
2025-11-11 13:30:14 +0400

    added nowrap for request amount

--- components/Swap/Summary/Summary.tsx
+++ components/Swap/Summary/Summary.tsx
@@ -59,7 +59,7 @@ const Summary: FC<SwapInfoProps> = (props) => {
                     <div className="flex flex-col col-start-7 col-span-4 items-end">
                         {
                             requestedAmount &&
-                            <p className="text-primary-text text-sm">{truncateDecimals(requestedAmount, sourceCurrency.precision)} {sourceCurrency.symbol}</p>
+                            <p className="text-primary-text text-sm whitespace-nowrap">{truncateDecimals(requestedAmount, sourceCurrency.precision)} {sourceCurrency.symbol}</p>
                         }
                         <p className="text-secondary-text text-sm flex justify-end"><NumberFlow value={requestedAmountInUsd || 0} format={{ style: 'currency', currency: 'USD' }} trend={0} /></p>
                     </div>
