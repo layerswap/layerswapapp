78bf662659426b55c980774aed5be0975e2ca071
babkenmes <mesropyan.b@gmail.com>
2025-11-18 21:08:45 +0400

    Removed unnecessary classes

--- components/Input/Amount/ReceiveAmount.tsx
+++ components/Input/Amount/ReceiveAmount.tsx
@@ -14,12 +14,12 @@ export const ReceiveAmount: FC<ReceiveAmountProps> = ({ destination_token, fee, 
     const receive_amount = fee?.quote.receive_amount
     const receiveAmountInUsd = receive_amount && destination_token && fee.quote?.destination_token?.price_in_usd ? (receive_amount * fee.quote.destination_token.price_in_usd).toFixed(2) : undefined
     const quote = fee?.quote
-    
+
     return (<>
         <div className="flex-col w-full flex min-w-0 font-normal border-0 text-[28px] leading-7 text-primary-text relative truncate">
             <div className="w-full flex items-center justify-start relative">
                 <div className={clsx(
-                    "w-full flex items-center py-[3px] pr-3 receiveAmount",
+                    "w-full flex items-center py-[3px] pr-3",
                     { "animate-pulse-stronger": isFeeLoading },
                     { "text-secondary-text": !receive_amount }
                 )}>
@@ -27,7 +27,7 @@ export const ReceiveAmount: FC<ReceiveAmountProps> = ({ destination_token, fee, 
                 </div>
             </div>
             <div className="flex items-baseline space-x-2">
-                <span className="text-base leading-5 font-medium text-secondary-text h-5 receiveAmountInUsd">
+                <span className="text-base leading-5 font-medium text-secondary-text h-5">
                     <NumberFlow className="p-0" value={receiveAmountInUsd || 0} format={{ style: 'currency', currency: 'USD', maximumFractionDigits: receiveAmountInUsd ? 2 : 0 }} trend={0} />
                 </span>
                 <PriceImpact className="h-5 text-base leading-5" quote={quote} />
