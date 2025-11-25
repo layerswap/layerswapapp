fc4a531d2996459f0b240a453c820aa113aff122
paur94 <paruyryeprikyan9@gmail.com>
2025-11-25 17:01:56 +0400

    redesign 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,6 +5,7 @@ import MessageComponent from "../components/MessageComponent"
 import Navbar from "../components/navbar"
 import GoHomeButton from "../components/utils/GoHome"
 import { posthog } from "posthog-js"
+import NotFoundIcon from "@/components/icons/NotFoundIcon"
 
 export default function Custom404() {
     useEffect(() => {
@@ -19,21 +20,35 @@ export default function Custom404() {
             <main className="styled-scroll">
                 <div className="min-h-screen overflow-hidden relative font-robo">
                     <Navbar />
-                    <div className="mx-auto max-w-xl bg-darkblue-900 md:shadow-card rounded-lg w-full overflow-hidden relative px-4 py-6 h-[500px] min-h-[550px]">
+                    <div className="mx-auto max-w-lg bg-secondary-700 shadow-card rounded-lg w-full overflow-hidden relative px-6 pt-6 pb-2 h-[500px] min-h-[550px]">
                         <MessageComponent>
-                            <MessageComponent.Content icon="red">
-                                <div className="text-center">
-                                    <p className="text-base font-semibold text-primary">404</p>
-                                    <h1 className="mt-2 text-4xl font-bold tracking-tight text-primary-text sm:text-5xl">Page not found.</h1>
-                                    <p className="mt-2 text-base text-secondary-text">Sorry, we couldn&apos;t find the page you&apos;re looking for.</p>
-                                </div>
+                            <MessageComponent.Content center>
+                                <MessageComponent.Header className="mb-3">
+                                    <div className="mx-auto mb-4 flex h-16 w-16 items-center justify-center rounded-full bg-red-600/20">
+                                        <NotFoundIcon />
+                                    </div>
+                                    <h1 className="text-center text-2xl font-semibold text-white">
+                                        Page not found.
+                                    </h1>
+                                </MessageComponent.Header>
+                                <MessageComponent.Description>
+                                    <p className="mx-auto text-center text-base font-normal leading-5 text-secondary-text px-9">
+                                        <span>Sorry, we couldn't find the page you're looking for.</span>
+                                    </p>
+                                </MessageComponent.Description>
                             </MessageComponent.Content>
                             <MessageComponent.Buttons>
-                                <GoHomeButton>
-                                    <SubmitButton buttonStyle="outline" isDisabled={false} isSubmitting={false} icon={<Home className="h-5 w-5" />}>
-                                        Go home
-                                    </SubmitButton>
-                                </GoHomeButton>
+                                <div className="flex flex-col w-full text-primary-text text-base space-y-2">
+                                    <GoHomeButton>
+                                        <button
+                                            type="button"
+                                            className="w-full inline-flex items-center justify-center gap-2 rounded-xl bg-secondary-300 px-5 py-3 text-base font-semibold leading-6 hover:bg-secondary-400 focus:outline-none transition"
+                                        >
+                                            <Home className="h-5 w-5" aria-hidden="true" />
+                                            <span>Back to app</span>
+                                        </button>
+                                    </GoHomeButton>
+                                </div>
                             </MessageComponent.Buttons>
                         </MessageComponent>
                     </div>
