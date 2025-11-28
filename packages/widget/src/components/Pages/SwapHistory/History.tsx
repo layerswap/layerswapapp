71839e0c5cfaa4eb42bb3b7d814e75dcb5f3d5a8
Aren <aren@bransfer.io>
2025-11-28 15:25:25 +0400

    Update History component to use shadow-card class for improved styling of skeleton items

--- packages/widget/src/components/Pages/SwapHistory/History.tsx
+++ packages/widget/src/components/Pages/SwapHistory/History.tsx
@@ -229,7 +229,7 @@ const BlankHistory = ({ onNewTransferClick }: BlankHistoryProps) => {
     return <div className="w-full h-full min-h-[inherit] flex flex-col justify-between items-center space-y-10">
         <div />
         <div className="w-full h-full flex flex-col justify-center items-center ">
-            <HistoryItemSceleton className="scale-[.63] w-full shadow-lg mr-7" />
+            <HistoryItemSceleton className="scale-[.63] w-full shadow-card mr-7" />
             <HistoryItemSceleton className="scale-[.63] -mt-12 shadow-card ml-7 w-full" />
             <div className="mt-2 text-center space-y-2">
                 <h1 className="text-secondary-text text-[28px] font-bold tracking-wide" >
