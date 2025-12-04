207da95eb1ab14071b27358a72beec0d8bc35520
Aren <aren@bransfer.io>
2025-12-04 14:00:04 +0400

    feat: add Controller connector to KnownStarknetConnectors

--- lib/wallets/starknet/KnownStarknetConnectors.tsx
+++ lib/wallets/starknet/KnownStarknetConnectors.tsx
@@ -1,6 +1,7 @@
 import Argent from "@/components/icons/Wallets/Argent"
 import ArgentX from "@/components/icons/Wallets/ArgentX"
 import Braavos from "@/components/icons/Wallets/Braavos"
+import Controller from "@/components/icons/Wallets/Controller"
 import Keplr from "@/components/icons/Wallets/Keplr"
 import Xverse from "@/components/icons/Wallets/Xverse"
 
@@ -24,6 +25,10 @@ const KnownStarknetConnectors = [
     {
         id: 'xverse wallet',
         icon: Xverse
+    },
+    {
+        id: 'cartridge controller',
+        icon: Controller
     }
 ]
 
