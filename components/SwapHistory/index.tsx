0d12f57d1e4ca0579a1bd68066dfe3e3fdf13d71
Aren <aren@bransfer.io>
2025-12-05 15:23:40 +0400

    refactor: update shadow styles and adjust container height in SwapHistory component

--- components/SwapHistory/index.tsx
+++ components/SwapHistory/index.tsx
@@ -7,7 +7,7 @@ function TransactionsHistory() {
     <div id="widget" className='bg-secondary-700 sm:shadow-card sm:relative rounded-3xl w-full text-primary-text overflow-y-auto sm:overflow-hidden max-h-screen h-full sm:h-[650px]'>
       <div className="overflow-y-auto flex flex-col h-full z-40 pb-4">
         <Header />
-        <div className="px-4 h-full overflow-y-auto styled-scroll max-h-[80vh]" id='virtualListContainer'>
+        <div className="px-4 overflow-y-auto styled-scroll h-[90svh] sm:h-full" id='virtualListContainer'>
           <Content />
         </div>
       </div>
