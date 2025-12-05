0a2d4fe09e5b43bf0a03e3c0bae73103b8e75b96
arentant <arentant@users.noreply.github.com>
2025-12-05 13:05:18 +0000

    Deploy preview for PR 1746 🛫

---
+++ pr-preview/pr-1746/487.0a142fce.iframe.bundle.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunklayerswap=self.webpackChunklayerswap||[]).push([[487],{"./node_modules/@reown/appkit-ui/dist/esm/src/assets/svg/chevron-top.js":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{chevronTopSvg:()=>chevronTopSvg});const chevronTopSvg=__webpack_require__("./node_modules/lit/index.js").JW`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M14.54 11.04a1 1 0 0 1-1.41 0L8 5.92l-5.13 5.12a1 1 0 1 1-1.41-1.41l5.83-5.84a1 1 0 0 1 1.42 0l5.83 5.84a1 1 0 0 1 0 1.41Z"
+    clip-rule="evenodd"
+  />
+</svg>`}}]);
