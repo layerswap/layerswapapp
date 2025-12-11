48c39c21fdef97311f9d16d0120dced686f800e8
Aren <aren@bransfer.io>
2025-12-11 19:00:53 +0400

    Update solana packages

--- components/WalletProviders/SolanaProvider.tsx
+++ components/WalletProviders/SolanaProvider.tsx
@@ -1,15 +1,11 @@
 import { clusterApiUrl } from "@solana/web3.js";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
-import { WalletConnectWalletAdapter } from "@solana/wallet-adapter-walletconnect";
-import { GlowWalletAdapter } from "@solana/wallet-adapter-glow";
-import { NightlyWalletAdapter } from "@solana/wallet-adapter-nightly";
 import {
     ConnectionProvider,
     WalletProvider,
 } from "@solana/wallet-adapter-react";
+import { NightlyWalletAdapter, WalletConnectWalletAdapter, PhantomWalletAdapter, CoinbaseWalletAdapter, SolflareWalletAdapter, BitgetWalletAdapter, TrustWalletAdapter, LedgerWalletAdapter, HuobiWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { ReactNode, useMemo } from "react";
-import { CoinbaseWalletAdapter } from "@solana/wallet-adapter-coinbase";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 
 const WALLETCONNECT_PROJECT_ID = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || '28168903b2d30c75e5f7f2d71902581b';
 
@@ -20,8 +16,12 @@ function SolanaProvider({ children }: { children: ReactNode }) {
         () => [
             new PhantomWalletAdapter(),
             new CoinbaseWalletAdapter(),
-            new GlowWalletAdapter(),
             new NightlyWalletAdapter(),
+            new SolflareWalletAdapter(),
+            new BitgetWalletAdapter(),
+            new TrustWalletAdapter(),
+            new LedgerWalletAdapter(),
+            new HuobiWalletAdapter(),
             new WalletConnectWalletAdapter({
                 network: solNetwork,
                 options: {
