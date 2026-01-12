6b160c144bf40dceae05b15413d561f75efeb946
Aren <aren@bransfer.io>
2026-01-12 15:03:05 +0400

    Update version to 1.1.5 and add changelog entry for minor bug fixes and improvements

--- packages/widget/src/context/settings.tsx
+++ packages/widget/src/context/settings.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { Context, FC, useEffect, useState } from 'react'
 import { LayerSwapAppSettings } from '../Models/LayerSwapAppSettings';
 import { InitialSettings } from '../Models/InitialSettings';
