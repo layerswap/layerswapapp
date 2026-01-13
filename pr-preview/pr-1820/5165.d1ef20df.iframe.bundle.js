1648d47b815492e7994e931bd4553b19c12f4728
babkenmes <babkenmes@users.noreply.github.com>
2026-01-13 18:49:15 +0000

    Deploy preview for PR 1820 🛫

---
+++ pr-preview/pr-1820/5165.d1ef20df.iframe.bundle.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunklayerswap=self.webpackChunklayerswap||[]).push([[5165],{"./node_modules/@walletconnect/solana-adapter/node_modules/@reown/appkit-ui/dist/esm/src/assets/svg/off.js":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{offSvg:()=>offSvg});const offSvg=__webpack_require__("./node_modules/@walletconnect/solana-adapter/node_modules/lit/index.js").JW`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M8 0a1 1 0 0 1 1 1v5.38a1 1 0 0 1-2 0V1a1 1 0 0 1 1-1ZM5.26 2.6a1 1 0 0 1-.28 1.39 5.46 5.46 0 1 0 6.04 0 1 1 0 1 1 1.1-1.67 7.46 7.46 0 1 1-8.25 0 1 1 0 0 1 1.4.28Z"
+    clip-rule="evenodd"
+  />
+</svg>`}}]);
