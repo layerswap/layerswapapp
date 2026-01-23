3a0b461d040a585ea114e5dcf86515bc8cbc4d1a
babkenmes <babkenmes@users.noreply.github.com>
2026-01-23 12:37:07 +0000

    Deploy preview for PR 1846 🛫

---
+++ pr-preview/pr-1846/2241.669be88e.iframe.bundle.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunklayerswap=self.webpackChunklayerswap||[]).push([[2241],{"./node_modules/@reown/appkit-ui/dist/esm/src/assets/svg/swapVertical.js":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{swapVerticalSvg:()=>swapVerticalSvg});const swapVerticalSvg=__webpack_require__("./node_modules/lit/index.js").JW`<svg fill="none" viewBox="0 0 14 14">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M3.48 2.18a1 1 0 0 1 1.41 0l2.68 2.68a1 1 0 1 1-1.41 1.42l-.98-.98v4.56a1 1 0 0 1-2 0V5.3l-.97.98A1 1 0 0 1 .79 4.86l2.69-2.68Zm6.34 2.93a1 1 0 0 1 1 1v4.56l.97-.98a1 1 0 1 1 1.42 1.42l-2.69 2.68a1 1 0 0 1-1.41 0l-2.68-2.68a1 1 0 0 1 1.41-1.42l.98.98V6.1a1 1 0 0 1 1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`}}]);
