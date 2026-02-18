bd71f7fa397d6b1a0c5942035e6a71fa20a40508
babkenmes <babkenmes@users.noreply.github.com>
2026-02-18 15:01:12 +0000

    Deploy preview for PR 1881 🛫

---
+++ pr-preview/pr-1881/6080.4066cbba.iframe.bundle.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunklayerswap=self.webpackChunklayerswap||[]).push([[6080],{"./node_modules/@reown/appkit-ui/dist/esm/src/assets/svg/arrow-right.js":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{arrowRightSvg:()=>arrowRightSvg});const arrowRightSvg=__webpack_require__("./node_modules/lit/index.js").JW`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M1 7.99a1 1 0 0 1 1-1h7.58L7.12 4.53A1 1 0 1 1 8.54 3.1l4.16 4.17a1 1 0 0 1 0 1.41l-4.16 4.17a1 1 0 1 1-1.42-1.41l2.46-2.46H2a1 1 0 0 1-1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`}}]);
