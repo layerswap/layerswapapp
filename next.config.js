b575e018896d46fa3a021632fd89384fe07ae598
babkenmes <mesropyan.b@gmail.com>
2026-02-24 18:16:34 +0400

    refactor: update PostHog configuration to conditionally set project ID based on environment variables

--- next.config.js
+++ next.config.js
@@ -36,17 +36,18 @@ module.exports = (phase, { defaultConfig }) => {
    * @type {import('next').NextConfig}
    */
 
-  const posthogWrapped = withPostHogConfig({}, {
+  const posthogConfigsAreSet = process.env.POSTHOG_ENV_ID && process.env.POSTHOG_API_KEY && process.env.NEXT_PUBLIC_POSTHOG_HOST;
+
+  const posthogWrapped = posthogConfigsAreSet ? withPostHogConfig({}, {
     personalApiKey: process.env.POSTHOG_API_KEY,
-    envId: process.env.POSTHOG_ENV_ID,
+    projectId: process.env.POSTHOG_ENV_ID,
     host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     sourcemaps: {
       enabled: true,
       project: 'Layerswap',
-      version: process.env.VERCEL_GIT_COMMIT_SHA,
       deleteAfterUpload: true,
     },
-  });
+  }) : {};
 
   const nextConfig = {
     i18n: {
