65d0dc23a6dd06ae3dcc7868a0d09e12cbccc3ec
babkenmes <babkenmes@users.noreply.github.com>
2026-02-26 19:15:28 +0000

    Deploy preview for PR 1896 🛫

---
+++ pr-preview/pr-1896/5527.11404535.iframe.bundle.js
@@ -0,0 +1,11 @@
+"use strict";(self.webpackChunklayerswap=self.webpackChunklayerswap||[]).push([[5527],{"./node_modules/@walletconnect/solana-adapter/node_modules/@reown/appkit-ui/dist/esm/src/assets/svg/more.js"(__unused_webpack_module,__webpack_exports__,__webpack_require__){__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{moreSvg:()=>moreSvg});const moreSvg=__webpack_require__("./node_modules/@walletconnect/solana-adapter/node_modules/lit/index.js").JW`<svg fill="none" viewBox="0 0 41 40">
+  <path
+    style="fill: var(--wui-color-fg-100);"
+    fill-opacity=".05"
+    d="M.6 20a20 20 0 1 1 40 0 20 20 0 0 1-40 0Z"
+  />
+  <path
+    fill="#949E9E"
+    d="M15.6 20.31a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0ZM23.1 20.31a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0ZM28.1 22.81a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"
+  />
+</svg>`}}]);
