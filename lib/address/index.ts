65f910199fb0985eb33cfac57991f32c1933ff4c
Aren <aren@bransfer.io>
2026-02-26 18:12:03 +0400

    refactor: remove Immutable X wallet references and related address utilities

--- lib/address/index.ts
+++
@@ -1,8 +0,0 @@
-// Export Address class and related types
-export { Address, EmailAddress, isEmailAddress } from './Address';
-export type { AddressDisplayFormat, AddressFormatOptions } from './Address';
-
-// Re-export existing utilities for backward compatibility
-export { isValidAddress } from './validator';
-export { addressFormat } from './formatter';
-export { getExplorerUrl } from './explorerUrl';
