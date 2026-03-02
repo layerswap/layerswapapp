ffcff069fbfb1ddf5351b4d5cdcaba6b72e022a5
babkenmes <babkenmes@users.noreply.github.com>
2026-03-02 18:45:55 +0000

    Deploy preview for PR 1901 🛫

---
+++ pr-preview/pr-1901/8682.7ce01ef4.iframe.bundle.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunklayerswap=self.webpackChunklayerswap||[]).push([[8682],{"./node_modules/@walletconnect/solana-adapter/node_modules/@reown/appkit-ui/dist/esm/src/assets/svg/external-link.js"(__unused_webpack_module,__webpack_exports__,__webpack_require__){__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{externalLinkSvg:()=>externalLinkSvg});const externalLinkSvg=__webpack_require__("./node_modules/@walletconnect/solana-adapter/node_modules/lit/index.js").JW`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M3.74 3.99a1 1 0 0 1 1-1H11a1 1 0 0 1 1 1v6.26a1 1 0 0 1-2 0V6.4l-6.3 6.3a1 1 0 0 1-1.4-1.42l6.29-6.3H4.74a1 1 0 0 1-1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`}}]);
