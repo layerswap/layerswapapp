c67053a4854e1b6358c0b30740817f70f93544ad
yasha-meursault <yasha-meursault@users.noreply.github.com>
2026-03-13 12:37:28 +0000

    Deploy preview for PR 1911 🛫

---
+++ pr-preview/pr-1911/6409.b0069690.iframe.bundle.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunklayerswap=self.webpackChunklayerswap||[]).push([[6409],{"./node_modules/@walletconnect/solana-adapter/node_modules/@reown/appkit-ui/dist/esm/src/assets/svg/arrow-bottom.js"(__unused_webpack_module,__webpack_exports__,__webpack_require__){__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{arrowBottomSvg:()=>arrowBottomSvg});const arrowBottomSvg=__webpack_require__("./node_modules/@walletconnect/solana-adapter/node_modules/lit/index.js").JW`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M7 1.99a1 1 0 0 1 1 1v7.58l2.46-2.46a1 1 0 0 1 1.41 1.42L7.7 13.69a1 1 0 0 1-1.41 0L2.12 9.53A1 1 0 0 1 3.54 8.1L6 10.57V3a1 1 0 0 1 1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`}}]);
