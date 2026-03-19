a52064d312953a03d46fe0e3fcc02acbc408c280
arentant <arentant@users.noreply.github.com>
2026-03-19 17:24:57 +0000

    Deploy preview for PR 1919 🛫

---
+++ pr-preview/pr-1919/4833.495f78e1.iframe.bundle.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunklayerswap=self.webpackChunklayerswap||[]).push([[4833],{"./node_modules/@walletconnect/solana-adapter/node_modules/@reown/appkit-ui/dist/esm/src/assets/svg/chevron-left.js"(__unused_webpack_module,__webpack_exports__,__webpack_require__){__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{chevronLeftSvg:()=>chevronLeftSvg});const chevronLeftSvg=__webpack_require__("./node_modules/@walletconnect/solana-adapter/node_modules/lit/index.js").JW`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M11.04 1.46a1 1 0 0 1 0 1.41L5.91 8l5.13 5.13a1 1 0 1 1-1.41 1.41L3.79 8.71a1 1 0 0 1 0-1.42l5.84-5.83a1 1 0 0 1 1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`}}]);
