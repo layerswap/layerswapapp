a7b037a9f752137190db15f3c6e4a674a0b4cd82
Aren <aren@bransfer.io>
2026-04-15 21:26:28 +0400

    Update WalletModal components for improved layout and responsiveness
    - Adjusted height properties in ConnectorsList, InstalledExtensionNotFound, LoadingConnect, and WalletQrCode components to ensure full height usage across different screen sizes.
    - Enhanced the layout of the VaulModal to support flexible content display.
    - Refined the WalletProvidersProvider to maintain consistent height for the ConnectorsList, improving user experience during wallet connection.

--- components/WalletModal/LoadingConnect.tsx
+++ components/WalletModal/LoadingConnect.tsx
@@ -2,23 +2,15 @@ import { FC } from "react";
 import { WalletModalConnector } from ".";
 import { Link2Off, RotateCw } from "lucide-react";
 import { resolveWalletConnectorIcon } from "../../lib/wallets/utils/resolveWalletIcon";
-import clsx from "clsx";
-import useWindowDimensions from "../../hooks/useWindowDimensions";
 import LayerSwapLogoSmall from "../icons/layerSwapLogoSmall";
 import { isMobile } from "@/lib/wallets/connectors/utils/isMobile";
 
 export const LoadingConnect: FC<{ onRetry: () => void, selectedConnector: WalletModalConnector, connectionError: string | undefined }> = ({ onRetry, selectedConnector, connectionError }) => {
     const ConnectorIcon = resolveWalletConnectorIcon({ connector: selectedConnector?.name, iconUrl: selectedConnector.icon });
-    const { isMobile: isMobileSize } = useWindowDimensions()
     const isMobilePlatform = isMobile();
 
     return (
-        <div
-            className={clsx('w-full flex flex-col justify-between items-center relative', {
-                'h-[60vh]': isMobileSize,
-                'h-full': !isMobileSize,
-            })}
-        >
+        <div className='w-full flex flex-col justify-between items-center relative h-full'>
             {
                 selectedConnector &&
                 <div className="flex grow items-center">
