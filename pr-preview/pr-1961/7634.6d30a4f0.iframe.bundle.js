be7736f4705992892e597209fb66d5b8bbc55ef5
arentant <arentant@users.noreply.github.com>
2026-04-20 17:57:21 +0000

    Deploy preview for PR 1961 🛫

---
+++ pr-preview/pr-1961/7634.6d30a4f0.iframe.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklayerswap=self.webpackChunklayerswap||[]).push([[7634],{"./node_modules/@metamask/connect-multichain/node_modules/ws/browser.js"(module){module.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
