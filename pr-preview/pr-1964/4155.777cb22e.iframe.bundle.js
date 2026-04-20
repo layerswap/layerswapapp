6426b8937456ebf7b595ce58702984de0810b64a
arentant <arentant@users.noreply.github.com>
2026-04-20 20:14:48 +0000

    Deploy preview for PR 1964 🛫

---
+++ pr-preview/pr-1964/4155.777cb22e.iframe.bundle.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunklayerswap=self.webpackChunklayerswap||[]).push([[4155],{"./node_modules/@walletconnect/solana-adapter/node_modules/@reown/appkit-ui/dist/esm/src/assets/svg/arrow-left.js"(__unused_webpack_module,__webpack_exports__,__webpack_require__){__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{arrowLeftSvg:()=>arrowLeftSvg});const arrowLeftSvg=__webpack_require__("./node_modules/@walletconnect/solana-adapter/node_modules/lit/index.js").JW`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M13 7.99a1 1 0 0 1-1 1H4.4l2.46 2.46a1 1 0 1 1-1.41 1.41L1.29 8.7a1 1 0 0 1 0-1.41L5.46 3.1a1 1 0 0 1 1.41 1.42L4.41 6.99H12a1 1 0 0 1 1 1Z"
+    clip-rule="evenodd"
+  />
+</svg>`}}]);
