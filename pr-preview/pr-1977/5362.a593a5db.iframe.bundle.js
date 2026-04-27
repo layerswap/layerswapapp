8f0cdde939c591d228b930d04dcb52f7ed4aaed5
arentant <arentant@users.noreply.github.com>
2026-04-27 17:22:24 +0000

    Deploy preview for PR 1977 🛫

---
+++ pr-preview/pr-1977/5362.a593a5db.iframe.bundle.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunklayerswap=self.webpackChunklayerswap||[]).push([[5362],{"./node_modules/@reown/appkit-ui/dist/esm/src/assets/svg/close.js"(__unused_webpack_module,__webpack_exports__,__webpack_require__){__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{closeSvg:()=>closeSvg});const closeSvg=__webpack_require__("./node_modules/lit/index.js").JW`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M2.54 2.54a1 1 0 0 1 1.42 0L8 6.6l4.04-4.05a1 1 0 1 1 1.42 1.42L9.4 8l4.05 4.04a1 1 0 0 1-1.42 1.42L8 9.4l-4.04 4.05a1 1 0 0 1-1.42-1.42L6.6 8 2.54 3.96a1 1 0 0 1 0-1.42Z"
+    clip-rule="evenodd"
+  />
+</svg>`}}]);
