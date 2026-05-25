dcf7c9c1242a8ac5064bbb3e7fa5b2c7b574082e
babkenmes <mesropyan.b@gmail.com>
2026-05-25 20:49:04 +0400

    fix: simplify type casting for selectedSource and selectedDestination in DepositAddressForm

--- components/Swap/Form/DepositAddressForm/index.tsx
+++ components/Swap/Form/DepositAddressForm/index.tsx
@@ -204,7 +204,7 @@ const DepositAddressForm: FC<Props> = () => {
                                     <>
                                         {/* Source (Pay from) */}
                                         <PayFromPicker
-                                            selectedSource={from && fromAsset ? { network: from as unknown as NetworkRoute, token: fromAsset as NetworkRouteToken } : null}
+                                            selectedSource={from && fromAsset ? { network: from, token: fromAsset } : null}
                                             onSourceChange={(network, token) => {
                                                 setSwapId(undefined);
                                                 setFieldValue('from', network, false);
@@ -216,7 +216,7 @@ const DepositAddressForm: FC<Props> = () => {
 
                                         {/* Destination network/token + recipient address share one "Receive" row */}
                                         <ReceivePicker
-                                            selectedDestination={destination && toCurrency ? { network: destination as unknown as NetworkRoute, token: toCurrency as NetworkRouteToken } : null}
+                                            selectedDestination={destination && toCurrency ? { network: destination, token: toCurrency } : null}
                                             onDestinationChange={(network, token) => {
                                                 setSwapId(undefined);
                                                 setFieldValue('to', network, false);
